b54e1118c5303227858cf30a6e467c67a1b1da51
Evengard <evengard@trioptimum.ru>
2019-09-19 03:53:27 +0300

    Filling backbuffer surface black before starting a video.

--- src/vqa/vqa_public.cpp
+++ src/vqa/vqa_public.cpp
@@ -1,6 +1,7 @@
 #include "vqa_file.h"
 
 extern "C" SDL_Surface* movieSurface;
+extern "C" SDL_Surface* g_backbuffer1;
 extern "C" Uint32 g_format;
 
 extern "C" int PlayMovieCallback(byte* frame, dword cx, dword cy);
@@ -20,6 +21,7 @@ extern "C" int PlayMovie(char* filename)
     if (valid)
     {
         movieSurface = SDL_CreateRGBSurfaceWithFormat(0, file.get_cx(), file.get_cy(), 16, g_format);
+        SDL_FillRect(g_backbuffer1, NULL, SDL_MapRGB(g_backbuffer1->format, 0, 0, 0));
         file.extract_both();
         movieSurface = NULL;
     }
