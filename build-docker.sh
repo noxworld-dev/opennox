3da72a5609220a1df6357ac7c88f936d78a82921
Denys Smirnov <denis.smirnov.91@gmail.com>
2019-11-18 00:20:33 +0200

    Add Docker-based build scripts for WASM and Linux.

---
+++ build-docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+rm -rf build;
+set -e
+mkdir -p build/linux
+mkdir -p build/web
+docker build -t nox:linux -f Dockerfile_linux .
+docker build -t nox:wasm -f Dockerfile_wasm .
+echo "build complete, copying files..."
+docker run --rm --user $(id -u):$(id -g) -v $(pwd)/build/linux:/ext --entrypoint bash nox:linux -c 'cp /src/out* /ext/'
+docker run --rm --user $(id -u):$(id -g) -v $(pwd)/build/web:/ext --entrypoint bash nox:wasm -c 'cp /src/out* /ext/'
+echo "done!"
