f78bc2c4e229e7b35b09971fdd1b1c8e24691d64
Denys Smirnov <denis.smirnov.91@gmail.com>
2019-11-23 21:11:16 +0200

    A few more fixes for WASM compilation.

--- build-docker.sh
+++ build-docker.sh
@@ -8,4 +8,5 @@ docker build -t nox:wasm -f Dockerfile_wasm .
 echo "build complete, copying files..."
 docker run --rm --user $(id -u):$(id -g) -v $(pwd)/build/linux:/ext --entrypoint bash nox:linux -c 'cp /src/out* /ext/'
 docker run --rm --user $(id -u):$(id -g) -v $(pwd)/build/web:/ext --entrypoint bash nox:wasm -c 'cp /src/out* /ext/'
+cp ./src/ws-server/static/* ./build/web/
 echo "done!"
