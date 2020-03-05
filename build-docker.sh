6d7bb42a79be16705c0afab89f1b18a4d7537bc3
Anton Afanasyev <aasoft@gmail.com>
2020-03-04 22:56:01 -0800

    Use ccache to cache builds. While here, also reorganize Dockerfiles for fewer rebuilds of the images

--- build-docker.sh
+++ build-docker.sh
@@ -1,12 +1,37 @@
 #!/bin/bash
-rm -rf build;
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+rm -rf ${DIR}/build/linux ${DIR}/build/web
+
 set -e
-mkdir -p build/linux
-mkdir -p build/web
+
+mkdir -p ${DIR}/build
+mkdir -p ${DIR}/build/linux
+mkdir -p ${DIR}/build/web
+mkdir -p ${DIR}/build/ccache
+mkdir -p ${DIR}/build/emscripten_cache
+
+echo "Building builder images..."
+cd ${DIR}/docker
+docker build -t nox:base_build_image -f Dockerfile_base .
 docker build -t nox:linux -f Dockerfile_linux .
 docker build -t nox:wasm -f Dockerfile_wasm .
-echo "build complete, copying files..."
-docker run --rm --user $(id -u):$(id -g) -v $(pwd)/build/linux:/ext --entrypoint bash nox:linux -c 'cp /src/out* /ext/'
-docker run --rm --user $(id -u):$(id -g) -v $(pwd)/build/web:/ext --entrypoint bash nox:wasm -c 'cp /src/out* /ext/'
-cp ./src/ws-server/static/* ./build/web/
+
+echo "Building project (target=linux)..."
+docker run --rm --user $(id -u):$(id -g) \
+	-v ${DIR}/build/ccache:/home/runner/.ccache \
+	-v ${DIR}/src:/home/runner/src:ro \
+	-v ${DIR}/build/linux:/home/runner/bin \
+	--network="none" \
+	nox:linux
+
+echo "Building project (target=wasm)..."
+docker run --rm --user $(id -u):$(id -g) \
+	-v ${DIR}/build/ccache:/home/runner/.ccache \
+	-v ${DIR}/build/emscripten_cache:/home/runner/.emscripten_cache \
+	-v ${DIR}/src:/home/runner/src:ro \
+	-v ${DIR}/build/web:/home/runner/bin \
+	nox:wasm
+cp ${DIR}/src/ws-server/static/* ${DIR}/build/web/
+
 echo "done!"
