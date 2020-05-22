e729fc17a912602c8490d0b2efcd833164f00a98
Denys Smirnov <dennwc@pm.me>
2020-05-22 21:38:44 +0300

    Split server system functions.

---
+++ src/server/system/trade.h
@@ -0,0 +1,13 @@
+#ifndef NOX_PORT_SERVER_SYSTEM_TRADE
+#define NOX_PORT_SERVER_SYSTEM_TRADE
+
+#include "../../defs.h"
+
+_DWORD* __cdecl sub_50EF10(int a1, int a2);
+int __cdecl sub_50F820(int, int, float); // idb
+float* __cdecl sub_5100C0(int a1, _DWORD* a2, int a3);
+float* __cdecl sub_510640(int a1, int a2, int a3, float* a4);
+_DWORD* __cdecl sub_5109C0(int* a1, int a2, _DWORD* a3);
+_DWORD* __cdecl sub_510BE0(int* a1, int a2, _DWORD* a3);
+
+#endif // NOX_PORT_SERVER_SYSTEM_TRADE
