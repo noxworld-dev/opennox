4642bec309939a1e7d31fab7cacbd6c1f6bb4ca1
Denys Smirnov <dennwc@pm.me>
2020-05-22 21:02:10 +0300

    Split common settings functions.

--- src/GAME1.c
+++ src/GAME1.c
@@ -1546,78 +1546,11 @@ int sub_409FA0()
     return *(_DWORD*)& byte_5D4594[3464];
 }
 
-//----- (00409FB0) --------------------------------------------------------
-void __cdecl sub_409FB0(__int16 a1, unsigned __int16 a2)
-{
-    int v2; // eax
-    unsigned __int16 v3; // si
-    wchar_t* v4; // eax
-
-    v2 = sub_409A70(a1);
-    v3 = a2;
-    if (*(_WORD*)& byte_5D4594[2 * v2 + 3488] != a2)
-    {
-        if (a2 > 0x3E7u)
-            v3 = 999;
-        *(_WORD*)& byte_5D4594[2 * v2 + 3488] = v3;
-        *(_DWORD*)& byte_5D4594[3532] = 1;
-        if (sub_43C700())
-        {
-            v4 = loadString_sub_40F1D0((char*)& byte_587000[4804], 0, "C:\\NoxPost\\src\\Common\\System\\settings.c", 389);
-            sub_440A20(v4, v3);
-        }
-    }
-}
-
 //----- (0040A020) --------------------------------------------------------
 __int16 __cdecl sub_40A020(__int16 a1)
 {
     return *(_WORD*)& byte_5D4594[2 * sub_409A70(a1) + 3488];
 }
-
-//----- (0040A040) --------------------------------------------------------
-void __cdecl sub_40A040(__int16 a1, unsigned __int8 a2)
-{
-    int v2; // edi
-    wchar_t* v3; // eax
-    int v4; // eax
-    wchar_t* v5; // eax
-    wchar_t* v6; // eax
-
-    v2 = sub_409A70(a1);
-    if (byte_5D4594[v2 + 3500] != a2)
-    {
-        if (!sub_40A5C0(0x4000000) && !*(_DWORD*)& byte_5D4594[3592])
-        {
-            *(_DWORD*)& byte_5D4594[3532] = 1;
-            if (sub_43C700())
-            {
-                if (!a2)
-                {
-                    v5 = loadString_sub_40F1D0((char*)& byte_587000[5048], 0, "C:\\NoxPost\\src\\Common\\System\\settings.c", 431);
-                    goto LABEL_9;
-                }
-                v3 = loadString_sub_40F1D0((char*)& byte_587000[4928], 0, "C:\\NoxPost\\src\\Common\\System\\settings.c", 423);
-                sub_440A20(v3, a2);
-                LOBYTE(v4) = sub_40A8A0();
-                if (!v4)
-                {
-                    v5 = loadString_sub_40F1D0((char*)& byte_587000[4988], 0, "C:\\NoxPost\\src\\Common\\System\\settings.c", 426);
-                LABEL_9:
-                    sub_440A20(v5);
-                    goto LABEL_10;
-                }
-            }
-        LABEL_10:
-            byte_5D4594[v2 + 3500] = a2;
-            *(_QWORD*)& byte_5D4594[3468] = 60000 * a2 + sub_416BB0();
-            return;
-        }
-        v6 = loadString_sub_40F1D0((char*)& byte_587000[4868], 0, "C:\\NoxPost\\src\\Common\\System\\settings.c", 414);
-        sub_445490(v6);
-    }
-}
-// 40A0D1: variable 'v4' is possibly undefined
 
 //----- (0040A180) --------------------------------------------------------
 unsigned __int8 __cdecl sub_40A180(__int16 a1)
