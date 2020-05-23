c2052528ca0291704c904ae091d4852015b51128
Denys Smirnov <dennwc@pm.me>
2020-05-23 10:27:15 +0300

    Split server network functions.

---
+++ src/server/network/playback.h
@@ -0,0 +1,8 @@
+#ifndef NOX_PORT_SERVER_NETWORK_PLAYBACK
+#define NOX_PORT_SERVER_NETWORK_PLAYBACK
+
+#include "../../defs.h"
+
+int __cdecl sub_4D3580_net_playback(int a1);
+
+#endif // NOX_PORT_SERVER_NETWORK_PLAYBACK
