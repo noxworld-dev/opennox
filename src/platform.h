614e1c372f4e5453ae35024f4bf692551b46fd80
Denys Smirnov <dennwc@pm.me>
2020-05-30 11:52:45 +0300

    Fix end-to-end test prototype.

--- src/platform.h
+++ src/platform.h
@@ -1,9 +1,6 @@
 #ifndef NOX_PLATFORM_H
 #define NOX_PLATFORM_H
 
-//#define NOX_PREDICTABLE
-//#define NOX_E2E_TEST
-
 #include <SDL2/SDL.h>
 
 int nox_rand();
@@ -15,5 +12,17 @@ void nox_sleep(unsigned int ms);
 
 int nox_SDL_PollEvent(SDL_Event* event);
 Uint8 nox_SDL_GetEventState(Uint32 type);
+
+#ifdef NOX_E2E_TEST
+void script_add_event(SDL_Event e);
+void script_wait(unsigned int dt);
+void script_move(int dx, int dy);
+void script_move_to(int x, int y);
+void script_hold(int btn);
+void script_release(int btn);
+void script_click(int btn);
+void time_hook();
+void init_script_events();
+#endif
 
 #endif // NOX_PLATFORM_H
