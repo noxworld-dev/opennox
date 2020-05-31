noxworld-dev/opennox

commit 6cdd52ab2d8931cb5af465064826f270345bf526

Fix and name a few more functions and globals.

src/defs.h
@@ -529,4 +529,11 @@ typedef struct {
     char name[12];
 } obj_5D4594_754088_t;
 
+typedef struct{
+    bool live;
+    BYTE pad[3];
+    int id;
+    _DWORD data8[327];
+} nox_npc;
+
 #endif

src/GAME1.c
@@ -17,6 +17,7 @@ extern void* nox_backbuffer1_pix;
 extern void* nox_backbuffer_pix;
 extern int nox_backbuffer_width;
 extern int nox_backbuffer_height;
+extern int nox_backbuffer_depth;
 extern int nox_backbuffer_width32;
 
 int g_fullscreen_cfg = 0;
@@ -1276,6 +1277,7 @@ mem_mapping mappings[] = {
     {0x5D4594+3798752, (void*)&nox_backbuffer_pix, sizeof(nox_backbuffer_pix),1},
     {0x5D4594+3801784, (void*)&nox_backbuffer_width, sizeof(nox_backbuffer_width),1},
     {0x5D4594+3801788, (void*)&nox_backbuffer_height, sizeof(nox_backbuffer_height),1},
+    {0x5D4594+3799568, (void*)&nox_backbuffer_depth, sizeof(nox_backbuffer_depth),1},
     {0x5D4594+3801800, (void*)&nox_backbuffer_width32, sizeof(nox_backbuffer_width32),1},
     {0x5D4594+3799572, (void*)&ptr_5D4594_3799572, sizeof(ptr_5D4594_3799572),1},
     {0x5D4594+3799660, (void*)&obj_5D4594_3799660, sizeof(obj_5D4594_3799660),1},
@@ -1370,7 +1372,7 @@ void cleanup()
     sub_48B1B0(&g_ddraw);
 #endif
     if (*(_DWORD*)& byte_5D4594[260])
-        sub_40ACA0(*(LPVOID*)& byte_5D4594[260]);
+        sub_40ACA0_free_ptr2(*(LPVOID*)& byte_5D4594[260]);
 }
 
 void mainloop_exit_1()
@@ -3566,8 +3568,9 @@ size_t* __cdecl sub_40ABF0(const char* path, int a2)
 
     v2 = 0;
     v3 = (size_t*)nox_malloc(0x10u);
-    if (!v3)
-        goto LABEL_7;
+    if (!v3) {
+        return 0;
+    }
     v4 = sub_408CC0(path, 0);
     v2 = v4;
     if (!v4
@@ -3579,8 +3582,7 @@ size_t* __cdecl sub_40ABF0(const char* path, int a2)
             (*v3 = (size_t)v5) == 0)
         || (v6 = v3[1], sub_408E40(v5, 1, v3[1], v2) != v6))
     {
-        sub_40ACA0(v3);
-    LABEL_7:
+        sub_40ACA0_free_ptr2(v3);
         v3 = 0;
         goto LABEL_9;
     }
@@ -3594,11 +3596,13 @@ LABEL_9:
 }
 
 //----- (0040ACA0) --------------------------------------------------------
-void __cdecl sub_40ACA0(LPVOID lpMem)
+void __cdecl sub_40ACA0_free_ptr2(void** ptr)
 {
-    if (*(_DWORD*)lpMem)
-        free(*(LPVOID*)lpMem);
-    free(lpMem);
+    if (*ptr) {
+        free(*ptr);
+        *ptr = NULL;
+    }
+    free(ptr);
 }
 
 //----- (0040ACC0) --------------------------------------------------------
@@ -13175,7 +13179,7 @@ size_t* __cdecl sub_415470(char* a1)
                         if (!sub_415700((int)v3, v1))
                         {
                         LABEL_34:
-                            sub_40ACA0(v3);
+                            sub_40ACA0_free_ptr2(v3);
                             return 0;
                         }
                         goto LABEL_35;
@@ -13183,7 +13187,7 @@ size_t* __cdecl sub_415470(char* a1)
                 }
             LABEL_35:
                 if (!*(_DWORD*)& byte_5D4594[260])
-                    sub_40ACA0(v3);
+                    sub_40ACA0_free_ptr2(v3);
                 free(v1);
                 result = (size_t*)1;
             }
@@ -39347,7 +39351,7 @@ int sub_435CC0()
                             result = sub_473680();
                             if (result)
                             {
-                                sub_49A2C0();
+                                nox_alloc_npcs();
                                 result = sub_499360();
                                 if (result)
                                 {
@@ -43017,12 +43021,12 @@ int __cdecl sub_43BF10(int a1)
     }
     sub_430BE0(v1, v2, v3);
     *(_DWORD*)& byte_5D4594[805872] = 0;
-    if (v1 == nox_backbuffer_width && v2 == nox_backbuffer_height && v3 == *(_DWORD*)& byte_5D4594[3799568])
+    if (v1 == nox_backbuffer_width && v2 == nox_backbuffer_height && v3 == nox_backbuffer_depth)
     {
         return 1;
     }
     sub_48BE50(1);
-    if (v3 == *(_DWORD*)& byte_5D4594[3799568] || (result = sub_42F370(*(int*)& byte_5D4594[3804680])) != 0)
+    if (v3 == nox_backbuffer_depth || (result = sub_42F370(*(int*)& byte_5D4594[3804680])) != 0)
     {
         result = sub_430BA0();
         if (result)
@@ -44912,7 +44916,7 @@ int map_download_loop(int first)
     unsigned __int8* v1; // eax
 
     sub_416C70(30);
-    sub_4453A0();
+    sub_4453A0_poll_events();
     sub_4308A0(1);
     sub_46B740();
     if (sub_43AF70() == 1)
@@ -44938,8 +44942,8 @@ int map_download_loop(int first)
     sub_46C2E0();
     sub_477830();
     sub_48A220();
-    sub_4AD170();
-    sub_48A290();
+    sub_4AD170_call_copy_backbuffer();
+    sub_48A290_call_present();
 
     if (!*(_DWORD*)& byte_587000[173328])
         return map_download_finish();
@@ -45117,7 +45121,7 @@ void mainloop()
         sub_40DF90();
     }
     sub_416C70(30);
-    sub_4453A0();
+    sub_4453A0_poll_events();
     sub_413520();
     sub_435770();
     if (!(*(int (**)(void)) & byte_5D4594[816388])())
@@ -45272,8 +45276,8 @@ void mainloop()
         if (!(*(_DWORD*)& byte_5D4594[2650636] & 0x40000) || byte_5D4594[2650637] & 1 || *(_DWORD*)& byte_5D4594[815132])
         {
             sub_48A220();
-            sub_4AD170();
-            sub_48A290();
+            sub_4AD170_call_copy_backbuffer();
+            sub_48A290_call_present();
         }
     }
     sub_435750();
@@ -45323,7 +45327,7 @@ void sub_43E290()
 #endif
 }
 // 43E815: variable 'v18' is possibly undefined
-// 4AD170: using guessed type int sub_4AD170(void);
+// 4AD170: using guessed type int sub_4AD170_call_copy_backbuffer(void);
 
 //----- (0043E8B0) --------------------------------------------------------
 int sub_43E8B0()
@@ -50394,110 +50398,110 @@ BOOL sub_444830()
 #endif
 
 //----- (004449D0) --------------------------------------------------------
-int __cdecl sub_4449D0(HWND a1, int a2, int a3, int a4, int a5)
+int __cdecl sub_4449D0(HWND wnd, int w, int h, int depth, int flags)
 {
     int result; // eax
 
-    result = sub_444AC0(a1, a2, a3, a4, a5);
+    result = sub_444AC0(wnd, w, h, depth, flags);
+    printf("%s: %d\n", __FUNCTION__, result);
+    if (!result)
+        return result;
+
+    result = sub_47D200();
+    printf("%s: %d\n", __FUNCTION__, result);
+    if (!result)
+        return result;
+
+    result = sub_486090();
+    printf("%s: %d\n", __FUNCTION__, result);
+    if (!result)
+        return result;
+
+    result = sub_4B0B30();
+    printf("%s: %d\n", __FUNCTION__, result);
+    if (!result)
+        return result;
+
+    result = sub_4408E0();
+    printf("%s: %d\n", __FUNCTION__, result);
+    if (!result)
+        return result;
+
+    result = sub_49F610();
+    printf("%s: %d\n", __FUNCTION__, result);
+    if (!result)
+        return result;
+
+    result = sub_4338D0();
+    printf("%s: %d\n", __FUNCTION__, result);
+    if (!result)
+        return result;
+
+    result = sub_4AD100();
+    printf("%s: %d\n", __FUNCTION__, result);
+    if (!result)
+        return result;
+
+    result = sub_48B1F0();
+    printf("%s: %d\n", __FUNCTION__, result);
+    if (!result)
+        return result;
+
+    result = sub_44D9A0();
+    printf("%s: %d\n", __FUNCTION__, result);
+    if (!result)
+        return result;
+
+    result = sub_4B0650();
+    printf("%s: %d\n", __FUNCTION__, result);
+    if (!result)
+        return result;
+
+    result = sub_49E3F0();
+    printf("%s: %d\n", __FUNCTION__, result);
+    if (!result)
+        return result;
+
+    result = sub_48C560();
+    printf("%s: %d\n", __FUNCTION__, result);
+    if (!result)
+        return result;
+
+    result = sub_4B02D0();
+    printf("%s: %d\n", __FUNCTION__, result);
+    if (!result)
+        return result;
+
+    result = sub_4AF8D0();
+    printf("%s: %d\n", __FUNCTION__, result);
+    if (!result)
+        return result;
+
+    result = sub_4AEE80();
+    printf("%s: %d\n", __FUNCTION__, result);
+    if (!result)
+        return result;
+
+    result = sub_4AE520();
+    printf("%s: %d\n", __FUNCTION__, result);
+    if (!result)
+        return result;
+
+    result = sub_49CB50();
+    printf("%s: %d\n", __FUNCTION__, result);
+    if (!result)
+        return result;
+
+    result = sub_43F1C0();
+    printf("%s: %d\n", __FUNCTION__, result);
+    if (!result)
+        return result;
+
+    result = sub_4AE400();
     printf("%s: %d\n", __FUNCTION__, result);
     if (result)
-    {
-        result = sub_47D200();
-        printf("%s: %d\n", __FUNCTION__, result);
-        if (result)
-        {
-            result = sub_486090();
-            printf("%s: %d\n", __FUNCTION__, result);
-            if (result)
-            {
-                result = sub_4B0B30();
-                printf("%s: %d\n", __FUNCTION__, result);
-                if (result)
-                {
-                    result = sub_4408E0();
-                    printf("%s: %d\n", __FUNCTION__, result);
-                    if (result)
-                    {
-                        result = sub_49F610();
-                        printf("%s: %d\n", __FUNCTION__, result);
-                        if (result)
-                        {
-                            result = sub_4338D0();
-                            printf("%s: %d\n", __FUNCTION__, result);
-                            if (result)
-                            {
-                                result = sub_4AD100();
-                                printf("%s: %d\n", __FUNCTION__, result);
-                                if (result)
-                                {
-                                    result = sub_48B1F0();
-                                    printf("%s: %d\n", __FUNCTION__, result);
-                                    if (result)
-                                    {
-                                        result = sub_44D9A0();
-                                        printf("%s: %d\n", __FUNCTION__, result);
-                                        if (result)
-                                        {
-                                            result = sub_4B0650();
-                                            printf("%s: %d\n", __FUNCTION__, result);
-                                            if (result)
-                                            {
-                                                result = sub_49E3F0();
-                                                printf("%s: %d\n", __FUNCTION__, result);
-                                                if (result)
-                                                {
-                                                    result = sub_48C560();
-                                                    printf("%s: %d\n", __FUNCTION__, result);
-                                                    if (result)
-                                                    {
-                                                        result = sub_4B02D0();
-                                                        printf("%s: %d\n", __FUNCTION__, result);
-                                                        if (result)
-                                                        {
-                                                            result = sub_4AF8D0();
-                                                            printf("%s: %d\n", __FUNCTION__, result);
-                                                            if (result)
-                                                            {
-                                                                result = sub_4AEE80();
-                                                                printf("%s: %d\n", __FUNCTION__, result);
-                                                                if (result)
-                                                                {
-                                                                    result = sub_4AE520();
-                                                                    printf("%s: %d\n", __FUNCTION__, result);
-                                                                    if (result)
-                                                                    {
-                                                                        result = sub_49CB50();
-                                                                        printf("%s: %d\n", __FUNCTION__, result);
-                                                                        if (result)
-                                                                        {
-                                                                            result = sub_43F1C0();
-                                                                            printf("%s: %d\n", __FUNCTION__, result);
-                                                                            if (result)
-                                                                            {
-                                                                                result = sub_4AE400();
-                                                                                printf("%s: %d\n", __FUNCTION__, result);
-                                                                                if (result)
-                                                                                    result = sub_49F4A0();
-                                                                                printf("%s: %d\n", __FUNCTION__, result);
-                                                                            }
-                                                                        }
-                                                                    }
-                                                                }
-                                                            }
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
+        result = sub_49F4A0();
+    printf("%s: %d\n", __FUNCTION__, result);
     return result;
 }
 
@@ -56120,13 +56124,13 @@ void* __cdecl sub_44C840(char* a1)
     {
         if (sub_40A5C0(1) && *(_DWORD*)& byte_5D4594[2650664])
         {
-            sub_40ACA0(*(LPVOID*)& byte_5D4594[260]);
+            sub_40ACA0_free_ptr2(*(LPVOID*)& byte_5D4594[260]);
             *(_DWORD*)& byte_5D4594[260] = 0;
         }
     }
     else
     {
-        sub_40ACA0(v2);
+        sub_40ACA0_free_ptr2(v2);
     }
     result = nox_malloc(4 * *(_DWORD*)& byte_5D4594[830612]);
     *(_DWORD*)& byte_5D4594[830608] = result;

src/GAME2.c
@@ -11,7 +11,8 @@ extern int default_win_height;
 extern obj_5D4594_3799572_t* ptr_5D4594_3799572;
 extern obj_5D4594_3799572_t obj_5D4594_3800716;
 
-BYTE* npc_array;
+const int nox_max_npcs = 1024;
+nox_npc* npc_array;
 
 FILE* nox_file_log = 0;
 
@@ -13977,7 +13978,7 @@ void sub_460590()
                 if (*(int*)& byte_5D4594[1049712] >= 5)
                 {
                     *(_BYTE*)(*(_DWORD*)& byte_587000[133480] + 200) = 4;
-                    return result;
+                    return;
                 }
             }
             *(_BYTE*)(*(_DWORD*)& byte_587000[133480] + 200) = result;
@@ -31456,9 +31457,7 @@ char* sub_477830()
         if (!v1 || v1[2251])
         {
             v2 = sub_424A90(*(int*)& byte_5D4594[1097192]);
-            if (v2)
-            {
-            LABEL_11:
+            if (v2) {
                 sub_47D2C0(v2, v0->field_0 - 15, v0->field_4 - 15);
                 goto LABEL_12;
             }
@@ -31466,8 +31465,10 @@ char* sub_477830()
         else
         {
             v2 = sub_425310(*(int*)& byte_5D4594[1097192], 0);
-            if (v2)
-                goto LABEL_11;
+            if (v2) {
+                sub_47D2C0(v2, v0->field_0 - 15, v0->field_4 - 15);
+                goto LABEL_12;
+            }
         }
     }
 LABEL_12:
@@ -36658,66 +36659,50 @@ int __stdcall sub_47FCE0(_DWORD* a1, int a2)
 //----- (0047FD70) --------------------------------------------------------
 int sub_47FD70()
 {
-    int v0; // esi
     int v1; // edi
     int v2; // ebx
-    wchar_t* v4; // eax
-    int result; // eax
-    int v6; // [esp-10h] [ebp-1Ch]
-    int v7; // [esp-Ch] [ebp-18h]
-    int v8; // [esp-8h] [ebp-14h]
-    wchar_t* v9; // [esp-8h] [ebp-14h]
 
-    v0 = 0;
-    if (*(_DWORD*)& byte_587000[80848])
-    {
+    int flags = 0;
+    if (*(_DWORD*)& byte_587000[80848]) {
         if (*(_DWORD*)& byte_5D4594[805868])
-            v0 = 32;
-    }
-    else
-    {
-        v0 = 16;
+            flags = 0x20;
+    } else {
+        flags = 0x10;
     }
     if (*(_DWORD*)& byte_5D4594[805860])
-        v0 |= 0x18u;
+        flags |= 0x18u;
     if (*(_DWORD*)& byte_5D4594[2650636] & 0x2000000 && *(_DWORD*)& byte_587000[80848])
-        v0 &= 0xFFFFFFDF;
+        flags &= 0xFFFFFFDF;
     if (!*(_DWORD*)& byte_587000[80804])
-        v0 |= 0x100u;
+        flags |= 0x100u;
     if (*(_DWORD*)& byte_5D4594[805864])
-        v0 |= 0x200u;
+        flags |= 0x200u;
     sub_48A210(sub_47FCE0);
     v1 = sub_477620();
     sub_477610(0);
     v2 = sub_48B3E0(0);
-    v8 = *(_DWORD*)& byte_5D4594[3804680] != 0 ? 16 : 8;
-    v7 = nox_win_height;
-    v6 = nox_win_width;
-    if (sub_444930(getWindowHandle_sub_401FD0(), v6, v7, v8, v0))
+    int depth = *(_DWORD*)& byte_5D4594[3804680] != 0 ? 16 : 8;
+    if (!sub_444930(getWindowHandle_sub_401FD0(), nox_win_width, nox_win_height, depth, flags))
     {
-        printf("%s: %d\n", __FUNCTION__, 1);
-        sub_477710();
-        sub_477610(v1);
-        sub_48B3E0(v2);
-        sub_440900();
-        sub_47FEF0();
-        sub_49F6D0(1);
-        sub_437290();
-        *(_DWORD*)& byte_5D4594[3804680] = *(_DWORD*)& byte_5D4594[3801780];
-        *(_DWORD*)& byte_5D4594[3805488] = *(_DWORD*)& byte_5D4594[3801808] * nox_backbuffer_height;
-        *(_DWORD*)& byte_5D4594[3807124] = *(_DWORD*)& byte_5D4594[3801780] == 1;
-        sub_430B50(0, 0, 639, 479);
-        sub_4453A0();
-        result = 1;
-    }
-    else
-    {
-        v9 = loadString_sub_40F1D0("result:ERROR", 0, "C:\\NoxPost\\src\\Client\\Io\\Win95\\dxvideo.c", 213);
-        v4 = loadString_sub_40F1D0("gfxDdraw.c:DXWarning", 0, "C:\\NoxPost\\src\\Client\\Io\\Win95\\dxvideo.c", 213);
+        wchar_t* v9 = loadString_sub_40F1D0("result:ERROR", 0, "C:\\NoxPost\\src\\Client\\Io\\Win95\\dxvideo.c", 213);
+        wchar_t* v4 = loadString_sub_40F1D0("gfxDdraw.c:DXWarning", 0, "C:\\NoxPost\\src\\Client\\Io\\Win95\\dxvideo.c", 213);
         nullsub_4(0, v4, v9, 1);
-        result = 0;
+        return 0;
     }
-    return result;
+    printf("%s: %d\n", __FUNCTION__, 1);
+    sub_477710();
+    sub_477610(v1);
+    sub_48B3E0(v2);
+    sub_440900();
+    sub_47FEF0();
+    sub_49F6D0(1);
+    sub_437290();
+    *(_DWORD*)& byte_5D4594[3804680] = *(_DWORD*)& byte_5D4594[3801780];
+    *(_DWORD*)& byte_5D4594[3805488] = *(_DWORD*)& byte_5D4594[3801808] * nox_backbuffer_height;
+    *(_DWORD*)& byte_5D4594[3807124] = *(_DWORD*)& byte_5D4594[3801780] == 1;
+    sub_430B50(0, 0, default_win_width-1, default_win_height-1);
+    sub_4453A0_poll_events();
+    return 1;
 }
 // 4514E0: using guessed type void __cdecl nullsub_4(_DWORD, _DWORD, _DWORD, _DWORD);
 
@@ -36839,7 +36824,7 @@ int sub_4800F0()
         *(_DWORD*)& byte_5D4594[3805488] = *(_DWORD*)& byte_5D4594[3801808] * nox_backbuffer_height;
         *(_DWORD*)& byte_5D4594[3807124] = *(_DWORD*)& byte_5D4594[3801780] == 1;
         sub_430B50(0, 0, 639, 479);
-        sub_4453A0();
+        sub_4453A0_poll_events();
         result = 1;
     }
     else
@@ -49242,14 +49227,14 @@ int __cdecl sub_48EA70(int a1, unsigned int a2, int a3)
                 v285 &= 0x7FFFu;
                 *(_WORD*)(v3 + 1) = v285;
                 v287 = v286 >> 15;
-                v288 = sub_49A340(v285);
+                v288 = nox_npc_by_id(v285);
                 if (v288)
                 {
-                    LODWORD(v5) = sub_49A380(v288, *(unsigned __int16*)(v3 + 1));
+                    LODWORD(v5) = nox_init_npc(v288, *(unsigned __int16 *) (v3 + 1));
                 }
                 else
                 {
-                    LODWORD(v5) = sub_49A300(*(unsigned __int16*)(v3 + 1));
+                    LODWORD(v5) = nox_new_npc(*(unsigned __int16*)(v3 + 1));
                     v288 = (char*)v5;
                 }
                 if (v288)
@@ -50924,7 +50909,7 @@ int __cdecl sub_48EA70(int a1, unsigned int a2, int a3)
             {
                 if (sub_4951F0(v138))
                     sub_4950F0(v138, *(_BYTE*)(v3 + 3));
-                sub_49A3B0(v138, *(unsigned __int8*)(v3 + 3));
+                nox_npc_set_328(v138, *(unsigned __int8*)(v3 + 3));
                 v3 += 4;
             }
             else
@@ -55951,133 +55936,113 @@ int __cdecl sub_49A150(int2* a1, int a2, unsigned __int8 a3)
 }
 
 //----- (0049A2C0) --------------------------------------------------------
-int sub_49A2C0()
+void nox_alloc_npcs()
 {
-    int result; // eax
-
-    result = 0;
-    npc_array = nox_calloc(1024, 1316);
-    return result;
+    npc_array = nox_calloc(nox_max_npcs, sizeof(nox_npc));
 }
 
 //----- (0049A300) --------------------------------------------------------
-unsigned __int8* __cdecl sub_49A300(int a1)
+nox_npc* __cdecl nox_new_npc(int id)
 {
-    int v1; // ecx
-    unsigned __int8* v2; // eax
-
-    v1 = 0;
-    v2 = npc_array;
-    while (*v2)
+    int n = 0;
+    nox_npc* cur = npc_array;
+    while (cur->live)
     {
-        v2 += 1316;
-        ++v1;
-        if (v1 >= 1024)
+        cur++;
+        n++;
+        if (n >= nox_max_npcs)
             return 0;
     }
-    sub_49A380(v2, a1);
-    return v2;
+    nox_init_npc(cur, id);
+    return cur;
 }
 
 //----- (0049A340) --------------------------------------------------------
-char* __cdecl sub_49A340(int a1)
+nox_npc* __cdecl nox_npc_by_id(int id)
 {
-    int v1; // ecx
-    unsigned __int8* v2; // eax
-
-    v1 = 0;
-    v2 = npc_array;
-    while (*((_DWORD*)v2 + 1) != a1 || *v2 != 1)
+    int n = 0;
+    nox_npc* cur = npc_array;
+    while (cur->id != id || !cur->live)
     {
-        v2 += 1316;
-        ++v1;
-        if (v1 >= 1024)
+        cur++;
+        n++;
+        if (n >= nox_max_npcs)
             return 0;
     }
-    return v2;
+    return cur;
 }
 
 //----- (0049A380) --------------------------------------------------------
-int __cdecl sub_49A380(_DWORD* a1, int a2)
+int __cdecl nox_init_npc(nox_npc* ptr, int id)
 {
-    int result; // eax
-
-    memset(a1, 0, 0x524u);
-    result = a2;
-    *(_BYTE*)a1 = 1;
-    a1[1] = a2;
-    return result;
+    memset(ptr, 0, 524); // TODO: why only part of it?
+    ptr->live = 1;
+    ptr->id = id;
+    return id;
 }
 
 //----- (0049A3B0) --------------------------------------------------------
-char* __cdecl sub_49A3B0(int a1, int a2)
+nox_npc* __cdecl nox_npc_set_328(int id, int a2)
 {
-    char* result; // eax
-
-    result = sub_49A340(a1);
-    if (result)
-        * ((_DWORD*)result + 328) = a2;
-    return result;
+    nox_npc* p = nox_npc_by_id(id);
+    if (p)
+        p->data8[326] = a2;
+    return p;
 }
 
 //----- (0049A3D0) --------------------------------------------------------
 char* __cdecl sub_49A3D0(char a1, int a2, int a3, int a4)
 {
-    char* result; // eax
-    int v5; // ecx
+    char* npc; // eax
     _DWORD* k; // edx
     char* v7; // ecx
     char** v8; // edi
     int l; // esi
-    int v10; // ecx
     _DWORD* i; // edx
     char* v12; // ecx
     char** v13; // edi
     int j; // esi
 
-    result = sub_49A340(a2);
-    if (result)
+    npc = nox_npc_by_id(a2);
+    if (!npc)
+        return 0;
+    if (a1 == 81 || a1 == 80)
     {
-        if (a1 == 81 || a1 == 80)
+        int v10 = 0;
+        for (i = npc + 32; *i; i += 6)
         {
-            v10 = 0;
-            for (i = result + 32; *i; i += 6)
-            {
-                if (++v10 >= 27)
-                    return result;
-            }
-            v12 = &result[24 * v10];
-            *((_DWORD*)v12 + 8) = a3;
-            v13 = (char**)(v12 + 36);
-            *((_DWORD*)result + 326) |= a3;
-            for (j = 0; j < 4; ++j)
-            {
-                result = (char*)sub_413330(*(unsigned __int8*)(j + a4));
-                *v13 = result;
-                ++v13;
-            }
+            if (++v10 >= 27)
+                return npc;
         }
-        else
+        v12 = &npc[24 * v10];
+        *((_DWORD*)v12 + 8) = a3;
+        v13 = (char**)(v12 + 36);
+        *((_DWORD*)npc + 326) |= a3;
+        for (j = 0; j < 4; ++j)
         {
-            v5 = 0;
-            for (k = result + 680; *k; k += 6)
-            {
-                if (++v5 >= 26)
-                    return result;
-            }
-            v7 = &result[24 * v5];
-            *((_DWORD*)v7 + 170) = a3;
-            v8 = (char**)(v7 + 684);
-            *((_DWORD*)result + 327) |= a3;
-            for (l = 0; l < 4; ++l)
-            {
-                result = (char*)sub_413330(*(unsigned __int8*)(l + a4));
-                *v8 = result;
-                ++v8;
-            }
+            npc = (char*)sub_413330(*(unsigned __int8*)(j + a4));
+            *v13 = npc;
+            ++v13;
+        }
+    } else {
+        int v5 = 0;
+        for (k = npc + 680; *k; k += 6)
+        {
+            if (++v5 >= 26)
+                return npc;
+        }
+        v7 = &npc[24 * v5];
+        *((_DWORD*)v7 + 170) = a3;
+        v8 = (char**)(v7 + 684);
+        *((_DWORD*)npc + 327) |= a3;
+        for (l = 0; l < 4; ++l)
+        {
+            npc = (char*)sub_413330(*(unsigned __int8*)(l + a4));
+            *v8 = npc;
+            ++v8;
         }
     }
-    return result;
+    return npc;
 }
 
 //----- (0049A4B0) --------------------------------------------------------

commit 2315204b0cc1d7d8c0be21830b699899b5614b16

Initialize nox_npc instances to 0 completely This was accidentally broken in commit 6cdd52ab2d8931cb5af465064826f270345bf526

src/GAME2_3.c
@@ -5893,7 +5893,7 @@ nox_npc* __cdecl nox_npc_by_id(int id)
 //----- (0049A380) --------------------------------------------------------
 int __cdecl nox_init_npc(nox_npc* ptr, int id)
 {
-    memset(ptr, 0, 524); // TODO: why only part of it?
+    memset(ptr, 0, sizeof(nox_npc));
     ptr->live = 1;
     ptr->id = id;
     return id;

src/defs.h
@@ -780,5 +780,6 @@ typedef struct{
     int id;
     _DWORD data8[327];
 } nox_npc;
+_Static_assert( sizeof( nox_npc ) == 1316, "struct nox_npc size is wrong" );
 
 #endif