d116ff8bde3407c90418ab74811907cac0c8865c
Denys Smirnov <dennwc@pm.me>
2020-06-04 17:46:54 +0300

    Reconstruct variable dword_5d4594_1316972.

--- src/client/gui/servopts/advserv.c
+++ src/client/gui/servopts/advserv.c
@@ -1,5 +1,6 @@
 #include "advserv.h"
 #include "../../../proto.h"
+extern _DWORD dword_5d4594_1316972;
 
 //----- (004BE2C0) --------------------------------------------------------
 int __cdecl sub_4BE2C0(int a1) {
@@ -10,6 +11,6 @@ int __cdecl sub_4BE2C0(int a1) {
 	v1 = loadString_sub_40F1D0((char*)&byte_587000[180448], 0, "C:\\NoxPost\\src\\client\\Gui\\ServOpts\\advserv.c",
 				   71);
 	nox_swprintf((wchar_t*)&byte_5D4594[1316716], v1, a1);
-	v2 = sub_46B0C0(*(_DWORD**)&byte_5D4594[1316972], 2120);
+	v2 = sub_46B0C0(*(_DWORD**)&dword_5d4594_1316972, 2120);
 	return nox_window_call_field_94((int)v2, 16385, (int)&byte_5D4594[1316716], -1);
 }
