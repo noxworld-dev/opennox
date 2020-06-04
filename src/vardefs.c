885d8720d6e69118869b4978ba859560a2331079
Denys Smirnov <dennwc@pm.me>
2020-06-04 11:32:02 +0300

    Prepare to move variables.

--- src/vardefs.c
+++ src/vardefs.c
@@ -1,1 +1,34 @@
+#include "client/shell/wolapi/wolchat.h"
+#include "client/system/parsecmd.h"
+#include "common/object/modifier.h"
+#include "server/object/die/die.h"
+#include "server/object/pickdrop/pickup.h"
+#include "server/system/cscrfunc.h"
+
+#include "client/gui/gamewin/psscript.h"
+#include "common/wolapi/wolstate.h"
+
+#include "client/drawable/update/dball.h"
+#include "client/drawable/update/drainup.h"
+#include "client/drawable/update/healup.h"
+#include "client/drawable/update/manabomb.h"
+#include "client/drawable/update/mmislup.h"
+#include "client/drawable/update/mtailup.h"
+#include "client/drawable/update/sparklup.h"
+#include "client/drawable/update/telwake.h"
+#include "client/drawable/update/vortexup.h"
+
 #include "proto.h"
+
+void sub_57F634();
+void nullsub_45();
+void nullsub_46();
+void nullsub_47();
+void nullsub_48();
+void nullsub_62();
+void nullsub_65();
+void nullsub_66();
+void nullsub_67();
+void nullsub_68();
+void nullsub_69();
+void nullsub_70();
