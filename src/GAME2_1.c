88308b114caba5a26005df262054b3eb2753945c
Denys Smirnov <dennwc@pm.me>
2020-06-04 17:48:33 +0300

    Reconstruct variable dword_5d4594_2650676.

--- src/GAME2_1.c
+++ src/GAME2_1.c
@@ -25,6 +25,7 @@
 
 #include "proto.h"
 
+extern _DWORD dword_5d4594_2650676;
 extern _DWORD dword_5d4594_1096516;
 extern _DWORD dword_587000_154952;
 extern _DWORD dword_5d4594_1062528;
@@ -3401,7 +3402,7 @@ char* __cdecl sub_469920(_DWORD* a1) {
 
 	if (nox_common_engineFlags & 0x800)
 		return (char*)&byte_587000[142336];
-	v1 = *a1 - *(_DWORD*)&byte_5D4594[2650676];
+	v1 = *a1 - dword_5d4594_2650676;
 	v2 = a1[1] - *(_DWORD*)&byte_5D4594[2650680];
 	v3 = v1 / 23;
 	v4 = v2 / 23;
