3c04c7a2eb3ee20bdde48d1143a06627b52f8778
Denys Smirnov <dennwc@pm.me>
2020-06-05 16:25:51 +0300

    Reconstruct variable dword_5d4594_741364.

--- src/GAME1_2.c
+++ src/GAME1_2.c
@@ -21,6 +21,7 @@
 
 #include "proto.h"
 
+extern _DWORD dword_5d4594_741364;
 extern _DWORD dword_5d4594_787212;
 extern _DWORD dword_5d4594_811104;
 extern _DWORD dword_5d4594_806052;
@@ -1293,15 +1294,15 @@ int __cdecl nox_server_mapRWWallMap_429B20(_DWORD* a1) {
 			*(_DWORD*)&byte_5D4594[741356] = v33.field_8 / 23;
 			*(_DWORD*)&byte_5D4594[741368] = v33.field_4 / 23;
 			v5 = v33.field_C / 23;
-			*(_DWORD*)&byte_5D4594[741364] = v5;
+			dword_5d4594_741364 = v5;
 		} else {
 			*(_DWORD*)&byte_5D4594[741368] = 256;
 			*(_DWORD*)&byte_5D4594[741360] = 256;
-			*(_DWORD*)&byte_5D4594[741364] = 0;
+			dword_5d4594_741364 = 0;
 			*(_DWORD*)&byte_5D4594[741356] = 0;
 			sub_410640((void(__cdecl*)(int, int))sub_42A0F0, 0);
 			v3 = *(_DWORD*)&byte_5D4594[741356];
-			v5 = *(_DWORD*)&byte_5D4594[741364];
+			v5 = dword_5d4594_741364;
 		}
 		v25 = v3 - *(_DWORD*)&byte_5D4594[741360] + 1;
 		v27 = v5 - *(_DWORD*)&byte_5D4594[741368] + 1;
@@ -1446,8 +1447,8 @@ int __cdecl sub_42A0F0(int a1) {
 	if ((int)*(unsigned __int8*)(a1 + 6) < *(int*)&byte_5D4594[741368])
 		*(_DWORD*)&byte_5D4594[741368] = *(unsigned __int8*)(a1 + 6);
 	result = *(unsigned __int8*)(a1 + 6);
-	if (result > *(int*)&byte_5D4594[741364])
-		*(_DWORD*)&byte_5D4594[741364] = *(unsigned __int8*)(a1 + 6);
+	if (result > *(int*)&dword_5d4594_741364)
+		dword_5d4594_741364 = *(unsigned __int8*)(a1 + 6);
 	return result;
 }
 
@@ -1497,15 +1498,15 @@ int __cdecl sub_42A150(__int16 a1, _DWORD* a2) {
 			*(_DWORD*)&byte_5D4594[741356] = v33.field_8 / 23;
 			*(_DWORD*)&byte_5D4594[741368] = v33.field_4 / 23;
 			v6 = v33.field_C / 23;
-			*(_DWORD*)&byte_5D4594[741364] = v33.field_C / 23;
+			dword_5d4594_741364 = v33.field_C / 23;
 		} else {
 			*(_DWORD*)&byte_5D4594[741368] = 256;
 			*(_DWORD*)&byte_5D4594[741360] = 256;
-			*(_DWORD*)&byte_5D4594[741364] = 0;
+			dword_5d4594_741364 = 0;
 			*(_DWORD*)&byte_5D4594[741356] = 0;
 			sub_410640((void(__cdecl*)(int, int))sub_42A0F0, 0);
 			v5 = *(_DWORD*)&byte_5D4594[741356];
-			v6 = *(_DWORD*)&byte_5D4594[741364];
+			v6 = dword_5d4594_741364;
 		}
 		v28 = v5 - *(_DWORD*)&byte_5D4594[741360] + 1;
 		v29 = v6 - *(_DWORD*)&byte_5D4594[741368] + 1;
