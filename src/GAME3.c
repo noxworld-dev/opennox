42f21bfc99141e7689b9c035c9486993d5228aba
Denys Smirnov <dennwc@pm.me>
2020-06-05 16:17:34 +0300

    Reconstruct variable dword_5d4594_1307768.

--- src/GAME3.c
+++ src/GAME3.c
@@ -25,6 +25,7 @@
 
 #include "proto.h"
 
+extern _DWORD dword_5d4594_1307768;
 extern _DWORD dword_5d4594_1313792;
 extern _DWORD dword_5d4594_1307744;
 extern _DWORD dword_587000_122852;
@@ -1722,7 +1723,7 @@ int sub_4A5C70() {
 		DeleteFileA((LPCSTR)(dword_5d4594_1307780 + 1278 * *(_DWORD*)&byte_5D4594[1307772] + 4));
 	nox_window_call_field_94(*(int*)&dword_5d4594_1307744, 16399, 0, 0);
 	nox_window_call_field_94(*(int*)&dword_5d4594_1307776, 16399, 0, 0);
-	nox_window_call_field_94(*(int*)&byte_5D4594[1307768], 16399, 0, 0);
+	nox_window_call_field_94(*(int*)&dword_5d4594_1307768, 16399, 0, 0);
 	return sub_4A5150();
 }
 
