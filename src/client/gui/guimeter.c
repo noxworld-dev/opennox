157c83799808c3841a3353428fb1d4d8519f386c
Denys Smirnov <dennwc@pm.me>
2020-06-05 16:26:19 +0300

    Reconstruct variable dword_5d4594_1093156.

--- src/client/gui/guimeter.c
+++ src/client/gui/guimeter.c
@@ -4,6 +4,7 @@
 #include "gamewin/gamewin.h"
 #include "tooltip.h"
 
+extern _DWORD dword_5d4594_1093156;
 extern _DWORD dword_5d4594_1096252;
 extern _DWORD dword_5d4594_1093136;
 extern _DWORD dword_5d4594_3799468;
@@ -61,16 +62,16 @@ int __cdecl sub_471160(int a1, int a2, int a3, int a4, int a5) {
 	int result;  // eax
 
 	dword_5d4594_1093136 = nox_window_new(a1, 1032, a2, a3, a4, a5, 0);
-	*(_DWORD*)&byte_5D4594[1093156] = nox_window_new(a1, 1032, a2 - 17, a3 - 15, 15, 15, 0);
+	dword_5d4594_1093156 = nox_window_new(a1, 1032, a2 - 17, a3 - 15, 15, 15, 0);
 	nox_window_set_all_funcs(*(_DWORD**)&dword_5d4594_1093136, 0, sub_471250, 0);
 	v5 = loadString_sub_40F1D0((char*)&byte_587000[148504], 0, "C:\\NoxPost\\src\\Client\\Gui\\guimeter.c", 921);
 	sub_46B000((wchar_t*)(dword_5d4594_1093136 + 36), v5);
-	nox_window_set_all_funcs(*(_DWORD**)&byte_5D4594[1093156], 0, sub_471450, 0);
+	nox_window_set_all_funcs(*(_DWORD**)&dword_5d4594_1093156, 0, sub_471450, 0);
 	v6 = loadString_sub_40F1D0((char*)&byte_587000[148560], 0, "C:\\NoxPost\\src\\Client\\Gui\\guimeter.c", 925);
-	sub_46B000((wchar_t*)(*(_DWORD*)&byte_5D4594[1093156] + 36), v6);
+	sub_46B000((wchar_t*)(dword_5d4594_1093156 + 36), v6);
 	result = dword_5d4594_1093136;
 	*(_DWORD*)(dword_5d4594_1093136 + 32) = 5;
-	*(_DWORD*)(*(_DWORD*)&byte_5D4594[1093156] + 32) = 6;
+	*(_DWORD*)(dword_5d4594_1093156 + 32) = 6;
 	return result;
 }
 
--- src/memmap.c
+++ src/memmap.c
@@ -764,6 +764,7 @@ extern _DWORD dword_5d4594_2386920;
 extern _DWORD dword_5d4594_1062452;
 extern _DWORD dword_5d4594_1046576;
 extern _DWORD dword_5d4594_2386564;
+extern _DWORD dword_5d4594_1093156;
 
 typedef struct mem_mapping {
 	uintptr_t base;
@@ -774,6 +775,7 @@ typedef struct mem_mapping {
 
 mem_mapping mappings[] = {
     // overrides
+	{0x5D4594+1093156, (void*)&dword_5d4594_1093156, sizeof(dword_5d4594_1093156), 1},
 	{0x5D4594+2386564, (void*)&dword_5d4594_2386564, sizeof(dword_5d4594_2386564), 1},
 	{0x5D4594+1046576, (void*)&dword_5d4594_1046576, sizeof(dword_5d4594_1046576), 1},
 	{0x5D4594+1062452, (void*)&dword_5d4594_1062452, sizeof(dword_5d4594_1062452), 1},
