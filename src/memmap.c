887605e78b648da457931b4a668d4cce8fa49f3b
Denys Smirnov <dennwc@pm.me>
2020-06-05 16:21:30 +0300

    Reconstruct variable dword_5d4594_832492.

--- src/memmap.c
+++ src/memmap.c
@@ -601,6 +601,7 @@ extern _DWORD dword_5d4594_588084;
 extern _DWORD dword_5d4594_2487848;
 extern _DWORD dword_5d4594_1321044;
 extern _DWORD dword_5d4594_816372;
+extern _DWORD dword_5d4594_832492;
 
 typedef struct mem_mapping {
 	uintptr_t base;
@@ -611,6 +612,7 @@ typedef struct mem_mapping {
 
 mem_mapping mappings[] = {
     // overrides
+	{0x5D4594+832492, (void*)&dword_5d4594_832492, sizeof(dword_5d4594_832492), 1},
 	{0x5D4594+816372, (void*)&dword_5d4594_816372, sizeof(dword_5d4594_816372), 1},
 	{0x5D4594+1321044, (void*)&dword_5d4594_1321044, sizeof(dword_5d4594_1321044), 1},
 	{0x5D4594+2487848, (void*)&dword_5d4594_2487848, sizeof(dword_5d4594_2487848), 1},
