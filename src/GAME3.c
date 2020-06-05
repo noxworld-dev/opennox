6273aa42e49ebcf67f93163d29e8bd1e096b7d26
Denys Smirnov <dennwc@pm.me>
2020-06-05 16:37:36 +0300

    Reconstruct variable dword_5d4594_1309764.

--- src/GAME3.c
+++ src/GAME3.c
@@ -25,6 +25,7 @@
 
 #include "proto.h"
 
+extern _DWORD dword_5d4594_1309764;
 extern _DWORD dword_5d4594_1308128;
 extern _DWORD dword_5d4594_1308124;
 extern _DWORD dword_5d4594_1309768;
@@ -4928,7 +4929,7 @@ LPCSTR sub_4AB580() {
 		fclose(nox_file_9);
 	result = *(LPCSTR*)&dword_5d4594_1309776;
 	nox_file_9 = 0;
-	*(_DWORD*)&byte_5D4594[1309764] = 0;
+	dword_5d4594_1309764 = 0;
 	dword_5d4594_1309784 = 0;
 	dword_5d4594_1309780 = 0;
 	*(_DWORD*)&byte_5D4594[1309772] = 0;
@@ -5003,7 +5004,7 @@ int sub_4AB720() {
 	_unlink(*(LPCSTR*)&dword_5d4594_1309776);
 	v0 = dword_5d4594_1309780;
 	nox_file_9 = 0;
-	*(_DWORD*)&byte_5D4594[1309764] = 0;
+	dword_5d4594_1309764 = 0;
 	dword_587000_173336 = 1;
 	*(_DWORD*)&byte_5D4594[1309772] = 0;
 	dword_5d4594_1309768 = 0;
@@ -5080,9 +5081,9 @@ void __cdecl sub_4AB7C0(unsigned __int16 a1, void* a2, size_t a3) {
 				v5 = (unsigned __int16*)*((_DWORD*)v5 + 3);
 			} while (v5);
 		}
-		// fprintf(stderr, "foo %d %d %d\n", *(_DWORD *)&byte_5D4594[1309764], *(_DWORD *)&dword_5d4594_1309768,
+		// fprintf(stderr, "foo %d %d %d\n", *(_DWORD *)&dword_5d4594_1309764, *(_DWORD *)&dword_5d4594_1309768,
 		// *(_DWORD *)&byte_5D4594[1309772]);
-		if (*(_DWORD*)&byte_5D4594[1309764] &&
+		if (dword_5d4594_1309764 &&
 		    *(unsigned int*)&dword_5d4594_1309768 >= *(unsigned int*)&byte_5D4594[1309772]) {
 			sub_4AB580();
 			sub_4AB560(0);
@@ -5119,8 +5120,8 @@ int __cdecl sub_4AB9B0(char* a1) {
 LPCSTR sub_4ABA90() {
 	LPCSTR result; // eax
 
-	result = *(LPCSTR*)&byte_5D4594[1309764];
-	if (*(_DWORD*)&byte_5D4594[1309764]) {
+	result = *(LPCSTR*)&dword_5d4594_1309764;
+	if (dword_5d4594_1309764) {
 		sub_43CAB0();
 		if (nox_file_9)
 			fclose(nox_file_9);
@@ -5146,7 +5147,7 @@ int __cdecl sub_4ABAD0(char* a1, unsigned int a2) {
 
 	if (!a1 || a2 <= 0)
 		return 0;
-	*(_DWORD*)&byte_5D4594[1309764] = 1;
+	dword_5d4594_1309764 = 1;
 	dword_587000_173336 = 1;
 	*(_DWORD*)&byte_5D4594[1309772] = a2;
 	dword_5d4594_1309768 = 0;
