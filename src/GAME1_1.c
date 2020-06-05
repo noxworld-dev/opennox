e58e6c34d2d1b53c5db05b31d63b173ec32cb40c
Denys Smirnov <dennwc@pm.me>
2020-06-05 16:26:43 +0300

    Reconstruct variable dword_5d4594_741248.

--- src/GAME1_1.c
+++ src/GAME1_1.c
@@ -21,6 +21,7 @@
 FILE* nox_file_2 = 0;
 FILE* nox_file_3 = 0;
 
+extern _DWORD dword_5d4594_741248;
 extern _DWORD dword_5d4594_529336;
 extern _DWORD dword_5d4594_2660032;
 extern _DWORD dword_5d4594_531656;
@@ -10229,15 +10230,15 @@ int __cdecl sub_4278B0(float* a1, float* a2, float* a3) {
 	double v4; // st7
 
 	*(float*)&byte_5D4594[741244] = a1[2] - *a1;
-	*(float*)&byte_5D4594[741248] = a1[3] - a1[1];
+	*(float*)&dword_5d4594_741248 = a1[3] - a1[1];
 	*(float*)&byte_5D4594[741252] = a2[2] - *a2;
 	*(float*)&byte_5D4594[741256] = a2[3] - a2[1];
 	*(float*)&byte_5D4594[741260] = *a2 - *a1;
 	v3 = a2[1] - a1[1];
 	*(float*)&byte_5D4594[741264] = v3;
 	*(float*)&dword_5d4594_741292 =
-	    v3 * *(float*)&byte_5D4594[741244] - *(float*)&byte_5D4594[741260] * *(float*)&byte_5D4594[741248];
-	v4 = *(float*)&byte_5D4594[741252] * *(float*)&byte_5D4594[741248] -
+	    v3 * *(float*)&byte_5D4594[741244] - *(float*)&byte_5D4594[741260] * *(float*)&dword_5d4594_741248;
+	v4 = *(float*)&byte_5D4594[741252] * *(float*)&dword_5d4594_741248 -
 	     *(float*)&byte_5D4594[741256] * *(float*)&byte_5D4594[741244];
 	*(float*)&dword_5d4594_741284 = v4;
 	if (v4 == 0.0)
@@ -10299,15 +10300,15 @@ int __cdecl sub_427980(float4* a1, float4* a2) {
 	if (a1->field_4 == a1->field_C && a2->field_4 == a2->field_C)
 		return 1;
 	*(float*)&byte_5D4594[741244] = a1->field_8 - a1->field_0;
-	*(float*)&byte_5D4594[741248] = a1->field_C - a1->field_4;
+	*(float*)&dword_5d4594_741248 = a1->field_C - a1->field_4;
 	*(float*)&byte_5D4594[741252] = a2->field_8 - a2->field_0;
 	*(float*)&byte_5D4594[741256] = a2->field_C - a2->field_4;
 	*(float*)&byte_5D4594[741260] = a2->field_0 - a1->field_0;
 	v7 = a2->field_4 - a1->field_4;
 	*(float*)&byte_5D4594[741264] = v7;
 	*(float*)&dword_5d4594_741292 =
-	    v7 * *(float*)&byte_5D4594[741244] - *(float*)&byte_5D4594[741260] * *(float*)&byte_5D4594[741248];
-	*(float*)&dword_5d4594_741284 = *(float*)&byte_5D4594[741252] * *(float*)&byte_5D4594[741248] -
+	    v7 * *(float*)&byte_5D4594[741244] - *(float*)&byte_5D4594[741260] * *(float*)&dword_5d4594_741248;
+	*(float*)&dword_5d4594_741284 = *(float*)&byte_5D4594[741252] * *(float*)&dword_5d4594_741248 -
 					*(float*)&byte_5D4594[741256] * *(float*)&byte_5D4594[741244];
 	if (*(float*)&dword_5d4594_741292 == 0.0 || *(float*)&dword_5d4594_741284 == 0.0 ||
 	    *(float*)&dword_5d4594_741292 < 0.0 && *(float*)&dword_5d4594_741284 > 0.0) {
