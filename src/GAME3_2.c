7e90d74afb873fd65d89c67e8845bfcb411d2f12
Denys Smirnov <dennwc@pm.me>
2020-06-05 16:38:11 +0300

    Reconstruct variable dword_5d4594_1523044.

--- src/GAME3_2.c
+++ src/GAME3_2.c
@@ -19,6 +19,7 @@
 
 #include "proto.h"
 
+extern _DWORD dword_5d4594_1523044;
 extern _DWORD dword_5d4594_1523032;
 extern _DWORD dword_5d4594_1563088;
 extern _DWORD dword_5d4594_1556848;
@@ -409,7 +410,7 @@ int __cdecl sub_4CEBA0(int a1, char* a2) {
 	dword_5d4594_1523032 = sub_46B0C0(*(_DWORD**)&dword_5d4594_1523024, 10171);
 	dword_5d4594_1523036 = sub_46B0C0(*(_DWORD**)&dword_5d4594_1523024, 10172);
 	*(_DWORD*)&byte_5D4594[1523040] = sub_46B0C0(*(_DWORD**)&dword_5d4594_1523024, 10173);
-	*(_DWORD*)&byte_5D4594[1523044] = sub_46B0C0(*(_DWORD**)&dword_5d4594_1523024, 10174);
+	dword_5d4594_1523044 = sub_46B0C0(*(_DWORD**)&dword_5d4594_1523024, 10174);
 	*(_DWORD*)&byte_5D4594[1523048] = sub_46B0C0(*(_DWORD**)&dword_5d4594_1523024, 10175);
 	v2 = sub_46B0C0(*(_DWORD**)&dword_5d4594_1523024, 10176);
 	sub_46B340((int)v2, sub_4CEED0);
@@ -487,15 +488,15 @@ int __cdecl sub_4CEED0(int a1, int a2) {
 	if (v2 < 0) {
 		if (v3 & 8)
 			sub_46ABB0(*(int*)&byte_5D4594[1523040], 0);
-		if (*(_BYTE*)(*(_DWORD*)&byte_5D4594[1523044] + 4) & 8)
-			sub_46ABB0(*(int*)&byte_5D4594[1523044], 0);
+		if (*(_BYTE*)(dword_5d4594_1523044 + 4) & 8)
+			sub_46ABB0(*(int*)&dword_5d4594_1523044, 0);
 		if (*(_BYTE*)(*(_DWORD*)&byte_5D4594[1523048] + 4) & 8)
 			sub_46ABB0(*(int*)&byte_5D4594[1523048], 0);
 	} else {
 		if (!(v3 & 8))
 			sub_46ABB0(*(int*)&byte_5D4594[1523040], 1);
-		if (!(*(_BYTE*)(*(_DWORD*)&byte_5D4594[1523044] + 4) & 8) && !nox_common_gameFlags_check_40A5C0(49152))
-			sub_46ABB0(*(int*)&byte_5D4594[1523044], 1);
+		if (!(*(_BYTE*)(dword_5d4594_1523044 + 4) & 8) && !nox_common_gameFlags_check_40A5C0(49152))
+			sub_46ABB0(*(int*)&dword_5d4594_1523044, 1);
 		if (!(*(_BYTE*)(*(_DWORD*)&byte_5D4594[1523048] + 4) & 8))
 			sub_46ABB0(*(int*)&byte_5D4594[1523048], 1);
 	}
