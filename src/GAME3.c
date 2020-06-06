f6e8f06a0ab9030112da35c6c25e559ad589a82f
Denys Smirnov <dennwc@pm.me>
2020-06-06 15:04:13 +0300

    Reconstruct variable dword_5d4594_3798684.

--- src/GAME3.c
+++ src/GAME3.c
@@ -25,6 +25,7 @@
 
 #include "proto.h"
 
+extern _DWORD dword_5d4594_3798684;
 extern _DWORD dword_5d4594_3798680;
 extern _DWORD dword_5d4594_3798676;
 extern _DWORD dword_5d4594_1308112;
@@ -12498,7 +12499,7 @@ LABEL_25:
 			*(_DWORD*)&byte_5D4594[3798672] = *((_DWORD*)v3 + 576);
 			dword_5d4594_3798676 = *((_DWORD*)v3 + 578);
 			dword_5d4594_3798680 = *((_DWORD*)v3 + 577);
-			*(_DWORD*)&byte_5D4594[3798684] = *((_DWORD*)v3 + 575);
+			dword_5d4594_3798684 = *((_DWORD*)v3 + 575);
 			*(_DWORD*)&byte_5D4594[3798688] = *((_DWORD*)v3 + 573);
 			v16 = *((_DWORD*)v3 + 920);
 			if (v16 & 0x400) {
@@ -12525,7 +12526,7 @@ LABEL_25:
 		*(_DWORD*)&byte_5D4594[3798672] = *(_DWORD*)&byte_5D4594[2650684];
 		dword_5d4594_3798676 = *(_DWORD*)&byte_5D4594[2650684];
 		dword_5d4594_3798680 = *(_DWORD*)&byte_5D4594[2650684];
-		*(_DWORD*)&byte_5D4594[3798684] = *(_DWORD*)&byte_5D4594[2650684];
+		dword_5d4594_3798684 = *(_DWORD*)&byte_5D4594[2650684];
 		*(_DWORD*)&byte_5D4594[3798688] = *(_DWORD*)&byte_5D4594[2650684];
 		goto LABEL_37;
 	}
@@ -12533,14 +12534,14 @@ LABEL_25:
 	*(_DWORD*)&byte_5D4594[3798672] = *(_DWORD*)&byte_5D4594[2523948];
 	dword_5d4594_3798676 = *(_DWORD*)&byte_5D4594[2523948];
 	dword_5d4594_3798680 = *(_DWORD*)&byte_5D4594[2523948];
-	*(_DWORD*)&byte_5D4594[3798684] = *(_DWORD*)&byte_5D4594[2523948];
+	dword_5d4594_3798684 = *(_DWORD*)&byte_5D4594[2523948];
 	*(_DWORD*)&byte_5D4594[3798688] = *(_DWORD*)&byte_5D4594[2523948];
 LABEL_37:
 	sub_4341D0(1, *(int*)&byte_5D4594[3798668]);
 	sub_4341D0(2, *(int*)&byte_5D4594[3798672]);
 	sub_4341D0(3, *(int*)&dword_5d4594_3798676);
 	sub_4341D0(4, *(int*)&dword_5d4594_3798680);
-	sub_4341D0(5, *(int*)&byte_5D4594[3798684]);
+	sub_4341D0(5, *(int*)&dword_5d4594_3798684);
 	sub_4341D0(6, *(int*)&byte_5D4594[3798688]);
 	v18 = sub_4B8FA0(dr, &v28, &v27);
 	if (!v18)
