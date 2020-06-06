059c9e865617e12471e5d1d1d4148ce103891a92
Denys Smirnov <dennwc@pm.me>
2020-06-06 15:03:50 +0300

    Reconstruct variable dword_5d4594_2523804.

--- src/common/gamemech/pausefx.c
+++ src/common/gamemech/pausefx.c
@@ -1,6 +1,7 @@
 #include "pausefx.h"
 
 #include "../../proto.h"
+extern _DWORD dword_5d4594_2523804;
 extern _DWORD dword_5d4594_2523776;
 extern _DWORD dword_5d4594_2523780;
 
@@ -11,7 +12,7 @@ void __cdecl sub_57AF30(int a1, int a2) {
 	wchar_t* v4; // eax
 	int v5;      // esi
 
-	if (*(_DWORD*)&byte_5D4594[2523804] != 1 && !sub_413A50()) {
+	if (dword_5d4594_2523804 != 1 && !sub_413A50()) {
 		v2 = a1;
 		if (a1)
 			dword_5d4594_2523780 = a1;
@@ -41,7 +42,7 @@ void __cdecl sub_57AF30(int a1, int a2) {
 					LABEL_30:
 						*(_DWORD*)&byte_5D4594[2523800] = 0;
 						*(_DWORD*)&byte_5D4594[2523772] = a2;
-						*(_DWORD*)&byte_5D4594[2523804] = 1;
+						dword_5d4594_2523804 = 1;
 						sub_413A00(1);
 						*(_QWORD*)&byte_5D4594[2523788] = sub_416BB0();
 						return;
