405aecf2f209a3fbcfd8a47e56c13f7d783956ff
Anton Afanasyev <aasoft@gmail.com>
2020-06-14 23:36:36 -0700

    Cleanup: sub_412260_parse_ident_desc

--- src/common/object/modifier.h
+++ src/common/object/modifier.h
@@ -6,7 +6,7 @@
 int __cdecl sub_411B90(const char* a1, char* a2, obj_412ae0_t* obj);
 int __cdecl sub_412100_parse_desc(const char* a1, char* a2, obj_412ae0_t* obj);
 int __cdecl sub_4121B0_parse_second_desc(const char* a1, char* a2, obj_412ae0_t* obj);
-int __cdecl sub_412260_parse_ident_desc(const char* a1, char* a2, obj_412ae0_t* a3);
+int __cdecl sub_412260_parse_ident_desc(const char* a1, char* a2, obj_412ae0_t* obj);
 wchar_t* __cdecl sub_413480(char a1);
 
 #endif // NOX_PORT_COMMON_OBJECT_MODIFIER
