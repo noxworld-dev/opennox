bf86b2febcb84a191e71d523b4e9c92d4f923c01
Anton Afanasyev <aasoft@gmail.com>
2020-06-14 23:35:32 -0700

    Cleanup: sub_412100_parse_desc

--- src/common/object/modifier.h
+++ src/common/object/modifier.h
@@ -4,7 +4,7 @@
 #include "../../defs.h"
 
 int __cdecl sub_411B90(const char* a1, char* a2, obj_412ae0_t* obj);
-int __cdecl sub_412100_parse_desc(const char* a1, char* a2, obj_412ae0_t* a3);
+int __cdecl sub_412100_parse_desc(const char* a1, char* a2, obj_412ae0_t* obj);
 int __cdecl sub_4121B0_parse_second_desc(const char* a1, char* a2, obj_412ae0_t* a3);
 int __cdecl sub_412260_parse_ident_desc(const char* a1, char* a2, obj_412ae0_t* a3);
 wchar_t* __cdecl sub_413480(char a1);
