74219071ff7129811234289b251cb6b0760b9426
Anton Afanasyev <aasoft@gmail.com>
2020-06-29 02:20:02 -0700

    Cleanup nox_parse_thing and nox_parse_thing_* signatures

--- src/client/drawable/drawdb.h
+++ src/client/drawable/drawdb.h
@@ -3,8 +3,8 @@
 
 #include "../../defs.h"
 
-bool __cdecl nox_parse_thing_pretty_name(nox_thing* obj, nox_memfile* f, char* a3);
-bool __cdecl nox_parse_thing_desc(nox_thing* obj, nox_memfile* f, char* a3);
+bool __cdecl nox_parse_thing_pretty_name(nox_thing* obj, nox_memfile* f, char* attr_value);
+bool __cdecl nox_parse_thing_desc(nox_thing* obj, nox_memfile* f, char* attr_value);
 void* __cdecl sub_44C840_read_things(void);
 
 #endif // NOX_PORT_CLIENT_DRAWABLE_DRAWDB
