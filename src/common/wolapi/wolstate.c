d7122b9bb3ec2504291a77874ccb53cc01a6af98
Denys Smirnov <dennwc@pm.me>
2020-07-08 19:05:53 +0300

    Reflow sub_41DDB0.

--- src/common/wolapi/wolstate.c
+++ src/common/wolapi/wolstate.c
@@ -46,44 +46,51 @@ int sub_41DDB0() {
 	wchar_t* v2; // eax
 	wchar_t* v3; // eax
 
-	if (*(_DWORD*)&byte_587000[58232]) {
-		v0 = *(_DWORD*)&byte_587000[58232];
-		do {
-			v1 = sub_41E2F0();
-			switch (sub_41DCC0(v1)) {
+	if (!*(_DWORD*)&byte_587000[58232]) {
+		sub_40D250();
+		return 1;
+	}
+	v0 = *(_DWORD*)&byte_587000[58232];
+	do {
+		v1 = sub_41E2F0();
+		switch (sub_41DCC0(v1)) {
 			case 5:
-				if (dword_5d4594_2660652) {
-					if (!*(_WORD*)&byte_5D4594[2660140]) {
-						switch (dword_5d4594_2660652 + 2147221403) {
-						case 0:
-							v2 = loadString_sub_40F1D0((char*)&byte_587000[58748], 0,
-													   "C:\\NoxPost\\src\\common\\WolAPI\\wolstate.c", 414);
-							nox_wcscpy((wchar_t*)&byte_5D4594[2660140], v2);
-							break;
-						case 1:
-							v3 = loadString_sub_40F1D0((char*)&byte_587000[58800], 0,
-													   "C:\\NoxPost\\src\\common\\WolAPI\\wolstate.c", 418);
-							goto LABEL_14;
-						case 155:
-							v3 = loadString_sub_40F1D0((char*)&byte_587000[58848], 0,
-													   "C:\\NoxPost\\src\\common\\WolAPI\\wolstate.c", 422);
-							goto LABEL_14;
-						case 156:
-							v3 = loadString_sub_40F1D0((char*)&byte_587000[58904], 0,
-													   "C:\\NoxPost\\src\\common\\WolAPI\\wolstate.c", 426);
-							goto LABEL_14;
-						default:
-							v3 = loadString_sub_40F1D0((char*)&byte_587000[58956], 0,
-													   "C:\\NoxPost\\src\\common\\WolAPI\\wolstate.c", 430);
-						LABEL_14:
-							nox_swprintf((wchar_t*)&byte_5D4594[2660140], v3);
-							break;
-						}
-					}
-					sub_468DF0((wchar_t*)&byte_5D4594[2660140]);
-				} else {
+				if (!dword_5d4594_2660652) {
 					sub_468F30_wol_reg();
+					break;
 				}
+				if (*(_WORD*)&byte_5D4594[2660140]) {
+					sub_468DF0((wchar_t*)&byte_5D4594[2660140]);
+					break;
+				}
+				switch (dword_5d4594_2660652 + 2147221403) {
+					case 0:
+						v2 = loadString_sub_40F1D0((char*)&byte_587000[58748], 0,
+												   "C:\\NoxPost\\src\\common\\WolAPI\\wolstate.c", 414);
+						nox_wcscpy((wchar_t*)&byte_5D4594[2660140], v2);
+						break;
+					case 1:
+						v3 = loadString_sub_40F1D0((char*)&byte_587000[58800], 0,
+												   "C:\\NoxPost\\src\\common\\WolAPI\\wolstate.c", 418);
+						nox_swprintf((wchar_t*)&byte_5D4594[2660140], v3);
+						break;
+					case 155:
+						v3 = loadString_sub_40F1D0((char*)&byte_587000[58848], 0,
+												   "C:\\NoxPost\\src\\common\\WolAPI\\wolstate.c", 422);
+						nox_swprintf((wchar_t*)&byte_5D4594[2660140], v3);
+						break;
+					case 156:
+						v3 = loadString_sub_40F1D0((char*)&byte_587000[58904], 0,
+												   "C:\\NoxPost\\src\\common\\WolAPI\\wolstate.c", 426);
+						nox_swprintf((wchar_t*)&byte_5D4594[2660140], v3);
+						break;
+					default:
+						v3 = loadString_sub_40F1D0((char*)&byte_587000[58956], 0,
+												   "C:\\NoxPost\\src\\common\\WolAPI\\wolstate.c", 430);
+						nox_swprintf((wchar_t*)&byte_5D4594[2660140], v3);
+						break;
+				}
+				sub_468DF0((wchar_t*)&byte_5D4594[2660140]);
 				break;
 			case 20:
 				sub_468E60(0);
@@ -94,10 +101,9 @@ int sub_41DDB0() {
 				break;
 			default:
 				break;
-			}
-			--v0;
-		} while (v0);
-	}
+		}
+		--v0;
+	} while (v0);
 	sub_40D250();
 	return 1;
 }
