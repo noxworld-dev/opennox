bfcfdac4d59cd03e90114befb3558d564e88bed1
Denys Smirnov <dennwc@pm.me>
2020-07-08 13:32:48 +0300

    Reflow sub_4EE500_obj_health.

--- src/server/object/health.c
+++ src/server/object/health.c
@@ -31,38 +31,42 @@ void __cdecl sub_4EE500_obj_health(int a1) {
 	wchar_t* v7; // eax
 
 	v1 = a1;
-	if (a1) {
-		if (nox_common_gameFlags_check_40A5C0(2048)) {
-			v2 = *(_DWORD*)(a1 + 520);
-			if (v2) {
-				v3 = 1;
-				v4 = sub_4EC580(*(_DWORD*)(a1 + 520));
-				if (*(_BYTE*)(v4 + 8) & 4) {
-					v5 = 0;
-					if (v2 == v4)
-						goto LABEL_12;
-					do {
-						if (v5)
-							break;
-						if (*(_BYTE*)(v2 + 8) & 2) {
-							v3 = sub_500CC0(v4, v2);
-							v5 = 1;
-						}
-						v2 = *(_DWORD*)(v2 + 508);
-					} while (v2 != v4);
-					if (v3) {
-						v1 = a1;
-					LABEL_12:
-						v6 = sub_4EF270(v4, *(float*)(v1 + 28));
-						if (v6 > 0.0) {
-							v7 = loadString_sub_40F1D0((char*)&byte_587000[206012], 0,
-													   "C:\\NoxPost\\src\\Server\\Object\\health.c", 172);
-							sub_4D9EB0(v4, v7, (unsigned int)(__int64)v6);
-						}
-						return;
-					}
-				}
-			}
-		}
+	if (!a1) {
+		return;
 	}
+	if (!nox_common_gameFlags_check_40A5C0(2048)) {
+		return;
+	}
+	v2 = *(_DWORD*)(a1 + 520);
+	if (!v2) {
+		return;
+	}
+	v3 = 1;
+	v4 = sub_4EC580(*(_DWORD*)(a1 + 520));
+	if (!(*(_BYTE*)(v4 + 8) & 4)) {
+		return;
+	}
+	v5 = 0;
+	if (v2 != v4) {
+		while (v2 != v4) {
+			if (v5)
+				break;
+			if (*(_BYTE *) (v2 + 8) & 2) {
+				v3 = sub_500CC0(v4, v2);
+				v5 = 1;
+			}
+			v2 = *(_DWORD *) (v2 + 508);
+		}
+		if (!v3) {
+			return;
+		}
+		v1 = a1;
+	}
+	v6 = sub_4EF270(v4, *(float*)(v1 + 28));
+	if (v6 > 0.0) {
+		v7 = loadString_sub_40F1D0((char*)&byte_587000[206012], 0,
+								   "C:\\NoxPost\\src\\Server\\Object\\health.c", 172);
+		sub_4D9EB0(v4, v7, (unsigned int)(__int64)v6);
+	}
+	return;
 }
