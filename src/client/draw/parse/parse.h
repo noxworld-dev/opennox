ec2a3c9f56e5b51f8b142d8576c642bb1b4868a8
Anton Afanasyev <aasoft@gmail.com>
2020-07-14 02:34:42 -0700

    Fix signature: sub_44BC50

--- src/client/draw/parse/parse.h
+++ src/client/draw/parse/parse.h
@@ -6,7 +6,7 @@
 void* __cdecl sub_44C000(char* attr_value, nox_memfile* f);
 int __cdecl sub_44BFA0(int a1, nox_memfile* f);
 int __cdecl sub_44B8B0(int a1, nox_memfile* f);
-int __cdecl sub_44BC50(int a1, int a2);
+int __cdecl sub_44BC50(int a1, nox_memfile* f);
 int __cdecl sub_44B4C0(const char* a1);
 
 #endif // NOX_PORT_CLIENT_DRAW_PARSE_PARSE
