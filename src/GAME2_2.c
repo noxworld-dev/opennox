5a686fd308b91090e22775c03bc6203ab230a725
Denys Smirnov <dennwc@pm.me>
2020-07-16 18:34:36 +0300

    Set correct type for nox_pixbuffer_rows_3798784.

--- src/GAME2_2.c
+++ src/GAME2_2.c
@@ -123,7 +123,7 @@ extern _DWORD dword_5d4594_3798844;
 extern _DWORD dword_5d4594_3804672;
 extern _DWORD dword_5d4594_3804656;
 extern _DWORD dword_5d4594_3804664;
-extern void* nox_pixbuffer_rows_3798784;
+extern BYTE** nox_pixbuffer_rows_3798784;
 extern int nox_win_width;
 extern int nox_win_height;
 extern int nox_backbuffer_width;
