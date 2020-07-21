a429236d23986aafe92afebf8dc4a8e23d63ee6b
Denys Smirnov <dennwc@pm.me>
2020-07-21 17:46:59 +0300

    Fix signature for sub_4191D0.

--- src/common/system/team.h
+++ src/common/system/team.h
@@ -7,6 +7,6 @@ int sub_417C60();
 int sub_417E10();
 char* __cdecl sub_4186D0(char a1);
 wchar_t* __cdecl sub_418C20(int a1);
-char* __cdecl sub_4191D0(unsigned __int8 a1, int a2, int a3, int a4, int a5);
+void sub_4191D0(unsigned __int8 a1, int a2, int a3, int a4, int a5);
 
 #endif // NOX_PORT_COMMON_TEAM
