0033dd2677a837d1e9a6f1a4409e106d3382c9f0
Denys Smirnov <dennwc@pm.me>
2020-07-30 13:49:52 +0300

    Reconstruct usages of nox_screenParticle in nox_client_screenParticleDraw_489700.

--- src/client/draw/partscrn.c
+++ src/client/draw/partscrn.c
@@ -3,62 +3,53 @@
 #include "../../proto.h"
 
 //----- (00489700) --------------------------------------------------------
-int nox_client_screenParticleDraw_489700(void* a1p, nox_screenParticle* a2p) {
+int nox_client_screenParticleDraw_489700(void* a1p, nox_screenParticle* p) {
 	int a1 = a1p;
-	int a2 = a2p;
-	int v2;             // ecx
-	char v3;            // al
-	char v4;            // al
-	unsigned __int8 v5; // al
-	char v6;            // al
 	int v7;             // eax
-	int v8;             // eax
 	int v10;            // [esp-20h] [ebp-30h]
 	char v11;           // [esp-10h] [ebp-20h]
 	char v12;           // [esp-Ch] [ebp-1Ch]
 	int2 xLeft;         // [esp+8h] [ebp-8h]
 
-	v2 = *(int*)(a2 + 28) >> 16;
-	xLeft.field_0 = *(int*)(a2 + 24) >> 16;
-	xLeft.field_4 = v2;
-	if (xLeft.field_0 <= 0 || v2 <= 0 || xLeft.field_0 >= *(int*)(a1 + 32) || v2 >= *(int*)(a1 + 36)) {
-		sub_431700((_QWORD *) a2);
+	xLeft.field_0 = p->field_24 >> 16;
+	xLeft.field_4 = p->field_28 >> 16;
+	if (xLeft.field_0 <= 0 || xLeft.field_4 <= 0 || xLeft.field_0 >= *(int*)(a1 + 32) || xLeft.field_4 >= *(int*)(a1 + 36)) {
+		sub_431700(p);
 		return 0;
 	}
-	sub_4B6720(&xLeft, *(_DWORD*)(a2 + 8), *(unsigned __int8*)(a2 + 40), *(_BYTE*)(a2 + 40));
-	nox_client_drawSetColor_434460(*(_DWORD*)(a2 + 12));
-	sub_499B70(xLeft.field_0, xLeft.field_4, *(unsigned __int8*)(a2 + 40) >> 1);
-	v3 = *(_BYTE*)(a2 + 41);
-	*(_DWORD*)(a2 + 20) += *(_DWORD*)(a2 + 36);
+	sub_4B6720(&xLeft, p->field_8, p->field_40[0], p->field_40[0]);
+	nox_client_drawSetColor_434460(p->field_12);
+	sub_499B70(xLeft.field_0, xLeft.field_4, p->field_40[0] >> 1);
+	char v3 = p->field_40[1];
+	p->field_20 += p->field_36;
 	if (v3) {
-		v4 = v3 - 1;
-		*(_BYTE *) (a2 + 41) = v4;
+		char v4 = v3 - 1;
+		p->field_40[1] = v4;
 		if (!v4) {
-			if (*(_BYTE *) (a2 + 42) == 1) {
-				v5 = *(_BYTE *) (a2 + 40) + 1;
-				*(_BYTE *) (a2 + 40) = v5;
+			if (p->field_40[2] == 1) {
+				_BYTE v5 = p->field_40[0] + 1;
+				p->field_40[0] = v5;
 				if (v5 >= 4u)
-					*(_BYTE *) (a2 + 42) = 2;
+					p->field_40[2] = 2;
 			} else {
-				v6 = *(_BYTE *) (a2 + 40) - 1;
-				*(_BYTE *) (a2 + 40) = v6;
+				char v6 = p->field_40[0] - 1;
+				p->field_40[0] = v6;
 				if (!v6) {
-					sub_431700((_QWORD *) a2);
+					sub_431700(p);
 					return 0;
 				}
 			}
-			*(_BYTE *) (a2 + 41) = *(_BYTE *) (a2 + 43);
+			p->field_40[1] = p->field_40[3];
 		}
 	}
-	if (*(_BYTE*)(a2 + 32) == 1 && sub_415FF0(0, 10, "C:\\NoxPost\\src\\client\\Draw\\PartScrn.c", 105) >= 8) {
+	if (*(_BYTE*)(&p->field_32) == 1 && sub_415FF0(0, 10, "C:\\NoxPost\\src\\client\\Draw\\PartScrn.c", 105) >= 8) {
 		v12 = sub_415FF0(3, 5, "C:\\NoxPost\\src\\client\\Draw\\PartScrn.c", 115);
 		v11 = sub_415FF0(2, 3, "C:\\NoxPost\\src\\client\\Draw\\PartScrn.c", 114);
 		v10 = xLeft.field_4;
 		v7 = sub_415FF0(-2, 2, "C:\\NoxPost\\src\\client\\Draw\\PartScrn.c", 109);
-		nox_client_newScreenParticle_431540(*(_DWORD*)(a2 + 4), xLeft.field_0 + v7, v10, 0, 0, 1, v11, v12, 2, 2);
+		nox_client_newScreenParticle_431540(p->field_4, xLeft.field_0 + v7, v10, 0, 0, 1, v11, v12, 2, 2);
 	}
-	v8 = *(_DWORD*)(a2 + 28);
-	*(_DWORD*)(a2 + 24) += *(_DWORD*)(a2 + 16);
-	*(_DWORD*)(a2 + 28) = *(_DWORD*)(a2 + 20) + v8;
+	p->field_24 += p->field_16;
+	p->field_28 += p->field_20;
 	return 1;
 }
