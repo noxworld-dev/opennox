2148611ecc27fb5ab121407381c03ae3451a8551
Anton Afanasyev <aasoft@gmail.com>
2020-08-05 00:51:22 -0700

    Add methods for manipulating common engine flags

--- src/proto.h
+++ src/proto.h
@@ -44,6 +44,11 @@ extern unsigned __int8 byte_5D4594[3844309];
 
 #define MEMACCESS(x) *((_DWORD*)getMem(x))
 
+void nox_common_setEngineFlag(const uint32_t flags);
+void nox_common_resetEngineFlag(const uint32_t flags);
+void nox_common_toggleEngineFlag(const uint32_t flags);
+bool nox_common_getEngineFlag(const uint32_t flags);
+
 void init_data();
 _BYTE* getMem(uintptr_t addr);
 
