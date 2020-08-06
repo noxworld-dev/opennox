25e7bd439a262935460950c7091bb51b459bb09d
Anton Afanasyev <aasoft@gmail.com>
2020-08-06 00:54:33 -0700

    Use methods for using engine flags: GAME4_3.c

--- src/GAME4_3.c
+++ src/GAME4_3.c
@@ -3052,7 +3052,7 @@ void sub_5341A0(char* a1, ...) {
 	va_start(va, a1);
 	// nox_vsprintf(debug, a1, va);
 	// OutputDebugStringA(debug);
-	if (nox_common_engineFlags & 8)
+	if (nox_common_getEngineFlag(1u << 3u))
 		nox_vsprintf((char*)&byte_5D4594[2487996], a1, va);
 }
 
