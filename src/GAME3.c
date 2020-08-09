noxworld-dev/opennox

commit d4b019b102a928907fbe23d78db5e4e77552e416

Fix type for dword_5d4594_1309740.

src/GAME3.c
@@ -158,7 +158,7 @@ nox_window* nox_win_main_bg = 0;
 nox_wnd_xxx* dword_5d4594_1307308 = 0;
 nox_wnd_xxx* dword_5d4594_1307732 = 0;
 nox_wnd_xxx* dword_5d4594_1308092 = 0;
-void* dword_5d4594_1309740 = 0;
+nox_wnd_xxx* dword_5d4594_1309740 = 0;
 
 //----- (004A19D0) --------------------------------------------------------
 int sub_4A19D0() {
@@ -4332,9 +4332,9 @@ int sub_4AA6B0() {
 			result = nox_wnd_sub_43C5B0(*(_DWORD**)&dword_5d4594_1309720, 0, 0, 0, -480, 0, 20, 0, -40);
 			dword_5d4594_1309740 = result;
 			if (result) {
-				*(_DWORD*)result = 300;
-				*(_DWORD*)((_DWORD)dword_5d4594_1309740 + 48) = sub_4AA9C0;
-				*(_DWORD*)((_DWORD)dword_5d4594_1309740 + 56) = sub_4AAA10;
+				dword_5d4594_1309740->field_0 = 300;
+				dword_5d4594_1309740->field_12 = sub_4AA9C0;
+				dword_5d4594_1309740->field_14 = sub_4AAA10;
 				v1 = sub_46B0C0(*(_DWORD**)&dword_5d4594_1309720, 351);
 				*(_DWORD*)(v1[100] + 8) = 24;
 				*(_DWORD*)(v1[100] + 12) = 20;
@@ -4403,7 +4403,7 @@ int sub_4AA9C0() {
 	sub_44D8F0();
 	sub_43BEF0_set_video_mode(*(int*)&dword_587000_172876, *(int*)&dword_587000_172872, *(int*)&dword_587000_172880);
 	nox_common_writecfgfile((char*)&byte_587000[173256]);
-	*(_BYTE*)((_DWORD)dword_5d4594_1309740 + 64) = 2;
+	*(_BYTE*)(&dword_5d4594_1309740->field_16) = 2;
 	sub_43BE40(2);
 	sub_452D80(923, 100);
 	return 1;
@@ -4411,10 +4411,10 @@ int sub_4AA9C0() {
 
 //----- (004AAA10) --------------------------------------------------------
 int sub_4AAA10() {
-	void (*v0)(void); // esi
+	int (*v0)(void); // esi
 
-	v0 = *(void (**)(void))((_DWORD)dword_5d4594_1309740 + 52);
-	sub_43C570(*(LPVOID*)&dword_5d4594_1309740);
+	v0 = dword_5d4594_1309740->field_12;
+	sub_43C570(dword_5d4594_1309740);
 	sub_46C4E0(*(_DWORD**)&dword_5d4594_1309720);
 	sub_4A1A40(1);
 	if (nox_win_depth_1 == nox_win_depth_2) {
@@ -4552,8 +4552,8 @@ int __cdecl sub_4AABE0(int a1, int a2, int* a3, int a4) {
 			}
 		} else if (v4 == 341) {
 			sub_4AA9C0();
-			*(_DWORD*)((_DWORD)dword_5d4594_1309740 + 56) = sub_4AB0C0;
-			*(_DWORD*)((_DWORD)dword_5d4594_1309740 + 52) = sub_4CB880;
+			dword_5d4594_1309740->field_14 = sub_4AB0C0;
+			dword_5d4594_1309740->field_13 = sub_4CB880;
 		} else {
 			switch (v4) {
 			case 311:
@@ -4731,10 +4731,10 @@ int __cdecl sub_4AABE0(int a1, int a2, int* a3, int a4) {
 
 //----- (004AB0C0) --------------------------------------------------------
 int sub_4AB0C0() {
-	void (*v0)(void); // esi
+	int (*v0)(void); // esi
 
-	v0 = *(void (**)(void))((_DWORD)dword_5d4594_1309740 + 52);
-	sub_43C570(*(LPVOID*)&dword_5d4594_1309740);
+	v0 = dword_5d4594_1309740->field_13;
+	sub_43C570(dword_5d4594_1309740);
 	sub_46C4E0(*(_DWORD**)&dword_5d4594_1309720);
 	v0();
 	return 1;

commit fc8578b1a9324770e2f16f4c25d40354fe12d1cd

Fix broken back button from options dialog
Introduced by a typo in commit d4b019b102a928907fbe23d78db5e4e77552e416

src/GAME3.c
@@ -4413,7 +4413,7 @@ int sub_4AA9C0() {
 int sub_4AAA10() {
 	int (*v0)(void); // esi
 
-	v0 = dword_5d4594_1309740->field_12;
+	v0 = dword_5d4594_1309740->field_13;
 	sub_43C570(dword_5d4594_1309740);
 	sub_46C4E0(*(_DWORD**)&dword_5d4594_1309720);
 	sub_4A1A40(1);