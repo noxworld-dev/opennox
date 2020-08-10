270b71b5916ee9654ffa02265000acc3ba7c691b
Anton Afanasyev <aasoft@gmail.com>
2020-08-10 08:13:06 -0700

    Remove unused return value from sub_445C40

--- src/client/gui/guiquit.h
+++ src/client/gui/guiquit.h
@@ -6,7 +6,7 @@
 int sub_445790();
 int __cdecl sub_445840(_DWORD* a1, int a2, int* a3, int a4);
 void* sub_445B40();
-int sub_445C40();
+void sub_445C40();
 void sub_446190();
 
 #endif // NOX_PORT_CLIENT_GUI_GUIQUIT
