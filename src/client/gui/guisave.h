1faf0541b481d08c515027540bd787ed1ff8ed84
Anton Afanasyev <aasoft@gmail.com>
2020-08-10 07:45:20 -0700

    Remove unused return value from sub_46D580

--- src/client/gui/guisave.h
+++ src/client/gui/guisave.h
@@ -7,6 +7,6 @@ int sub_46C730();
 int __cdecl sub_46C920(_DWORD* a1, int a2, int* a3, int a4);
 int sub_46CBD0();
 int __cdecl sub_46CE40(int a1, int a2, int a3, _BYTE* a4);
-int sub_46D580();
+void sub_46D580();
 
 #endif // NOX_PORT_CLIENT_GUI_GUISAVE
