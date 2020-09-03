5bc2c22c7becc0cd781c02f2794b74ad747cf4b3
Denys Smirnov <dennwc@pm.me>
2020-09-03 22:59:17 +0300

    Wrap blob accesses in guimsg.c.

--- src/client/gui/guimsg.c
+++ src/client/gui/guimsg.c
@@ -14,13 +14,13 @@ void __cdecl sub_445490(wchar_t* a1) {
 			v1 = 0;
 			*(unsigned int*)&dword_5d4594_825736 = 0;
 		}
-		nox_wcscpy((unsigned short*)&byte_5D4594[644 * v1 + 823804], a1);
+		nox_wcscpy((unsigned short*)getMemAt(0x5D4594, 644 * v1 + 823804), a1);
 		v2 = 644 * *(unsigned int*)&dword_5d4594_825736;
-		*(unsigned int*)&byte_5D4594[v2 + 824440] = *(unsigned int*)&byte_5D4594[2598000] +
-													4 * *(unsigned int*)&byte_5D4594[2649704] +
-													*(unsigned int*)&byte_5D4594[2649704];
-		byte_5D4594[v2 + 824444] = 0;
-		v3 = loadString_sub_40F1D0((char*)&byte_587000[107916], 0, "C:\\NoxPost\\src\\Client\\Gui\\guimsg.c", 69);
+		*(unsigned int*)getMemAt(0x5D4594, v2 + 824440) = *(unsigned int*)getMemAt(0x5D4594, 2598000) +
+													4 * *(unsigned int*)getMemAt(0x5D4594, 2649704) +
+													*(unsigned int*)getMemAt(0x5D4594, 2649704);
+		*getMemAt( 0x5D4594, v2 + 824444) = 0;
+		v3 = loadString_sub_40F1D0((char*)getMemAt(0x587000, 107916), 0, "C:\\NoxPost\\src\\Client\\Gui\\guimsg.c", 69);
 		sub_450C00(6u, v3, a1);
 	}
 }
