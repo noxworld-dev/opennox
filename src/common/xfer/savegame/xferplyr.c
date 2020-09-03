2f75c3f51e1f0be1907317bccc42197d4dd3fedb
Denys Smirnov <dennwc@pm.me>
2020-09-03 23:11:32 +0300

    Wrap blob accesses in xferplyr.c.

--- src/common/xfer/savegame/xferplyr.c
+++ src/common/xfer/savegame/xferplyr.c
@@ -18,11 +18,11 @@ int __cdecl sub_41C8F0(char* a1, unsigned int a2) {
 	wchar_t* v10;        // eax
 	CHAR PathName[1024]; // [esp+4h] [ebp-400h]
 
-	if (nox_common_gameFlags_check_40A5C0(1) || a2 >= *(int*)&byte_587000[55984]) {
+	if (nox_common_gameFlags_check_40A5C0(1) || a2 >= *(int*)getMemAt(0x587000, 55984)) {
 		v5 = nox_common_get_data_path_409E10();
 		nox_sprintf(PathName, "%s\\Save\\", v5);
 		CreateDirectoryA(PathName, 0);
-		v6 = sub_408CC0_fopen((char*)&byte_5D4594[2660688], 1);
+		v6 = sub_408CC0_fopen((char*)getMemAt(0x5D4594, 2660688), 1);
 		nox_file_2 = v6;
 		if (v6) {
 			if (sub_408D40((int)v6, 27)) {
@@ -36,37 +36,37 @@ int __cdecl sub_41C8F0(char* a1, unsigned int a2) {
 				}
 				sub_408D90(nox_file_2);
 				if (nox_common_gameFlags_check_40A5C0(4096))
-					byte_5D4594[2661961] = sub_465DF0();
+					*getMemAt( 0x5D4594, 2661961) = sub_465DF0();
 				else
-					byte_5D4594[2661961] = 0;
-				if (sub_41A230((char*)&byte_5D4594[2660688])) {
-					v9 = loadString_sub_40F1D0((char*)&byte_587000[56752], 0,
+					*getMemAt( 0x5D4594, 2661961) = 0;
+				if (sub_41A230((char*)getMemAt(0x5D4594, 2660688))) {
+					v9 = loadString_sub_40F1D0((char*)getMemAt(0x587000, 56752), 0,
 											   "C:\\NoxPost\\src\\common\\Xfer\\SaveGame\\XferPlyr.c", 3420);
 					sub_450C00(0xCu, v9);
-					v10 = loadString_sub_40F1D0((char*)&byte_587000[56816], 0,
+					v10 = loadString_sub_40F1D0((char*)getMemAt(0x587000, 56816), 0,
 												"C:\\NoxPost\\src\\common\\Xfer\\SaveGame\\XferPlyr.c", 3421);
 					sub_445490(v10);
 					result = 1;
 				} else {
-					sub_413D30((char*)&byte_587000[56648]);
+					sub_413D30((char*)getMemAt(0x587000, 56648));
 					result = 0;
 				}
 			} else {
-				sub_413D30((char*)&byte_587000[56600], &byte_5D4594[2660688]);
+				sub_413D30((char*)getMemAt(0x587000, 56600), getMemAt(0x5D4594, 2660688));
 				result = 0;
 			}
 		} else {
-			sub_413D30((char*)&byte_587000[56552], &byte_5D4594[2660688]);
+			sub_413D30((char*)getMemAt(0x587000, 56552), getMemAt(0x5D4594, 2660688));
 			result = 0;
 		}
 	} else {
-		v2 = loadString_sub_40F1D0((char*)&byte_587000[56392], 0,
+		v2 = loadString_sub_40F1D0((char*)getMemAt(0x587000, 56392), 0,
 								   "C:\\NoxPost\\src\\common\\Xfer\\SaveGame\\XferPlyr.c", 3341);
 		sub_450C00(0xCu, v2);
-		v3 = loadString_sub_40F1D0((char*)&byte_587000[56460], 0,
+		v3 = loadString_sub_40F1D0((char*)getMemAt(0x587000, 56460), 0,
 								   "C:\\NoxPost\\src\\common\\Xfer\\SaveGame\\XferPlyr.c", 3342);
 		sub_445490(v3);
-		sub_413D30((char*)&byte_587000[56480], &byte_5D4594[2660688]);
+		sub_413D30((char*)getMemAt(0x587000, 56480), getMemAt(0x5D4594, 2660688));
 		result = 0;
 	}
 	return result;
