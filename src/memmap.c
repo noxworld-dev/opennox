73a42765d2acb981401194af11a5955870813396
Denys Smirnov <dennwc@pm.me>
2020-09-03 23:22:31 +0300

    Remove byte_587000 from the proto.h.

--- src/memmap.c
+++ src/memmap.c
@@ -4,6 +4,7 @@
 
 extern unsigned __int8 byte_581450[23472];
 extern unsigned __int8 byte_5D4594[3844309];
+extern unsigned __int8 byte_587000[316820];
 
 extern nox_engine_flag nox_common_engineFlags;
 extern int nox_enable_audio;
