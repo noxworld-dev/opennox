56535f136b0283ba62d5e8bb4d0ba5c1c19257f2
Denys Smirnov <dennwc@pm.me>
2020-09-03 23:02:34 +0300

    Wrap blob accesses in mtailup.c.

--- src/client/drawable/update/mtailup.c
+++ src/client/drawable/update/mtailup.c
@@ -22,10 +22,10 @@ void __cdecl sub_4CDD80(int a1, _DWORD* a2) {
 	v2 = a2;
 	v3 = a2[3] - a2[108];
 	if (v3 * v3 + (a2[4] - a2[109]) * (a2[4] - a2[109]) > 200) {
-		v4 = *(_DWORD*)&byte_5D4594[1523000];
-		if (!*(_DWORD*)&byte_5D4594[1523000]) {
-			v4 = sub_44CFC0((CHAR*)&byte_587000[190560]);
-			*(_DWORD*)&byte_5D4594[1523000] = v4;
+		v4 = *(_DWORD*)getMemAt(0x5D4594, 1523000);
+		if (!*(_DWORD*)getMemAt(0x5D4594, 1523000)) {
+			v4 = sub_44CFC0((CHAR*)getMemAt(0x587000, 190560));
+			*(_DWORD*)getMemAt(0x5D4594, 1523000) = v4;
 		}
 		v5 = (_DWORD*)sub_45A360_drawable(v4, a2[108], a2[109]);
 		v5[108] = a2[3];
@@ -33,19 +33,19 @@ void __cdecl sub_4CDD80(int a1, _DWORD* a2) {
 		sub_45A110_drawable(v5);
 		a2[108] = a2[3];
 		a2[109] = a2[4];
-		sub_49B950(v5, *(int*)&byte_5D4594[2649704]);
+		sub_49B950(v5, *(int*)getMemAt(0x5D4594, 2649704));
 	}
 	v13 = a2[3] - a2[8];
 	v14 = a2[4] - a2[9];
-	if (!*(_DWORD*)&byte_5D4594[1523004])
-		*(_DWORD*)&byte_5D4594[1523004] = sub_44CFC0((CHAR*)&byte_587000[190576]);
+	if (!*(_DWORD*)getMemAt(0x5D4594, 1523004))
+		*(_DWORD*)getMemAt(0x5D4594, 1523004) = sub_44CFC0((CHAR*)getMemAt(0x587000, 190576));
 	v6 = 0;
 	v7 = 0;
 	v15 = 4;
 	do {
 		v8 = v2[8] + v7 / 4 + sub_415FF0(-8, 8, "C:\\NoxPost\\src\\Client\\Drawable\\Update\\mtailup.c", 66);
 		v9 = sub_415FF0(-8, 8, "C:\\NoxPost\\src\\Client\\Drawable\\Update\\mtailup.c", 68);
-		v10 = sub_45A360_drawable(*(int*)&byte_5D4594[1523004], v8, v2[9] + v6 / 4 + v9);
+		v10 = sub_45A360_drawable(*(int*)getMemAt(0x5D4594, 1523004), v8, v2[9] + v6 / 4 + v9);
 		v11 = v10;
 		if (v10) {
 			*(_DWORD*)(v10 + 300) = nox_thing_magic_sparkle_draw;
@@ -54,9 +54,9 @@ void __cdecl sub_4CDD80(int a1, _DWORD* a2) {
 			*(_DWORD*)(v11 + 436) = v2[4] << 12;
 			*(_BYTE*)(v11 + 299) = 0;
 			*(_DWORD*)(v11 + 440) = 0;
-			*(_DWORD*)(v11 + 448) = *(_DWORD*)&byte_5D4594[2598000] +
+			*(_DWORD*)(v11 + 448) = *(_DWORD*)getMemAt(0x5D4594, 2598000) +
 									sub_415FF0(10, 20, "C:\\NoxPost\\src\\Client\\Drawable\\Update\\mtailup.c", 90);
-			*(_DWORD*)(v11 + 444) = *(_DWORD*)&byte_5D4594[2598000];
+			*(_DWORD*)(v11 + 444) = *(_DWORD*)getMemAt(0x5D4594, 2598000);
 			*(_WORD*)(v11 + 104) = *((_WORD*)v2 + 52);
 			*(_WORD*)(v11 + 106) = *((_WORD*)v2 + 53);
 			*(_BYTE*)(v11 + 296) = 0;
