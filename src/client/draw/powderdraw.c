d4a595c5c9b0e35774a0d7d2869287649bda6696
Denys Smirnov <dennwc@pm.me>
2020-09-03 23:06:03 +0300

    Wrap blob accesses in powderdraw.c.

--- src/client/draw/powderdraw.c
+++ src/client/draw/powderdraw.c
@@ -10,7 +10,7 @@ int __cdecl nox_thing_black_powder_draw(_DWORD* a1, nox_drawable* dr) {
 
 	v2 = *a1 + *(_DWORD*)(a2 + 12) - a1[4];
 	v3 = *(_DWORD*)(a2 + 16) + a1[1] - a1[5];
-	nox_client_drawSetColor_434460(*(int*)&byte_5D4594[2650660]);
+	nox_client_drawSetColor_434460(*(int*)getMemAt(0x5D4594, 2650660));
 	nox_client_drawRectFilledOpaque_49CE30(v2 - 1, v3 - 1, 3, 3);
 	nox_client_drawRectFilledOpaque_49CE30(v2 - 5, v3, 1, 1);
 	nox_client_drawRectFilledOpaque_49CE30(v2, v3 + 7, 1, 1);
