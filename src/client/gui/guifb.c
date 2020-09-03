b5dae758cb8ea4d35c26b73a574f6a32d07cefa8
Denys Smirnov <dennwc@pm.me>
2020-09-03 22:58:35 +0300

    Wrap blob accesses in guifb.c.

--- src/client/gui/guifb.c
+++ src/client/gui/guifb.c
@@ -13,28 +13,28 @@ wchar_t* __cdecl sub_456140(unsigned __int8 a1) {
 	wchar_t* v5;     // eax
 	wchar_t* v6;     // eax
 
-	byte_5D4594[1045644] = a1;
+	*getMemAt( 0x5D4594, 1045644) = a1;
 	result = (wchar_t*)a1;
 	v2 = dword_5d4594_1045636 + 36;
 	switch (a1) {
 	case 0u:
 		*(_DWORD*)(v2 + 24) = sub_42F970("BallAtHome");
-		v3 = loadString_sub_40F1D0((char*)&byte_587000[128780], 0, "C:\\NoxPost\\src\\client\\Gui\\guifb.c", 165);
+		v3 = loadString_sub_40F1D0((char*)getMemAt(0x587000, 128780), 0, "C:\\NoxPost\\src\\client\\Gui\\guifb.c", 165);
 		result = sub_46B000((wchar_t*)v2, v3);
 		break;
 	case 1u:
 		*(_DWORD*)(v2 + 24) = sub_42F970("BallAway");
-		v4 = loadString_sub_40F1D0((char*)&byte_587000[128840], 0, "C:\\NoxPost\\src\\client\\Gui\\guifb.c", 170);
+		v4 = loadString_sub_40F1D0((char*)getMemAt(0x587000, 128840), 0, "C:\\NoxPost\\src\\client\\Gui\\guifb.c", 170);
 		result = sub_46B000((wchar_t*)v2, v4);
 		break;
 	case 2u:
 		*(_DWORD*)(v2 + 24) = sub_42F970("BallRed");
-		v5 = loadString_sub_40F1D0((char*)&byte_587000[128896], 0, "C:\\NoxPost\\src\\client\\Gui\\guifb.c", 175);
+		v5 = loadString_sub_40F1D0((char*)getMemAt(0x587000, 128896), 0, "C:\\NoxPost\\src\\client\\Gui\\guifb.c", 175);
 		result = sub_46B000((wchar_t*)v2, v5);
 		break;
 	case 4u:
 		*(_DWORD*)(v2 + 24) = sub_42F970("BallBlue");
-		v6 = loadString_sub_40F1D0((char*)&byte_587000[128956], 0, "C:\\NoxPost\\src\\client\\Gui\\guifb.c", 180);
+		v6 = loadString_sub_40F1D0((char*)getMemAt(0x587000, 128956), 0, "C:\\NoxPost\\src\\client\\Gui\\guifb.c", 180);
 		result = sub_46B000((wchar_t*)v2, v6);
 		break;
 	default:
