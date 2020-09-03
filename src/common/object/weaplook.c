6deb134d652028d4999383f19ff91db26ea426eb
Denys Smirnov <dennwc@pm.me>
2020-09-03 23:09:23 +0300

    Wrap blob accesses in weaplook.c.

--- src/common/object/weaplook.c
+++ src/common/object/weaplook.c
@@ -6,12 +6,12 @@ char** sub_4158C0() {
 	unsigned __int8* v1; // esi
 	int v2;              // ecx
 
-	result = *(char***)&byte_5D4594[371248];
-	if (!*(_DWORD*)&byte_5D4594[371248]) {
-		result = *(char***)&byte_587000[33396];
-		if (*(_DWORD*)&byte_587000[33396]) {
-			result = (char**)&byte_587000[33396];
-			v1 = &byte_587000[33396];
+	result = *(char***)getMemAt(0x5D4594, 371248);
+	if (!*(_DWORD*)getMemAt(0x5D4594, 371248)) {
+		result = *(char***)getMemAt(0x587000, 33396);
+		if (*(_DWORD*)getMemAt(0x587000, 33396)) {
+			result = (char**)getMemAt(0x587000, 33396);
+			v1 = getMemAt(0x587000, 33396);
 			do {
 				*((_DWORD*)v1 - 1) =
 					loadString_sub_40F1D0(*result, 0, "C:\\NoxPost\\src\\common\\Object\\WeapLook.c", 200);
@@ -20,7 +20,7 @@ char** sub_4158C0() {
 				result = (char**)v1;
 			} while (v2);
 		}
-		*(_DWORD*)&byte_5D4594[371248] = 1;
+		*(_DWORD*)getMemAt(0x5D4594, 371248) = 1;
 	}
 	return result;
 }
