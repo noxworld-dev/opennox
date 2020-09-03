a4056345972b2cc2e0dbbd189c9b55634d2983c8
Denys Smirnov <dennwc@pm.me>
2020-09-03 23:05:07 +0300

    Wrap blob accesses in partrain.c.

--- src/client/draw/partrain.c
+++ src/client/draw/partrain.c
@@ -17,15 +17,15 @@ int __cdecl nox_thing_blue_rain_draw(int a1, nox_drawable* dr) {
 
 	if (nox_common_gameFlags_check_40A5C0(0x200000))
 		return 1;
-	if (!*(_DWORD*)&byte_5D4594[1313716])
-		*(_DWORD*)&byte_5D4594[1313716] = sub_44CFC0((CHAR*)&byte_587000[176640]);
+	if (!*(_DWORD*)getMemAt(0x5D4594, 1313716))
+		*(_DWORD*)getMemAt(0x5D4594, 1313716) = sub_44CFC0((CHAR*)getMemAt(0x587000, 176640));
 	v3 = a2;
 	v9 = 2;
 	do {
 		v4 = *(_DWORD*)(v3 + 12) + sub_415FF0(-10, 10, "C:\\NoxPost\\src\\client\\Draw\\PartRain.c", 42);
 		v5 = sub_415FF0(-10, 10, "C:\\NoxPost\\src\\client\\Draw\\PartRain.c", 43);
 		v6 = *(_DWORD*)(v3 + 16) + v5;
-		v7 = sub_45A360_drawable(*(int*)&byte_5D4594[1313716], v4, v6);
+		v7 = sub_45A360_drawable(*(int*)getMemAt(0x5D4594, 1313716), v4, v6);
 		v8 = v7;
 		if (v7) {
 			*(_DWORD*)(v7 + 432) = v4 << 12;
@@ -33,8 +33,8 @@ int __cdecl nox_thing_blue_rain_draw(int a1, nox_drawable* dr) {
 			*(_BYTE*)(v7 + 299) = 0;
 			*(_DWORD*)(v7 + 440) = 0;
 			*(_DWORD*)(v7 + 448) =
-				*(_DWORD*)&byte_5D4594[2598000] + sub_415FF0(90, 120, "C:\\NoxPost\\src\\client\\Draw\\PartRain.c", 63);
-			*(_DWORD*)(v8 + 444) = *(_DWORD*)&byte_5D4594[2598000];
+				*(_DWORD*)getMemAt(0x5D4594, 2598000) + sub_415FF0(90, 120, "C:\\NoxPost\\src\\client\\Draw\\PartRain.c", 63);
+			*(_DWORD*)(v8 + 444) = *(_DWORD*)getMemAt(0x5D4594, 2598000);
 			*(_WORD*)(v8 + 106) = 0;
 			*(_BYTE*)(v8 + 296) = -5;
 			*(_WORD*)(v8 + 104) = v6 - *(_WORD*)(a1 + 20);
@@ -49,10 +49,10 @@ int __cdecl nox_thing_blue_rain_draw(int a1, nox_drawable* dr) {
 int __cdecl nox_thing_levelup_draw(int a1, nox_drawable* dr) {
 	int v2; // eax
 
-	v2 = *(_DWORD*)&byte_5D4594[1313708];
-	if (!*(_DWORD*)&byte_5D4594[1313708]) {
-		v2 = sub_44CFC0((CHAR*)&byte_587000[176492]);
-		*(_DWORD*)&byte_5D4594[1313708] = v2;
+	v2 = *(_DWORD*)getMemAt(0x5D4594, 1313708);
+	if (!*(_DWORD*)getMemAt(0x5D4594, 1313708)) {
+		v2 = sub_44CFC0((CHAR*)getMemAt(0x587000, 176492));
+		*(_DWORD*)getMemAt(0x5D4594, 1313708) = v2;
 	}
 	nox_thing_falling_sparks_draw_4B7740(v2, a1, dr);
 	return 1;
@@ -62,10 +62,10 @@ int __cdecl nox_thing_levelup_draw(int a1, nox_drawable* dr) {
 int __cdecl nox_thing_oblivion_up_draw(int a1, nox_drawable* dr) {
 	int v2; // eax
 
-	v2 = *(_DWORD*)&byte_5D4594[1313712];
-	if (!*(_DWORD*)&byte_5D4594[1313712]) {
-		v2 = sub_44CFC0((CHAR*)&byte_587000[176628]);
-		*(_DWORD*)&byte_5D4594[1313712] = v2;
+	v2 = *(_DWORD*)getMemAt(0x5D4594, 1313712);
+	if (!*(_DWORD*)getMemAt(0x5D4594, 1313712)) {
+		v2 = sub_44CFC0((CHAR*)getMemAt(0x587000, 176628));
+		*(_DWORD*)getMemAt(0x5D4594, 1313712) = v2;
 	}
 	nox_thing_falling_sparks_draw_4B7740(v2, a1, dr);
 	return 1;
