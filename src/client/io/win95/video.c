fe9edeccfa8b4819cda614dbd59dfde3739e4b86
Denys Smirnov <dennwc@pm.me>
2020-09-03 23:09:06 +0300

    Wrap blob accesses in video.c.

--- src/client/io/win95/video.c
+++ src/client/io/win95/video.c
@@ -20,7 +20,7 @@ int sub_430F00_screenshot() {
 	++screenshot_num;
 	FILE* file = fopen(path, "w");
 	if (!file) {
-		v2 = loadString_sub_40F1D0((char*)&byte_587000[80968], 0, "C:\\NoxPost\\src\\Client\\Io\\Win95\\Video.c", 375);
+		v2 = loadString_sub_40F1D0((char*)getMemAt(0x587000, 80968), 0, "C:\\NoxPost\\src\\Client\\Io\\Win95\\Video.c", 375);
 		return sub_4517A0(v2, path);
 	}
 	fprintf(file, "P3\n#Nox Bitmap data\n640 480 256\n");
@@ -29,13 +29,13 @@ int sub_430F00_screenshot() {
 		for (int v7 = 0; v7 < nox_backbuffer_width; v7++) {
 			if (v7 != 0 && (v7 % 60) == 0)
 				fprintf(file, "\n");
-			fprintf(file, "%d %d %d ", byte_5D4594[3 * *(unsigned __int8*)(v7 + v4) + 3804716],
-					byte_5D4594[3 * *(unsigned __int8*)(v7 + v4) + 3804717],
-					byte_5D4594[3 * *(unsigned __int8*)(v7 + v4) + 3804718]);
+			fprintf(file, "%d %d %d ", getMemByte(0x5D4594, 3 * *(unsigned __int8*)(v7 + v4) + 3804716),
+					getMemByte(0x5D4594, 3 * *(unsigned __int8*)(v7 + v4) + 3804717),
+					getMemByte(0x5D4594, 3 * *(unsigned __int8*)(v7 + v4) + 3804718));
 		}
 		v4 += nox_pitch_3801808;
 	}
-	v8 = loadString_sub_40F1D0((char*)&byte_587000[81068], 0, "C:\\NoxPost\\src\\Client\\Io\\Win95\\Video.c", 399);
+	v8 = loadString_sub_40F1D0((char*)getMemAt(0x587000, 81068), 0, "C:\\NoxPost\\src\\Client\\Io\\Win95\\Video.c", 399);
 	sub_4517A0(v8, path);
 	return fclose(file);
 }
