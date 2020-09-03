88912d23c4618215e747330b44a0f69fc58c1c05
Denys Smirnov <dennwc@pm.me>
2020-09-03 22:54:30 +0300

    Wrap blob accesses in chaticon.c.

--- src/client/gui/chaticon.c
+++ src/client/gui/chaticon.c
@@ -11,12 +11,12 @@ extern int nox_win_height;
 int sub_445650() {
 	unsigned short* v0; // eax
 
-	*(unsigned int*)&byte_5D4594[825748] = sub_42F970("ChatIcon");
+	*(unsigned int*)getMemAt(0x5D4594, 825748) = sub_42F970("ChatIcon");
 	*(unsigned int*)&dword_5d4594_825744 =
 		nox_window_new(0, 136, nox_win_width - 50, nox_win_height / 2 - 50, 50, 50, 0);
-	sub_46AE60(*(int*)&dword_5d4594_825744, *(int*)&byte_5D4594[825748]);
+	sub_46AE60(*(int*)&dword_5d4594_825744, *(int*)getMemAt(0x5D4594, 825748));
 	nox_window_set_all_funcs(*(unsigned int**)&dword_5d4594_825744, 0, sub_4456E0, 0);
-	v0 = loadString_sub_40F1D0((char*)&byte_587000[107980], 0, "C:\\NoxPost\\src\\client\\Gui\\chaticon.c", 73);
+	v0 = loadString_sub_40F1D0((char*)getMemAt(0x587000, 107980), 0, "C:\\NoxPost\\src\\client\\Gui\\chaticon.c", 73);
 	sub_46B000((unsigned short*)(*(unsigned int*)&dword_5d4594_825744 + 36), v0);
 	return 1;
 }
