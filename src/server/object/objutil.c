28f195ade6d2e746f93e2a7e41969778e4a614f9
Denys Smirnov <dennwc@pm.me>
2020-09-03 23:04:05 +0300

    Wrap blob accesses in objutil.c.

--- src/server/object/objutil.c
+++ src/server/object/objutil.c
@@ -24,41 +24,41 @@ wchar_t* __cdecl sub_4E77E0_obj_util(int a1) {
 		v4 = v3;
 		if (!v3) {
 			v12 = sub_4E39D0(a1);
-			v5 = loadString_sub_40F1D0((char*)&byte_587000[203812], 0, "C:\\NoxPost\\src\\Server\\Object\\objutil.c",
+			v5 = loadString_sub_40F1D0((char*)getMemAt(0x587000, 203812), 0, "C:\\NoxPost\\src\\Server\\Object\\objutil.c",
 									   983);
-			nox_swprintf((wchar_t*)&byte_5D4594[1565660], v5, v12);
-			return (wchar_t*)&byte_5D4594[1565660];
+			nox_swprintf((wchar_t*)getMemAt(0x5D4594, 1565660), v5, v12);
+			return (wchar_t*)getMemAt(0x5D4594, 1565660);
 		}
-		nox_wcscpy((wchar_t*)&byte_5D4594[1565660], (const wchar_t*)&byte_5D4594[1567732]);
+		nox_wcscpy((wchar_t*)getMemAt(0x5D4594, 1565660), (const wchar_t*)getMemAt(0x5D4594, 1567732));
 		if (*v2 && *(_DWORD*)(*v2 + 8)) {
-			nox_wcscat((wchar_t*)&byte_5D4594[1565660], *(const wchar_t**)(*v2 + 8));
-			nox_wcscat((wchar_t*)&byte_5D4594[1565660], L" ");
+			nox_wcscat((wchar_t*)getMemAt(0x5D4594, 1565660), *(const wchar_t**)(*v2 + 8));
+			nox_wcscat((wchar_t*)getMemAt(0x5D4594, 1565660), L" ");
 		}
 		v7 = v2[1];
 		if (v7) {
 			v8 = *(const wchar_t**)(v7 + 8);
 			if (v8) {
-				nox_wcscat((wchar_t*)&byte_5D4594[1565660], v8);
-				nox_wcscat((wchar_t*)&byte_5D4594[1565660], L" ");
+				nox_wcscat((wchar_t*)getMemAt(0x5D4594, 1565660), v8);
+				nox_wcscat((wchar_t*)getMemAt(0x5D4594, 1565660), L" ");
 			}
 		}
 		if (v4[2])
-			nox_wcscat((wchar_t*)&byte_5D4594[1565660], (const wchar_t*)v4[2]);
+			nox_wcscat((wchar_t*)getMemAt(0x5D4594, 1565660), (const wchar_t*)v4[2]);
 		v9 = v2[2];
 		if (v9 && *(_DWORD*)(v9 + 8)) {
-			nox_wcscat((wchar_t*)&byte_5D4594[1565660], L" ");
-			nox_wcscat((wchar_t*)&byte_5D4594[1565660], *(const wchar_t**)(v2[2] + 8));
+			nox_wcscat((wchar_t*)getMemAt(0x5D4594, 1565660), L" ");
+			nox_wcscat((wchar_t*)getMemAt(0x5D4594, 1565660), *(const wchar_t**)(v2[2] + 8));
 		}
 		v10 = v2[3];
 		if (v10 && *(_DWORD*)(v10 + 12)) {
-			nox_wcscat((wchar_t*)&byte_5D4594[1565660], L" ");
-			nox_wcscat((wchar_t*)&byte_5D4594[1565660], *(const wchar_t**)(v2[3] + 12));
-			return (wchar_t*)&byte_5D4594[1565660];
+			nox_wcscat((wchar_t*)getMemAt(0x5D4594, 1565660), L" ");
+			nox_wcscat((wchar_t*)getMemAt(0x5D4594, 1565660), *(const wchar_t**)(v2[3] + 12));
+			return (wchar_t*)getMemAt(0x5D4594, 1565660);
 		}
 	} else {
 		v11 =
-			loadString_sub_40F1D0((char*)&byte_587000[203876], 0, "C:\\NoxPost\\src\\Server\\Object\\objutil.c", 1032);
-		nox_wcscpy((wchar_t*)&byte_5D4594[1565660], v11);
+			loadString_sub_40F1D0((char*)getMemAt(0x587000, 203876), 0, "C:\\NoxPost\\src\\Server\\Object\\objutil.c", 1032);
+		nox_wcscpy((wchar_t*)getMemAt(0x5D4594, 1565660), v11);
 	}
-	return (wchar_t*)&byte_5D4594[1565660];
+	return (wchar_t*)getMemAt(0x5D4594, 1565660);
 }
