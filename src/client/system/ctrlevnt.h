8e816a3600898d61187d20a6c875491151e6ab4f
Denys Smirnov <dennwc@pm.me>
2021-01-15 23:14:35 +0200

    Refactor mouse handling in nox_xxx_clientControl_42D6B0.

--- src/client/system/ctrlevnt.h
+++ src/client/system/ctrlevnt.h
@@ -4,6 +4,6 @@
 #include "../../defs.h"
 
 char* nox_xxx_bindevent_42EAE0();
-void nox_xxx_clientControl_42D6B0(_DWORD* a3, int a4);
+void nox_xxx_clientControl_42D6B0(nox_mouse_state_t* mouse, int a4);
 
 #endif // NOX_PORT_CLIENT_SYSTEM_CTRLEVNT
