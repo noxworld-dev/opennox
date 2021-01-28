de746cfa31cb42ed1c8c9ce1e5b3fc1328278f5e
Denys Smirnov <dennwc@pm.me>
2021-01-28 19:51:18 +0200

    Split platform into a separate file.

--- src/platform.go
+++ src/platform.go
@@ -2,49 +2,37 @@ package main
 
 import "C"
 import (
-	"math/rand"
 	"time"
+
+	"nox/common/platform"
 )
-
-var (
-	start  = time.Now()
-	random = seedRandomTime()
-)
-
-func timeSeed() int64 {
-	return time.Now().UnixNano()
-}
-
-func seedRandomTime() *rand.Rand {
-	return rand.New(rand.NewSource(timeSeed()))
-}
 
 //export nox_platform_rand
 func nox_platform_rand() C.int {
-	return C.int(random.Int())
+	return C.int(platform.RandInt())
 }
 
 //export nox_platform_srand
 func nox_platform_srand(seed C.uint) {
-	random = rand.New(rand.NewSource(int64(seed)))
+	platform.RandSeed(int64(seed))
 }
 
 //export nox_platform_time_seed
 func nox_platform_time_seed() C.uint {
-	return C.uint(timeSeed())
+	return C.uint(platform.TimeSeed())
 }
 
 //export nox_platform_srand_time
 func nox_platform_srand_time() {
-	random = seedRandomTime()
+	platform.RandSeedTime()
 }
 
 //export nox_platform_get_ticks
 func nox_platform_get_ticks() C.uint {
-	return C.uint(time.Since(start) / time.Millisecond)
+	return C.uint(platform.Ticks() / time.Millisecond)
 }
 
 //export nox_platform_sleep
 func nox_platform_sleep(ms C.uint) {
-	time.Sleep(time.Duration(ms) * time.Millisecond)
+	platform.Sleep(time.Duration(ms) * time.Millisecond)
 }
