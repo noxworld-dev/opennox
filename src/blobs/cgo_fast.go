93b0a0b44284e2710d44c617b182edccc2bec86d
Denys Smirnov <dennwc@pm.me>
2021-01-30 11:45:16 +0200

    Move memmap to a separate package.

---
+++ src/blobs/cgo_fast.go
@@ -0,0 +1,10 @@
+//+build !safe
+
+package blobs
+
+/*
+#cgo CFLAGS: -w -fshort-wchar -fno-strict-aliasing -fno-strict-overflow
+#cgo linux LDFLAGS: -Wl,-unresolved-symbols=ignore-all
+#cgo darwin LDFLAGS: -Wl,-undefined,dynamic_lookup
+*/
+import "C"
