38c577146fc070116f03355fe073fc902da8334b
Denys Smirnov <dennwc@pm.me>
2021-02-03 02:18:08 +0200

    Move console command parsing to Go.

---
+++ src/cgo_strings.go
@@ -0,0 +1,124 @@
+package main
+
+/*
+#include <stddef.h>
+#include <stdlib.h>
+*/
+import "C"
+import (
+	"reflect"
+	"unicode/utf16"
+	"unsafe"
+)
+
+func StrFree(s *C.char) {
+	C.free(unsafe.Pointer(s))
+}
+
+func WStrFree(s *C.wchar_t) {
+	C.free(unsafe.Pointer(s))
+}
+
+func WStrSliceFree(arr []*C.wchar_t) {
+	for _, p := range arr {
+		WStrFree(p)
+	}
+	C.free(unsafe.Pointer(&arr[0]))
+}
+
+func asU16Slice(p unsafe.Pointer, sz int) (out []uint16) {
+	*(*reflect.SliceHeader)(unsafe.Pointer(&out)) = reflect.SliceHeader{
+		Data: uintptr(p),
+		Len:  sz, Cap: sz,
+	}
+	return
+}
+
+func asWStr(p unsafe.Pointer, sz int) (out []C.wchar_t) {
+	*(*reflect.SliceHeader)(unsafe.Pointer(&out)) = reflect.SliceHeader{
+		Data: uintptr(p),
+		Len:  sz, Cap: sz,
+	}
+	return
+}
+
+func asWStrSlice(p unsafe.Pointer, sz int) (out []*C.wchar_t) {
+	*(*reflect.SliceHeader)(unsafe.Pointer(&out)) = reflect.SliceHeader{
+		Data: uintptr(p),
+		Len:  sz, Cap: sz,
+	}
+	return
+}
+
+func StrLen(s *C.char) int {
+	if s == nil {
+		return 0
+	}
+	n := 0
+	for *s != 0 {
+		n++
+		s = (*C.char)(unsafe.Pointer(uintptr(unsafe.Pointer(s)) + 1))
+	}
+	return n
+}
+
+func WStrLen(s *C.wchar_t) int {
+	if s == nil {
+		return 0
+	}
+	n := 0
+	for *s != 0 {
+		n++
+		s = (*C.wchar_t)(unsafe.Pointer(uintptr(unsafe.Pointer(s)) + 2))
+	}
+	return n
+}
+
+func GoString(s *C.char) string {
+	return C.GoString(s)
+}
+
+func CString(s string) *C.char {
+	return C.CString(s)
+}
+
+func GoWString(s *C.wchar_t) string {
+	n := WStrLen(s)
+	if n == 0 {
+		return ""
+	}
+	arr := asU16Slice(unsafe.Pointer(s), n)
+	return string(utf16.Decode(arr))
+}
+
+func CWString(s string) *C.wchar_t {
+	buf := utf16.Encode([]rune(s))
+	ptr := C.calloc(C.uint(len(buf)+1), 2)
+	copy(asU16Slice(ptr, len(buf)), buf)
+	return (*C.wchar_t)(ptr)
+}
+
+func GoWStrSlice(arr **C.wchar_t) []string {
+	n := PtrArrLen(unsafe.Pointer(arr))
+	return GoWStrSliceN(arr, n)
+}
+
+func GoWStrSliceN(arr **C.wchar_t, n int) []string {
+	if n == 0 {
+		return nil
+	}
+	out := make([]string, 0, n)
+	for _, c := range asWStrSlice(unsafe.Pointer(arr), n) {
+		out = append(out, GoWString(c))
+	}
+	return out
+}
+
+func CWStrSlice(arr []string) []*C.wchar_t {
+	ptr := C.calloc(C.uint(len(arr)+1), C.uint(ptrSize))
+	out := asWStrSlice(ptr, len(arr))
+	for i, s := range arr {
+		out[i] = CWString(s)
+	}
+	return out
+}
--- src/strman.go
+++ src/strman.go
@@ -1,16 +1,12 @@
 package main
 
 /*
-#include <wchar.h>
+#include <stddef.h>
 #include <stdlib.h>
 */
 import "C"
 import (
 	"log"
-	"reflect"
-	"unicode/utf16"
-	"unsafe"
-
 	"nox/common/strman"
 )
 
@@ -31,23 +27,12 @@ func internCStr(s string) *C.char {
 	return p
 }
 
-func asU16Slice(p unsafe.Pointer, sz int) (out []uint16) {
-	*(*reflect.SliceHeader)(unsafe.Pointer(&out)) = reflect.SliceHeader{
-		Data: uintptr(p),
-		Len:  sz, Cap: sz,
-	}
-	return
-}
-
 func internWStr(s string) *C.wchar_t {
 	p, ok := strManW[s]
 	if ok {
 		return p
 	}
-	buf := utf16.Encode([]rune(s))
-	ptr := C.calloc(C.uint(len(buf)+1), 2)
-	copy(asU16Slice(ptr, len(buf)), buf)
-	p = (*C.wchar_t)(ptr)
+	p = CWString(s)
 	strManW[s] = p
 	return p
 }
@@ -78,11 +63,11 @@ func strmanReadFile(path string) error {
 func nox_strman_loadString_40F1D0(name *C.char, strOut **C.char, srcFile *C.char, srcLine C.int) *C.wchar_t {
 	if strOut != nil {
 		*strOut = nil
-		s1, s2 := strMan.GetString2InFile(C.GoString(srcFile), C.GoString(name))
+		s1, s2 := strMan.GetString2InFile(C.GoString(name), C.GoString(srcFile))
 		*strOut = internCStr(s2)
 		return internWStr(s1)
 	}
-	s := strMan.GetStringInFile(C.GoString(srcFile), C.GoString(name))
+	s := strMan.GetStringInFile(C.GoString(name), C.GoString(srcFile))
 	return internWStr(s)
 }
 
@@ -91,10 +76,10 @@ func nox_strman_free_410020() {
 	strMan = strman.New()
 	strManDone = false
 	for _, p := range strManC {
-		C.free(unsafe.Pointer(p))
+		StrFree(p)
 	}
 	for _, p := range strManW {
-		C.free(unsafe.Pointer(p))
+		WStrFree(p)
 	}
 	strManC = make(map[string]*C.char)
 	strManW = make(map[string]*C.wchar_t)
