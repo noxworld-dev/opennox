1e1893fe5c5566e44b885b41961f0e69fca41d32
Denys Smirnov <dennwc@pm.me>
2021-02-06 22:53:41 +0200

    Move memmap to Go and add more checks.

--- src/cgo_safe.go
+++ src/cgo_safe.go
@@ -7,14 +7,21 @@ package main
 typedef unsigned int uintptr_t;
 */
 import "C"
-import "unsafe"
+import (
+	"nox/common/memmap"
+	"unsafe"
+)
+
+func init() {
+	memmap.SetRuntimeChecks(true)
+}
 
 //export mem_getPtr
 func mem_getPtr(base, off C.uintptr_t) unsafe.Pointer {
-	return Ptr(uintptr(base), uintptr(off))
+	return memmap.PtrOff(uintptr(base), uintptr(off))
 }
 
 //export mem_getPtrSize
 func mem_getPtrSize(base, off, size C.uintptr_t) unsafe.Pointer {
-	return PtrSize(uintptr(base), uintptr(off), uintptr(size))
+	return memmap.PtrSizeOff(uintptr(base), uintptr(off), uintptr(size))
 }
