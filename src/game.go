ffe148618327d66b5cca6a122311ef32e968ea4e
Denys Smirnov <dennwc@pm.me>
2021-02-25 22:51:20 +0200

    Allow running a command on server startup.

--- src/game.go
+++ src/game.go
@@ -3,12 +3,21 @@ package main
 /*
 #include "proto.h"
 #include "client__shell__noxworld.h"
+#include "client__system__parsecmd.h"
+#include "common__magic__comguide.h"
+#include "client__drawable__drawdb.h"
 extern unsigned int nox_game_createOrJoin_815048;
 extern unsigned int dword_5d4594_815132;
+extern unsigned int dword_5d4594_1556112;
+extern unsigned int dword_5d4594_811372;
+extern int nox_win_width;
+extern int nox_win_height;
+extern obj_5D4594_811068_t obj_5D4594_811068;
 */
 import "C"
 import (
 	"nox/common/alloc"
+	"nox/common/memmap"
 	"unsafe"
 )
 
@@ -20,7 +29,7 @@ func nox_game_setMovieFile_4CB230(name string, out *C.char) bool {
 
 //export nox_game_rollLogoAndStart_4AB1F0
 func nox_game_rollLogoAndStart_4AB1F0() C.int {
-	if isServer {
+	if isServer || isServerQuest {
 		return C.int(startServer())
 	}
 	path := (*C.char)(alloc.Calloc(128, 1))
@@ -73,17 +82,28 @@ func startServer() int {
 	setEngineFlag(NOX_ENGINE_FLAG_5)
 	resetEngineFlag(NOX_ENGINE_FLAG_6)
 	setGameFlag(0x2000)
-	setGameFlag(0x10000)
+	if !isServerQuest {
+		setGameFlag(0x10000)
+	} else {
+		unsetGameFlag(0x10000)
+	}
 	unsetGameFlag(2048)
 	C.sub_461440(0)
 	C.sub_4D6F40(0)
 	C.sub_4D6F90(0)
-	C.sub_4D6F60(0)
+	if !isServerQuest {
+		C.sub_4D6F60(0)
+	} else {
+		C.sub_4D6F60(1)
+		C.nox_xxx_cliShowHideTubes_470AA0(1)
+	}
 	C.sub_4D6F80(0)
 	if C.sub_473670() == 0 {
 		C.sub_473610()
 	}
-	C.nox_xxx_cliShowHideTubes_470AA0(0)
+	if !isServerQuest {
+		C.nox_xxx_cliShowHideTubes_470AA0(0)
+	}
 	C.nox_xxx_cliSetMinimapZoom_472520(2300)
 	C.sub_43AF50(0)
 	if C.nox_xxx_parseGamedataBinPre_4D1630() == 0 {
@@ -107,4 +127,113 @@ func nox_xxx_serverHost_43B4D0() {
 		C.sub_43B440()
 	}
 	C.sub_49FF20()
+}
+
+//export nox_xxx_initGameSession_435CC0
+func nox_xxx_initGameSession_435CC0() C.int {
+	C.sub_445450()
+	C.sub_45DB90()
+	C.sub_41D1A0(0)
+	C.nox_xxx_initTime_435570()
+	C.dword_5d4594_1556112 = 0
+
+	if C.nox_alloc_drawable_init(5000) == 0 {
+		return 0
+	}
+
+	if C.sub_49A8E0_init() == 0 {
+		return 0
+	}
+
+	if C.nox_xxx_allocArrayHealthChanges_49A5F0() == 0 {
+		return 0
+	}
+
+	if C.nox_xxx_parseThingBinClient_44C840_read_things() == nil {
+		return 0
+	}
+
+	if C.nox_xxx_loadGuides_427070() == 0 {
+		return 0
+	}
+
+	if C.sub_494F00() == 0 {
+		return 0
+	}
+
+	if C.nox_xxx_wnd_473680() == 0 {
+		return 0
+	}
+
+	C.nox_alloc_npcs()
+	if C.nox_xxx_loadReflSheild_499360() == 0 {
+		return 0
+	}
+
+	C.sub_485F80()
+	if C.sub_4960B0() == 0 {
+		return 0
+	}
+
+	if C.sub_473A40() == 0 {
+		return 0
+	}
+
+	if C.nox_xxx_allocArrayDrawableFX_495AB0() == 0 {
+		return 0
+	}
+
+	if C.nox_xxx_allocClassListFriends_495980() == 0 {
+		return 0
+	}
+
+	C.sub_4958F0()
+	C.nox_xxx_gameSetCliConnected_43C720(0)
+	setGameFlag(0x800000)
+	if getGameFlag(1) {
+		C.nox_xxx_netPlayerIncomingServ_4DDF60(31)
+	} else {
+		C.nox_xxx_netSendIncomingClient_43CB00()
+	}
+	C.nox_game_SetCliDrawFunc((*[0]byte)(C.nox_xxx_client_435F80_draw))
+	C.dword_5d4594_811372 = 3
+	*memmap.PtrUint32(0x587000, 85720) = 1
+	C.obj_5D4594_811068.field_0 = 0
+	C.obj_5D4594_811068.field_1 = 0
+	C.obj_5D4594_811068.field_2 = C.uint(C.nox_win_width - 1)
+	C.obj_5D4594_811068.field_3 = C.uint(C.nox_win_height - 1)
+	C.obj_5D4594_811068.field_8 = C.int(C.nox_win_width)
+	C.obj_5D4594_811068.field_9 = C.int(C.nox_win_height)
+	C.obj_5D4594_811068.field_10 = 0
+	C.obj_5D4594_811068.field_11 = 0
+	C.obj_5D4594_811068.field_12 = 0
+	v1 := C.sub_4766D0()
+	C.sub_476700(v1, 0)
+	if getGameFlag(2048) {
+		C.sub_41CC00((*C.char)(memmap.PtrOff(0x5D4594, 2660688)))
+	} else if C.sub_4D6F50() != 0 || C.sub_4D6F70() != 0 {
+		if C.sub_4D6F50() != 0 || C.sub_4D6F70() != 0 {
+			C.sub_460380()
+			C.nox_xxx_cliPrepareGameplay1_460E60()
+			C.nox_xxx_cliPrepareGameplay2_4721D0()
+		}
+		if !getGameFlag(1) {
+			C.sub_41CC00((*C.char)(memmap.PtrOff(0x5D4594, 2660688)))
+		}
+	}
+	C.nox_xxx_plrLoad_41A480((*C.char)(memmap.PtrOff(0x5D4594, 2660688)))
+	nox_server_parseCmdText_443C80("execrul autoexec.rul", 1)
+	if isServer && serverExec != "" {
+		old := parseCmd.Cheats()
+		parseCmd.SetCheats(true)
+		nox_server_parseCmdText_443C80(serverExec, 1)
+		parseCmd.SetCheats(old)
+	}
+	C.sub_4951C0()
+	C.sub_465DE0(0)
+	return 1
+}
+
+func nox_server_parseCmdText_443C80(cmd string, flag int) int {
+	return int(C.nox_server_parseCmdText_443C80(internWStr(cmd), C.int(flag)))
 }
