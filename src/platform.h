4bde26599ffe7c84c6f78b21d50c01e5e8ff64e2
Denys Smirnov <dennwc@pm.me>
2021-02-26 20:17:27 +0200

    Remove incomplete E2E tests. Needs a rewrite anyway.

--- src/platform.h
+++ src/platform.h
@@ -14,17 +14,4 @@ void nox_platform_sleep(unsigned int ms);
 int nox_SDL_PollEvent(SDL_Event* event);
 Uint8 nox_SDL_GetEventState(Uint32 type);
 
-#ifdef NOX_E2E_TEST
-void script_add_event(SDL_Event e);
-void script_exit();
-void script_wait(unsigned int dt);
-void script_move(int dx, int dy);
-void script_move_to(int x, int y);
-void script_hold(int btn);
-void script_release(int btn);
-void script_click(int btn);
-void nox_platform_time_hook();
-void init_script_events();
-#endif
-
 #endif // NOX_PLATFORM_H
