8cff98565306b22ce8c7882520b460669cdec1cf
Denys Smirnov <dennwc@pm.me>
2021-02-27 17:29:57 +0200

    Remove nop calls to nullsub_4.

--- src/client__io__console.c
+++ src/client__io__console.c
@@ -29,7 +29,7 @@ unsigned __int8* sub_451630() {
 	if (!nox_file_log) {
 		v4 = nox_strman_loadString_40F1D0("FatalError", 0, "C:\\NoxPost\\src\\Client\\Io\\Console.c", 272);
 		v3 = nox_strman_loadString_40F1D0("CantOpenLog", 0, "C:\\NoxPost\\src\\Client\\Io\\Console.c", 271);
-		nullsub_4(nox_video_getWindow_401FD0(), v3, v4, 0);
+		// nullsub_4(nox_video_getWindow_401FD0(), v3, v4, 0);
 		nox_exit(0);
 	}
 	v1 = getMemAt(0x5D4594, 835880);
@@ -46,7 +46,6 @@ unsigned __int8* sub_451630() {
 //----- (004516C0) --------------------------------------------------------
 void sub_4516C0(wchar_t* a1, ...) {
 	unsigned short* v1; // eax
-	int v2;             // eax
 	unsigned short* v3; // [esp-8h] [ebp-8h]
 	va_list va;         // [esp+8h] [ebp+8h]
 
@@ -60,8 +59,7 @@ void sub_4516C0(wchar_t* a1, ...) {
 	fprintf(nox_file_log, "%S", getMemAt(0x5D4594, 833752));
 	fflush(nox_file_log);
 	v3 = nox_strman_loadString_40F1D0("FatalError", 0, (int)"C:\\NoxPost\\src\\Client\\Io\\Console.c", 324);
-	v2 = nox_video_getWindow_401FD0();
-	nullsub_4(v2, getMemAt(0x5D4594, 833752), v3, 0);
+	// nullsub_4(nox_video_getWindow_401FD0(), getMemAt(0x5D4594, 833752), v3, 0);
 	fprintf(nox_file_log, "exiting..\n");
 	fclose(nox_file_log);
 	if (*(unsigned int*)&dword_5d4594_823776)
@@ -88,7 +86,7 @@ wchar_t* sub_4517A0(wchar_t* a1, ...) {
 	fprintf(nox_file_log, "%S", getMemAt(0x5D4594, 833752));
 	fflush(nox_file_log);
 	v5 = nox_strman_loadString_40F1D0("WarningMessage", 0, "C:\\NoxPost\\src\\Client\\Io\\Console.c", 365);
-	nullsub_4(nox_video_getWindow_401FD0(), getMemAt(0x5D4594, 833752), v5, 0);
+	// nullsub_4(nox_video_getWindow_401FD0(), getMemAt(0x5D4594, 833752), v5, 0);
 	return nox_wcsncpy(v1, (const wchar_t*)getMemAt(0x5D4594, 833752), 0x63u);
 }
 
