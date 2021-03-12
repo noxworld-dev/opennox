baf3104c9127ec7a98d019de79ce5627eac51356
Denys Smirnov <dennwc@pm.me>
2021-03-12 19:04:25 +0200

    Refactor script strings and ncobj parsing.

--- src/server__script__internal.h
+++ src/server__script__internal.h
@@ -4,7 +4,8 @@
 void nox_script_push(int val);
 int nox_script_pop();
 unsigned int nox_script_stack_at(int i);
-int  nox_xxx_addScriptString_512E40(char* a1);
+int  nox_script_addString_512E40(char* a1);
+const char* nox_script_getString_512E40(int i);
 int  nox_server_scriptValToObjectPtr_511B60(int a1);
 const char* nox_script_getField36(int i);
 unsigned int nox_script_getField40(int i);
--- src/server__script__script.h
+++ src/server__script__script.h
@@ -1,6 +1,7 @@
 #ifndef NOX_SERVER_SCRIPT_SCRIPT_H
 #define NOX_SERVER_SCRIPT_SCRIPT_H
 
+int nox_script_ncobj_parse_505360();
 void nox_script_callByIndex_507310(int index, int a1, int a2);
 void nox_script_callOnEvent(const char* event, int a1, int a2);
 int  nox_script_indexByEvent(const char* a1);
