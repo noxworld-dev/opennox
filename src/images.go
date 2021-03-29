21a1dc35aafe0f7573307e99cbeb5c137c9bfe3d
Denys Smirnov <dennwc@pm.me>
2021-03-29 20:40:22 +0300

    Support jpeg sprites.

--- src/images.go
+++ src/images.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"image"
+	"image/jpeg"
 	"image/png"
 	"io/ioutil"
 	"log"
@@ -44,6 +45,23 @@ func loadAndIndexVideoBag() error {
 	return nil
 }
 
+func openImage(base string) (image.Image, error) {
+	isJPG := false
+	f, err := os.Open(base + ".png")
+	if os.IsNotExist(err) {
+		f, err = os.Open(base + ".jpg")
+		isJPG = true
+	}
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	if !isJPG {
+		return png.Decode(f)
+	}
+	return jpeg.Decode(f)
+}
+
 func imageByBagSection(sect, offs int) (image.Image, image.Point, error) {
 	videoBag.once.Do(func() {
 		if err := loadAndIndexVideoBag(); err != nil {
@@ -65,15 +83,10 @@ func imageByBagSection(sect, offs int) (image.Image, image.Point, error) {
 	ext := path.Ext(img.Name)
 	base := strings.TrimSuffix(img.Name, ext)
 	base = filepath.Join(getDataPath(), "images", base)
-	f, err := os.Open(base + ".png")
+	im, err := openImage(base)
 	if os.IsNotExist(err) {
 		return nil, image.Point{}, nil
 	} else if err != nil {
-		return nil, image.Point{}, err
-	}
-	defer f.Close()
-	im, err := png.Decode(f)
-	if err != nil {
 		return nil, image.Point{}, err
 	}
 	var pt image.Point
