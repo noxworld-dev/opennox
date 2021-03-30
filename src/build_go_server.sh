fbc5f158a818ee57941a6f9b2a668272b24bb0b7
Denys Smirnov <dennwc@pm.me>
2021-03-30 17:06:52 +0300

    Xwis lib is now public.

--- src/build_go_server.sh
+++ src/build_go_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-GOPRIVATE=github.com/noxworld-dev/xwis CGO_CFLAGS_ALLOW="(-fshort-wchar)|(-fno-strict-aliasing)|(-fno-strict-overflow)" CGO_ENABLED=1 GOARCH=386 go build -v -tags server -o nox_server
+CGO_CFLAGS_ALLOW="(-fshort-wchar)|(-fno-strict-aliasing)|(-fno-strict-overflow)" CGO_ENABLED=1 GOARCH=386 go build -v -tags server -o nox_server
