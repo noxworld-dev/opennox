4ca90a712dc3cd69c10141a3956878d38033a077
Denys Smirnov <dennwc@pm.me>
2021-04-12 11:45:22 +0300

    Add a few more memmap checks and expose API.

--- src/common/memmap/checks.go
+++ src/common/memmap/checks.go
@@ -1,6 +1,7 @@
 package memmap
 
 import (
+	"bytes"
 	"fmt"
 	"sync/atomic"
 )
@@ -19,5 +20,20 @@ func checkAddr(addr uintptr) {
 	if v := VariableByAddr(addr); v != nil {
 		blob, off := RelativeAddr(addr)
 		panic(fmt.Errorf("intersection with %s at (0x%X, %d)", v.Name, blob, off))
+	}
+}
+
+// ValidateZeros checks if mapped regions are still set to zero as they should be.
+func ValidateZeros() {
+	for _, v := range Variables() {
+		b := v.Blob()
+		off := v.Addr - b.Addr
+		data := b.Data[off:]
+		zero := b.Initial[off:]
+		data = data[:v.Size]
+		zero = zero[:v.Size]
+		if !bytes.Equal(data, zero) {
+			panic(fmt.Errorf("found non-nil data for %s at (0x%X, %d)", v.Name, b.Addr, off))
+		}
 	}
 }
