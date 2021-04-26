65a6df031fd2c68db9d6d810a86124a78aeb24bd
Denys Smirnov <dennwc@pm.me>
2021-04-26 14:26:01 +0300

    Implement CanRead for Go sockets on Win.

---
+++ src/cgo_net_linux.go
@@ -0,0 +1,18 @@
+//+build linux
+
+package main
+
+/*
+#include <sys/ioctl.h>
+*/
+import "C"
+import (
+	"syscall"
+	"unsafe"
+)
+
+func netCanRead(fd uintptr) (uint32, syscall.Errno) {
+	var n uint32
+	_, _, err := syscall.Syscall(syscall.SYS_IOCTL, fd, uintptr(C.FIONREAD), uintptr(unsafe.Pointer(&n)))
+	return n, err
+}
