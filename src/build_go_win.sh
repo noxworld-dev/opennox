00601b742fb852ab24dfa9571a66ec6c47fed383
Denys Smirnov <dennwc@pm.me>
2021-04-26 18:09:00 +0300

    Hide console window on Win.

--- src/build_go_win.sh
+++ src/build_go_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-CGO_CFLAGS_ALLOW="(-fshort-wchar)|(-fno-strict-aliasing)|(-fno-strict-overflow)" CGO_ENABLED=1 GOARCH=386 GOOS=windows CXX_FOR_TARGET=i686-w64-mingw32-g++ CC_FOR_TARGET=i686-w64-mingw32-gcc CC=i686-w64-mingw32-gcc go build -o noxg.exe
+CGO_CFLAGS_ALLOW="(-fshort-wchar)|(-fno-strict-aliasing)|(-fno-strict-overflow)" CGO_ENABLED=1 GOARCH=386 GOOS=windows CXX_FOR_TARGET=i686-w64-mingw32-g++ CC_FOR_TARGET=i686-w64-mingw32-gcc CC=i686-w64-mingw32-gcc go build -v -ldflags "-H windowsgui" -o noxg.exe
