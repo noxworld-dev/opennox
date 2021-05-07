d583bb58592b1302d859bc3a740b0953b15f3f1f
Denys Smirnov <dennwc@pm.me>
2021-05-07 11:07:48 +0300

    Refactor opcode 24 in scripts.

--- src/server__script__script.c
+++ src/server__script__script.c
@@ -671,15 +671,13 @@ void nox_script_callByIndex_507310(int index, int a2, int a3) {
 			sa1 = nox_script_pop();
 			sa2 = nox_script_pop();
 			if (nox_script_pop()) {
-				*(unsigned int*)((char*)(nox_script_arr_xxx_1599636[1].field_28) + 4 * sa2) = sa1;
-				nox_script_push(sa1);
+				nox_script_arr_xxx_1599636[1].field_28[sa2] = sa1;
+			} else if (sa2 < 0) {
+                *(unsigned int*)(*(unsigned int*)((int)a3 + 760) - 4 * sa2) = sa1;
 			} else {
-				if (sa2 < 0)
-					*(unsigned int*)(*(unsigned int*)(v3 + 760) - 4 * sa2) = sa1;
-				else
-					*(unsigned int*)((unsigned int)(script->field_28) + 4 * sa2) = sa1;
-				nox_script_push(sa1);
+				script->field_28[sa2] = sa1;
 			}
+			nox_script_push(sa1);
 			continue;
 		case 23: // set xxx float
 			sf1 = nox_script_popf();
