92809622cd831feb517df677db82ff7a8d527185
Denys Smirnov <dennwc@pm.me>
2021-06-07 15:14:56 +0300

    Optimize blob access rewrite.

--- src/internal/blobs/rewrite.go
+++ src/internal/blobs/rewrite.go
@@ -6,12 +6,16 @@ import (
 	"io/ioutil"
 	"log"
 	"path/filepath"
+	"time"
 )
 
 // RewriteAccess rewrite blob accesses in Path using provided function.
 // The function must change a pointer value and return true if the change must be persisted.
 func RewriteAccess(fnc func(a *Access) (bool, error)) error {
+	var buf bytes.Buffer
 	return EachFile(func(path string) error {
+		buf.Reset()
+		start := time.Now()
 		data, err := ReadFile(path)
 		if err != nil {
 			return err
@@ -23,6 +27,9 @@ func RewriteAccess(fnc func(a *Access) (bool, error)) error {
 		}
 		changed := 0
 		for {
+			if changed == 1 {
+				log.Println("rewriting", path)
+			}
 			fs := token.NewFileSet()
 			acc, err := blobAccesses(fs, path, data)
 			if err != nil {
@@ -43,11 +50,22 @@ func RewriteAccess(fnc func(a *Access) (bool, error)) error {
 				}
 			}
 			if sa == nil {
-				if changed == 0 {
-					return nil
+				if changed > 0 {
+					buf.Write(data)
+					if err := ioutil.WriteFile(Path(path), buf.Bytes(), 0644); err != nil {
+						return err
+					}
+					if dt := time.Since(start); dt > time.Second {
+						log.Println("rewriting", path, ":", dt)
+					}
 				}
-				log.Println("rewriting", path)
-				return ioutil.WriteFile(Path(path), data, 0644)
+				return nil
+			}
+			if ext != ".go" {
+				// write unaffected part, leave the rest
+				si := fs.Position(sa.Expr.Pos[0])
+				buf.Write(data[:si.Offset])
+				data = data[si.Offset:]
 			}
 			// replace access and parse again; it's slow but correct
 			data = bytes.Replace(data, []byte(old.Val), []byte(sa.String()), 1)
