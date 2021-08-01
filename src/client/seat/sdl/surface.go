5a688dad5b3a5f88780441186225e2c6e38b3333
Denys Smirnov <dennwc@pm.me>
2021-08-01 21:41:33 +0300

    Reuse SDL surface.

--- src/client/seat/sdl/surface.go
+++ src/client/seat/sdl/surface.go
@@ -2,75 +2,74 @@ package sdl
 
 import (
 	"image"
+	"sync"
 
 	"github.com/veandco/go-sdl2/sdl"
 
-	noxcolor "nox/v1/common/color"
+	"nox/v1/common/alloc"
 	"nox/v1/common/types"
 )
 
 type Surface struct {
-	win *Window
-	p   *sdl.Surface
+	win   *Window
+	sz    types.Size
+	pitch int
+	mu    sync.Mutex
+	data  []byte
+	tex   *sdl.Texture
 }
 
 func (s *Surface) Lock() bool {
-	return s.p.Lock() == nil
+	s.mu.Lock()
+	return true
 }
 
 func (s *Surface) Unlock() {
-	s.p.Unlock()
+	s.mu.Unlock()
 }
 
 func (s *Surface) Bytes() []byte {
-	return s.p.Pixels()
+	return s.data
 }
 
 func (s *Surface) Pitch() int {
-	return int(s.p.Pitch)
+	return s.pitch
 }
 
 func (s *Surface) Size() types.Size {
-	return types.Size{W: int(s.p.W), H: int(s.p.H)}
-}
-
-func (s *Surface) SetTransparency(cl noxcolor.Color16) {
-	if s == nil || s.p == nil {
-		return
-	}
-	v := noxcolor.ExtendColor16(cl)
-	key := uint32(uint16(v))
-	s.p.SetColorKey(true, key)
+	return s.sz
 }
 
 func (s *Surface) Present(vp image.Rectangle) {
 	ren := s.win.ren
-	// TODO: maybe find a way to get the underlying buffer
-	tex, err := ren.CreateTextureFromSurface(s.p)
+	rect := &sdl.Rect{W: int32(s.sz.W), H: int32(s.sz.H)}
+	s.mu.Lock()
+	err := s.tex.Update(rect, s.data, s.pitch)
+	s.mu.Unlock()
 	if err != nil {
 		panic(err)
 	}
-	// TODO: reuse the texture
-	defer tex.Destroy()
 	if err := ren.Clear(); err != nil {
 		panic(err)
 	}
-	var srect sdl.Rect
-	s.p.GetClipRect(&srect)
 	svp := sdl.Rect{
 		X: int32(vp.Min.X), Y: int32(vp.Min.Y),
 		W: int32(vp.Dx()), H: int32(vp.Dy()),
 	}
-	if err := ren.Copy(tex, &srect, &svp); err != nil {
+	if err := ren.Copy(s.tex, rect, &svp); err != nil {
 		panic(err)
 	}
 	ren.Present()
 }
 
 func (s *Surface) Destroy() {
-	if s.p != nil {
-		s.p.Free()
-		s.p = nil
+	if s.tex != nil {
+		_ = s.tex.Destroy()
+		s.tex = nil
+	}
+	if s.data != nil {
+		alloc.FreeBytes(s.data)
+		s.data = nil
 	}
 	s.win = nil
 }
