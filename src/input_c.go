faa9fe1c75dce1424f8f5f7dc3c2222315cb8b06
Denys Smirnov <dennwc@pm.me>
2021-10-10 22:22:36 +0300

    Extract input mapping from asc_9800B0.

--- src/input_c.go
+++ src/input_c.go
@@ -883,12 +883,12 @@ func nox_input_scanCodeToAlpha(inp *input.Handler, r keybind.Key) uint16 {
 	}
 	if inputShiftState || inputCapsState && bool(C.iswalpha_go(C.wchar_t(str2u16(noxInputMap[r].lower)))) {
 		if scrollLockStatus {
-			return uint16(asc_9800B0[3*int(r)+264])
+			return str2u16(noxInputMapLangX[r].upper)
 		}
 		return str2u16(noxInputMap[r].upper)
 	}
 	if scrollLockStatus {
-		return uint16(asc_9800B0[3*int(r)])
+		return str2u16(noxInputMapLangX[r].lower)
 	}
 	return str2u16(noxInputMap[r].lower)
 }
@@ -1218,30 +1218,71 @@ var (
 		0x9c: {lower: "\n", upper: "\n"},
 		0xb5: {lower: "/", upper: "/"},
 	}
+	noxInputMapLangX = map[keybind.Key]inputCharMap{
+		0x2:  {lower: "1", upper: "!"},
+		0x3:  {lower: "2", upper: "@"},
+		0x4:  {lower: "3", upper: "#"},
+		0x5:  {lower: "4", upper: "$"},
+		0x6:  {lower: "5", upper: "%"},
+		0x7:  {lower: "6", upper: "^"},
+		0x8:  {lower: "7", upper: "&"},
+		0x9:  {lower: "8", upper: "*"},
+		0xa:  {lower: "9", upper: "("},
+		0xb:  {lower: "0", upper: ")"},
+		0xc:  {lower: "-", upper: "_"},
+		0xd:  {lower: "=", upper: "+"},
+		0xe:  {lower: "\b", upper: "\b"},
+		0xf:  {lower: "\t", upper: "\t"},
+		0x10: {lower: "\xe9", upper: "\xc9"},
+		0x11: {lower: "\xf6", upper: "\xd6"},
+		0x12: {lower: "\xf3", upper: "\xd3"},
+		0x13: {lower: "\xea", upper: "\xca"},
+		0x14: {lower: "\xe5", upper: "\xc5"},
+		0x15: {lower: "\xed", upper: "\xcd"},
+		0x16: {lower: "\xe3", upper: "\xc3"},
+		0x17: {lower: "\xf8", upper: "\xd8"},
+		0x18: {lower: "\xf9", upper: "\xd9"},
+		0x19: {lower: "\xe7", upper: "\xc7"},
+		0x1a: {lower: "\xf5", upper: "\xd5"},
+		0x1b: {lower: "\xfa", upper: "\xda"},
+		0x1c: {lower: "\n", upper: "\n"},
+		0x1e: {lower: "\xf4", upper: "\xd4"},
+		0x1f: {lower: "\xfb", upper: "\xdb"},
+		0x20: {lower: "\xe2", upper: "\xc2"},
+		0x21: {lower: "\xe0", upper: "\xc0"},
+		0x22: {lower: "\xef", upper: "\xcf"},
+		0x23: {lower: "\xf0", upper: "\xd0"},
+		0x24: {lower: "\xee", upper: "\xce"},
+		0x25: {lower: "\xeb", upper: "\xcb"},
+		0x26: {lower: "\xe4", upper: "\xc4"},
+		0x27: {lower: "\xe6", upper: "\xc6"},
+		0x28: {lower: "\xfd", upper: "\xdd"},
+		0x29: {lower: "`", upper: "~"},
+		0x2b: {lower: "\\", upper: "|"},
+		0x2c: {lower: "\xff", upper: "\xdf"},
+		0x2d: {lower: "\xf7", upper: "\xd7"},
+		0x2e: {lower: "\xf1", upper: "\xd1"},
+		0x2f: {lower: "\xec", upper: "\xcc"},
+		0x30: {lower: "\xe8", upper: "\xc8"},
+		0x31: {lower: "\xf2", upper: "\xd2"},
+		0x32: {lower: "\xfc", upper: "\xdc"},
+		0x33: {lower: "\xe1", upper: "\xc1"},
+		0x34: {lower: "\xfe", upper: "\xde"},
+		0x35: {lower: "/", upper: "?"},
+		0x37: {lower: "*", upper: "*"},
+		0x39: {lower: " ", upper: " "},
+		0x47: {lower: "7", upper: "7"},
+		0x48: {lower: "8", upper: "8"},
+		0x49: {lower: "9", upper: "9"},
+		0x4a: {lower: "-", upper: "-"},
+		0x4b: {lower: "4", upper: "4"},
+		0x4c: {lower: "5", upper: "5"},
+		0x4d: {lower: "6", upper: "6"},
+		0x4e: {lower: "+", upper: "+"},
+		0x4f: {lower: "1", upper: "1"},
+		0x50: {lower: "2", upper: "2"},
+		0x51: {lower: "3", upper: "3"},
+		0x52: {lower: "0", upper: "0"},
+		0x53: {lower: ".", upper: "."},
+	}
 )
-
-var asc_9800B0 = []byte{
-	0, 0, 0, 0, 0, 0, 49, 33, 0, 50, 64, 0, 51, 35, 0, 52, 36, 0, 53, 37, 0, 54, 94,
-	0, 55, 38, 0, 56, 42, 0, 57, 40, 0, 48, 41, 0, 45, 95, 0, 61, 43, 0, 8, 8, 0, 9,
-	9, 0, 233, 201, 0, 246, 214, 0, 243, 211, 0, 234, 202, 0, 229, 197, 0, 237, 205, 0, 227, 195, 0,
-	248, 216, 0, 249, 217, 0, 231, 199, 0, 245, 213, 0, 250, 218, 0, 10, 10, 0, 0, 0, 0, 244, 212,
-	0, 251, 219, 0, 226, 194, 0, 224, 192, 0, 239, 207, 0, 240, 208, 0, 238, 206, 0, 235, 203, 0, 228,
-	196, 0, 230, 198, 0, 253, 221, 0, 96, 126, 0, 0, 0, 0, 92, 124, 0, 255, 223, 0, 247, 215, 0,
-	241, 209, 0, 236, 204, 0, 232, 200, 0, 242, 210, 0, 252, 220, 0, 225, 193, 0, 254, 222, 0, 47, 63,
-	0, 0, 0, 0, 42, 42, 0, 0, 0, 0, 32, 32, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 55, 55, 0, 56, 56, 0, 57, 57, 0, 45, 45, 0, 52, 52, 0, 53, 53,
-	0, 54, 54, 0, 43, 43, 0, 49, 49, 0, 50, 50, 0, 51, 51, 0, 48, 48, 0, 46, 46, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 49, 33, 0, 50, 64, 0, 51,
-	35, 0, 52, 36, 0, 53, 37, 0, 54, 94, 0, 55, 38, 0, 56, 42, 0, 57, 40, 0, 48, 41, 0,
-	45, 95, 0, 61, 43, 0, 8, 8, 0, 9, 9, 0, 233, 201, 0, 246, 214, 0, 243, 211, 0, 234, 202,
-	0, 229, 197, 0, 237, 205, 0, 227, 195, 0, 248, 216, 0, 249, 217, 0, 231, 199, 0, 245, 213, 0, 250,
-	218, 0, 10, 10, 0, 0, 0, 0, 244, 212, 0, 251, 219, 0, 226, 194, 0, 224, 192, 0, 239, 207, 0,
-	240, 208, 0, 238, 206, 0, 235, 203, 0, 228, 196, 0, 230, 198, 0, 253, 221, 0, 96, 126, 0, 0, 0,
-	0, 92, 124, 0, 255, 223, 0, 247, 215, 0, 241, 209, 0, 236, 204, 0, 232, 200, 0, 242, 210, 0, 252,
-	220, 0, 225, 193, 0, 254, 222, 0, 47, 63, 0, 0, 0, 0, 42, 42, 0, 0, 0, 0, 32, 32, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 55, 55, 0, 56, 56, 0, 57,
-	57, 0, 45, 45, 0, 52, 52, 0, 53, 53, 0, 54, 54, 0, 43, 43, 0, 49, 49, 0, 50, 50, 0,
-	51, 51, 0, 48, 48, 0, 46, 46, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-}
