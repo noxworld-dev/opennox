1e0fd091cfc864337a7bfb740505207d207a8d37
Denys Smirnov <dennwc@pm.me>
2021-11-15 19:41:56 +0200

    Rename fields in nox_savegame_xxx.

--- src/common__savegame.h
+++ src/common__savegame.h
@@ -14,16 +14,16 @@
 #define NOX_SAVEGAME_XXX_MAX 14
 #pragma pack(push, 1)
 typedef struct nox_savegame_xxx {
-	uint32_t field_0;
-	uint8_t field_4[1024];
+	uint32_t flags; // 0
+	char path[1024]; // 4
 	uint8_t field_1028[160];
-	SYSTEMTIME field_1188;
+	SYSTEMTIME timestamp; // 1188
 	uint8_t field_1204[20];
-	uint8_t field_1224[50];
-	uint8_t field_1274;
+	uint8_t player_name[50]; // 1224
+	uint8_t player_class; // 1274
 	uint8_t field_1275;
 	uint8_t field_1276;
-	uint8_t field_1277;
+	uint8_t stage; // 1277
 } nox_savegame_xxx;
 #pragma pack(pop)
 _Static_assert(sizeof(nox_savegame_xxx) == 1278, "wrong size of nox_savegame_xxx structure!");
