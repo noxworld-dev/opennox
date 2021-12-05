7fd82731716412a8de09712e3600cebaefe62f12
Denys Smirnov <dennwc@pm.me>
2021-12-05 19:39:21 +0200

    Fix sub4C8EC0 draw op.

--- src/draw_image.go
+++ src/draw_image.go
@@ -469,13 +469,13 @@ func (r *NoxRender) sub4C9050u8(dst []uint16, src []byte, op byte, sz int) (_ []
 
 func (r *NoxRender) sub4C8EC0u8(dst []uint16, src []byte, op byte, sz int) (_ []uint16, _ []byte) { // sub_4C8EC0
 	const (
-		rshift = 3
-		gshift = 2
-		bshift = 7
+		rshift = 7 // -10+3
+		gshift = 2 // -5+3
+		bshift = 3 // -0+3
 
-		rmask = 0x001f
+		rmask = 0x7c00
 		gmask = 0x03e0
-		bmask = 0x7c00
+		bmask = 0x001f
 	)
 
 	a := r.p.Alpha()
