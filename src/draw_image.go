b1256508c74520a633d0a2fdda4dd8e364d2610c
Denys Smirnov <dennwc@pm.me>
2021-12-05 19:37:43 +0200

    Fix sub4C8410 draw op.

--- src/draw_image.go
+++ src/draw_image.go
@@ -267,13 +267,13 @@ func (r *NoxRender) sub4C86B0(dst []uint16, src []byte, sz int) (_ []uint16, _ [
 
 func (r *NoxRender) sub4C8410(dst []uint16, src []byte, sz int) (_ []uint16, _ []byte) { // sub_4C8410
 	const (
-		rshift = 3
-		gshift = 2
-		bshift = 7
+		rshift = 7 // -10+3
+		gshift = 2 // -5+3
+		bshift = 3 // -0+3
 
-		rmask = 0x001f
+		rmask = 0x7c00
 		gmask = 0x03e0
-		bmask = 0x7c00
+		bmask = 0x001f
 	)
 	return r.drawOpU16(dst, src, sz, func(c1 uint16, c2 uint16) uint16 {
 		cr1 := (c1 & rmask) >> rshift
