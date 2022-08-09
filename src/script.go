e9e37d69bf1bb1de17182dbe9f86c4161e522314
Denys Smirnov <dennwc@pm.me>
2022-08-09 20:26:10 +0300

    Rename LUA script adapter.

--- src/script.go
+++ src/script.go
@@ -55,28 +55,28 @@ func (s *Server) scriptOnEvent(event script.EventType) {
 	}
 }
 
-type noxScript struct {
+type noxScriptImpl struct {
 	s *Server
 }
 
-func (noxScript) Frame() int {
+func (noxScriptImpl) Frame() int {
 	return int(gameFrame())
 }
 
-func (noxScript) Time() time.Duration {
+func (noxScriptImpl) Time() time.Duration {
 	sec := float64(gameFrame()) / float64(gameFPS())
 	return time.Duration(sec) * time.Second
 }
 
-func (noxScript) BlindPlayers(blind bool) {
+func (noxScriptImpl) BlindPlayers(blind bool) {
 	BlindPlayers(blind)
 }
 
-func (noxScript) CinemaPlayers(v bool) {
+func (noxScriptImpl) CinemaPlayers(v bool) {
 	noxServer.CinemaPlayers(v)
 }
 
-func (s noxScript) Players() []script.Player {
+func (s noxScriptImpl) Players() []script.Player {
 	list := s.s.getPlayers()
 	out := make([]script.Player, 0, len(list))
 	for _, p := range list {
@@ -85,19 +85,19 @@ func (s noxScript) Players() []script.Player {
 	return out
 }
 
-func (noxScript) HostPlayer() script.Player {
+func (noxScriptImpl) HostPlayer() script.Player {
 	return HostPlayer()
 }
 
-func (s noxScript) OnPlayerJoin(fnc func(p script.Player)) {
+func (s noxScriptImpl) OnPlayerJoin(fnc func(p script.Player)) {
 	s.s.OnPlayerJoin(fnc)
 }
 
-func (s noxScript) OnPlayerLeave(fnc func(p script.Player)) {
+func (s noxScriptImpl) OnPlayerLeave(fnc func(p script.Player)) {
 	s.s.OnPlayerLeave(fnc)
 }
 
-func (s noxScript) ObjectTypeByID(id string) script.ObjectType {
+func (s noxScriptImpl) ObjectTypeByID(id string) script.ObjectType {
 	tp := s.s.getObjectTypeByID(id)
 	if tp == nil {
 		return nil
@@ -105,7 +105,7 @@ func (s noxScript) ObjectTypeByID(id string) script.ObjectType {
 	return tp
 }
 
-func (s noxScript) ObjectByID(id string) script.Object {
+func (s noxScriptImpl) ObjectByID(id string) script.Object {
 	obj := s.s.getObjectByID(id)
 	if obj == nil {
 		return nil
@@ -116,7 +116,7 @@ func (s noxScript) ObjectByID(id string) script.Object {
 	return obj
 }
 
-func (s noxScript) ObjectGroupByID(id string) *script.ObjectGroup {
+func (s noxScriptImpl) ObjectGroupByID(id string) *script.ObjectGroup {
 	g := s.s.getObjectGroupByID(id)
 	if g == nil {
 		return nil
@@ -124,7 +124,7 @@ func (s noxScript) ObjectGroupByID(id string) *script.ObjectGroup {
 	return g
 }
 
-func (s noxScript) WaypointByID(id string) script.Waypoint {
+func (s noxScriptImpl) WaypointByID(id string) script.Waypoint {
 	w := s.s.getWaypointByID(id)
 	if w == nil {
 		return nil
@@ -132,7 +132,7 @@ func (s noxScript) WaypointByID(id string) script.Waypoint {
 	return w
 }
 
-func (s noxScript) WaypointGroupByID(id string) *script.WaypointGroup {
+func (s noxScriptImpl) WaypointGroupByID(id string) *script.WaypointGroup {
 	g := s.s.getWaypointGroupByID(id)
 	if g == nil {
 		return nil
@@ -140,7 +140,7 @@ func (s noxScript) WaypointGroupByID(id string) *script.WaypointGroup {
 	return g
 }
 
-func (s noxScript) WallAt(pos types.Pointf) script.Wall {
+func (s noxScriptImpl) WallAt(pos types.Pointf) script.Wall {
 	w := s.s.getWallAt(pos)
 	if w == nil {
 		return nil
@@ -148,7 +148,7 @@ func (s noxScript) WallAt(pos types.Pointf) script.Wall {
 	return w
 }
 
-func (s noxScript) WallNear(pos types.Pointf) script.Wall {
+func (s noxScriptImpl) WallNear(pos types.Pointf) script.Wall {
 	w := s.s.getWallNear(pos)
 	if w == nil {
 		return nil
@@ -156,7 +156,7 @@ func (s noxScript) WallNear(pos types.Pointf) script.Wall {
 	return w
 }
 
-func (s noxScript) WallAtGrid(pos image.Point) script.Wall {
+func (s noxScriptImpl) WallAtGrid(pos image.Point) script.Wall {
 	w := s.s.getWallAtGrid(pos)
 	if w == nil {
 		return nil
@@ -164,7 +164,7 @@ func (s noxScript) WallAtGrid(pos image.Point) script.Wall {
 	return w
 }
 
-func (s noxScript) WallGroupByID(id string) *script.WallGroup {
+func (s noxScriptImpl) WallGroupByID(id string) *script.WallGroup {
 	return s.s.getWallGroupByID(id)
 }
 
@@ -174,14 +174,14 @@ func (c scriptConsole) Print(text string) {
 	noxConsole.Print(console.Color(c), text)
 }
 
-func (noxScript) Console(error bool) script.Printer {
+func (noxScriptImpl) Console(error bool) script.Printer {
 	if error {
 		return scriptConsole(console.ColorLightRed)
 	}
 	return scriptConsole(console.ColorYellow)
 }
 
-func (noxScript) AudioEffect(name string, pos script.Positioner) {
+func (noxScriptImpl) AudioEffect(name string, pos script.Positioner) {
 	// FIXME: trigger audio event
 }
 
@@ -191,6 +191,6 @@ func (scriptGlobalPrint) Print(text string) {
 	PrintToPlayers(text)
 }
 
-func (noxScript) Global() script.Printer {
+func (noxScriptImpl) Global() script.Printer {
 	return scriptGlobalPrint{}
 }
