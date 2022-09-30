22f476c3d38f081b9e047ca919b833520b408607
Jeong, Heon <blmarket@gmail.com>
2022-09-29 21:42:13 -0400

    Merged builtins script after working wall builtins

--- src/script_noxscript_builtins.go
+++ src/script_noxscript_builtins.go
@@ -1113,3 +1113,51 @@ func nox_script_canInteract_513E80() int {
 
 	return 0
 }
+
+func pointUnpack(packed int32) image.Point {
+	return image.Pt(int(packed>>16), int(uint16(packed))) // Or, packed & 0xffff
+}
+
+func nox_script_openSecretWall_511F50() int {
+	s := &noxServer.noxScript
+
+	grid := pointUnpack(s.PopI32())
+	wall := noxServer.getWallAtGrid(grid)
+	if wall != nil {
+		wall.Enable(false)
+	}
+	return 0
+}
+
+func nox_script_closeWall_512040() int {
+	s := &noxServer.noxScript
+
+	grid := pointUnpack(s.PopI32())
+	wall := noxServer.getWallAtGrid(grid)
+	if wall != nil {
+		wall.Enable(false)
+	}
+	return 0
+}
+
+func nox_script_toggleWall_512130() int {
+	s := &noxServer.noxScript
+
+	grid := pointUnpack(s.PopI32())
+	wall := noxServer.getWallAtGrid(grid)
+	if wall != nil {
+		wall.Toggle()
+	}
+	return 0
+}
+
+func nox_script_wallBreak_512290() int {
+	s := &noxServer.noxScript
+
+	grid := pointUnpack(s.PopI32())
+	wall := noxServer.getWallAtGrid(grid)
+	if wall != nil {
+		wall.Destroy()
+	}
+	return 0
+}
