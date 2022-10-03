420291aa1f053506d3c4d5868618dd664d8cf5e4
Denys Smirnov <dennwc@pm.me>
2022-10-04 02:27:16 +0300

    Fix argument for playerInfoStructParser_1.

--- src/GameEx.h
+++ src/GameEx.h
@@ -3,7 +3,7 @@
 
 char getPlayerClassFromObjPtr(int a1);
 char playerInfoStructParser_0(char* a1);
-char playerInfoStructParser_1(int a1, int a2, int* a3);
+char playerInfoStructParser_1(int a1, int* a3);
 char mix_MouseKeyboardWeaponRoll(int playerObj, char a2);
 char playerDropATrap(int playerObj);
 int getFlagValueFromFlagIndex(signed int a1);
