399b406d6e15090eb73eaa08b7f19f152b4178f2
Jeong, Heon <blmarket@gmail.com>
2022-10-07 22:42:16 -0400

    Simplify some math related functions

--- src/noxmath.go
+++ src/noxmath.go
@@ -6,29 +6,18 @@ import (
 	"github.com/noxworld-dev/opennox-lib/types"
 )
 
+// nox_xxx_math_509ED0 converts arbitrary point into angle value which is integer ranged 0-255.
 func nox_xxx_math_509ED0(p types.Pointf) int32 {
 	v := int32((math.Atan2(float64(p.Y), float64(p.X))+6.2831855)*40.743664 + 0.5)
 	return nox_xxx_math_roundDir(v)
 }
 
 func nox_xxx_math_roundDir(v int32) int32 {
-	if v < 0 {
-		v += int32(uint32(255-v) >> 8 << 8)
-	}
-	if v >= 256 {
-		v -= int32(uint32(v) >> 8 << 8)
-	}
-	return v
+	return int32(uint8(v))
 }
 
 func nox_xxx_math_roundDirI16(v int16) uint16 {
-	if v < 0 {
-		v += int16((uint32(255-v) >> 8) << 8)
-	}
-	if v >= 256 {
-		v -= int16((uint32(v) >> 8) << 8)
-	}
-	return uint16(v)
+	return uint16(uint8(v))
 }
 
 func sincosDir(d byte) (cos, sin float32) { // 194136, 194140
