3cc3f3cf3174f78a85b7d5867707cae8fe4962b8
Denys Smirnov <dennwc@pm.me>
2022-10-24 22:41:36 +0300

    Port nox_xxx_parseDraw_535CD0.

--- src/object_parse.go
+++ src/object_parse.go
@@ -2,7 +2,6 @@ package opennox
 
 /*
 #include "defs.h"
-int nox_xxx_parseDraw_535CD0(nox_objectType_t* a1p, nox_memfile* a2p, void* a3);
 */
 import "C"
 import (
@@ -17,6 +16,8 @@ import (
 	"github.com/noxworld-dev/opennox-lib/things"
 
 	"github.com/noxworld-dev/opennox/v1/common/alloc"
+	"github.com/noxworld-dev/opennox/v1/common/memmap"
+	"github.com/noxworld-dev/opennox/v1/common/sound"
 )
 
 type objectFieldFunc func(objt *ObjectType, f *MemFile, str string, buf []byte) error
@@ -232,13 +233,189 @@ var noxObjectFieldByName = map[string]objectFieldFunc{
 	"UPDATE":      nox_xxx_parseUpdate_536620,
 	"USE":         nox_xxx_parseUseFn_5363F0,
 	"XFER":        nox_xxx_parseXFer_5360A0,
-	"DRAW": func(objt *ObjectType, f *MemFile, str string, buf []byte) error {
-		n := copy(buf, str)
-		buf[n] = 0
-		ok := C.nox_xxx_parseDraw_535CD0(objt.C(), f.C(), unsafe.Pointer(&buf[0]))
-		if ok == 0 {
-			return errors.New("parsing draw data failed")
-		}
+	"DRAW":        nox_xxx_parseDraw_535CD0,
+}
+
+var _ = [1]struct{}{}[16-unsafe.Sizeof(monsterAnim{})]
+
+type monsterAnim struct {
+	sound   uint32  // 0, 0
+	data4   [4]byte // 1, 4
+	field8  byte    // 2, 8
+	frames  byte    // 2, 9
+	field10 byte    // 2, 10
+	field11 byte    // 2, 11
+	loop    uint32  // 3, 12
+}
+
+var playerAnimNames = []string{
+	"IDLE",
+	"DIE",
+	"DEAD",
+	"JUMP",
+	"WALK",
+	"WALK_AND_DRAG",
+	"RUN",
+	"RUNNING_JUMP",
+	"PICKUP",
+	"DODGE_LEFT",
+	"DODGE_RIGHT",
+	"ELECTROCUTED",
+	"FALL",
+	"TRIP",
+	"GET_UP",
+	"LAUGH",
+	"POINT",
+	"SIT",
+	"SLEEP",
+	"TALK",
+	"TAUNT",
+	"CAST_SPELL",
+	"CONCENTRATE",
+	"PUNCH_LEFT",
+	"PUNCH_RIGHT",
+	"PUNCH_RIGHT_HOOK",
+	"MACE_STRIKE",
+	"SWORD_STRIKE",
+	"LONG_SWORD_STRIKE",
+	"STAFF_STRIKE",
+	"STAFF_BLOCK",
+	"STAFF_SPELL_BLAST",
+	"STAFF_THRUST",
+	"SHOOT_BOW",
+	"SHOOT_CROSSBOW",
+	"AXE_STRIKE",
+	"GREAT_SWORD_PARRY",
+	"GREAT_SWORD_STRIKE",
+	"GREAT_SWORD_IDLE",
+	"HAMMER_STRIKE",
+	"RAISE_SHIELD",
+	"RECOIL_FORWARD",
+	"RECOIL_BACKWARD",
+	"RECOIL_SHIELD",
+	"CHAKRAM_STRIKE",
+	"BERSERKER_CHARGE",
+	"WARCRY",
+	"GREAT_SWORD_BLOCK_LEFT",
+	"GREAT_SWORD_BLOCK_RIGHT",
+	"GREAT_SWORD_BLOCK_DOWN",
+	"ELECTRIC_ZAP",
+	"DUST",
+	"RECOIL",
+	"SNEAK",
+	"HARPOONTHROW",
+}
+
+func nox_xxx_parseDraw_535CD0(objt *ObjectType, f *MemFile, str string, buf []byte) error {
+	name, _ := f.ReadString8()
+	sectSz := int(f.ReadU64Align())
+	switch name {
+	default:
+		f.Skip(sectSz)
 		return nil
-	},
+	case "MonsterDraw":
+		ud := objt.updateDataMonster()
+		if ud == nil {
+			return errors.New("no monster update data")
+		}
+		anims, _ := alloc.Make([]monsterAnim{}, 16)
+		ud.field_119 = unsafe.Pointer(&anims[0])
+		var hdr [4]byte
+		for {
+			n, _ := f.Read(hdr[:])
+			swap4(hdr[:])
+			switch string(hdr[:n]) {
+			default:
+				return fmt.Errorf("invalid section name: %q", string(hdr[:n]))
+			case "END ":
+				return nil
+			case "STAT":
+				ind := int(f.ReadU8())
+				if ind >= len(anims) {
+					return errors.New("unexpected animation index")
+				}
+				ani := &anims[ind]
+
+				snd, _ := f.ReadString8()
+				ani.sound = uint32(sound.ByName(snd))
+
+				v51, _ := f.ReadBytes8()
+				ani.field8 = byte(copy(ani.data4[:len(v51)], v51))
+
+				ani.frames = f.ReadU8()
+				ani.field10 = f.ReadU8()
+
+				kind, _ := f.ReadString8()
+				ani.loop = uint32(bool2int(kind == "Loop" || kind == "LoopAndFade"))
+
+				for i := 0; i < 8*int(ani.frames); i++ {
+					if f.ReadI32() == -1 {
+						f.Skip(1)
+						f.SkipString8()
+					}
+				}
+			}
+		}
+	case "PlayerDraw":
+		var (
+			hdr [4]byte
+			ssz int
+		)
+		for {
+			n, _ := f.Read(hdr[:])
+			swap4(hdr[:])
+			switch string(hdr[:n]) {
+			default:
+				return fmt.Errorf("invalid section name: %q", string(hdr[:n]))
+			case "END ":
+				return nil
+			case "STAT":
+				name, _ := f.ReadString8()
+				ind := -1
+				for i, s := range playerAnimNames {
+					if name == s {
+						ind = i
+						break
+					}
+				}
+				if ind < 0 {
+					return fmt.Errorf("unsupported player animation: %q", name)
+				}
+				v36 := f.ReadU8()
+				v37 := f.ReadU8()
+				f.SkipString8()
+				playerAnimSetFrameRange(ind, int(v36), int(v37))
+				ssz = int(v36)
+			case "SEQU":
+				f.SkipString8()
+				for i := 0; i < 8*ssz; i++ {
+					if f.ReadI32() == -1 {
+						f.Skip(1)
+						f.SkipString8()
+					}
+				}
+			}
+		}
+	}
+}
+
+func playerAnimSetFrameRange(i int, v1, v2 int) {
+	*memmap.PtrUint32(0x5D4594, 1568412+8*uintptr(i)) = uint32(v1)
+	*memmap.PtrUint32(0x5D4594, 1568416+8*uintptr(i)) = uint32(v2)
+}
+
+func playerAnimGetFrameRange(i int) (_, _ int) {
+	return int(memmap.Uint32(0x5D4594, 1568412+8*uintptr(i))),
+		int(memmap.Uint32(0x5D4594, 1568416+8*uintptr(i)))
+}
+
+//export nox_xxx_animPlayerGetFrameRange_4F9F90
+func nox_xxx_animPlayerGetFrameRange_4F9F90(a1 int, a2, a3 *C.int) {
+	v1, v2 := playerAnimGetFrameRange(a1)
+	if a2 != nil {
+		*a2 = C.int(v1)
+	}
+	if a3 != nil {
+		*a3 = C.int(v2)
+	}
 }
