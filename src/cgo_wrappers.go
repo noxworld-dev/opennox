bdc76a2ee698c5b2540cd6c478a0d110e38bac3e
Denys Smirnov <dennwc@pm.me>
2022-12-19 20:35:16 +0200

    Port nox_xxx_itemApplyUpdateEffect_4FA490.

--- src/cgo_wrappers.go
+++ src/cgo_wrappers.go
@@ -4,6 +4,7 @@ package opennox
 static int go_call_int_void_func(int (*fnc)(void)) { return fnc(); }
 static void go_call_void_ptr_func(void (*fnc)(void*), void* a1) { fnc(a1); }
 static void go_call_void_ptr2_func(void (*fnc)(void*, void*), void* a1, void* a2) { fnc(a1, a2); }
+static void go_call_void_ptr3_func(void (*fnc)(void*, void*, void*), void* a1, void* a2, void* a3) { fnc(a1, a2, a3); }
 static void go_call_void_uint_func(void (*fnc)(unsigned int), unsigned int a1) { fnc(a1); }
 static int go_call_int_voidPtr_func(int (*fnc)(void*), void* a1) { return fnc(a1); }
 static void go_call_void2_func(void (*fnc)(void)) { fnc(); }
@@ -21,6 +22,10 @@ func cgoCallVoidPtrFunc(fnc unsafe.Pointer, a1 unsafe.Pointer) {
 
 func cgoCallVoidPtr2Func(fnc unsafe.Pointer, a1, a2 unsafe.Pointer) {
 	C.go_call_void_ptr2_func((*[0]byte)(fnc), a1, a2)
+}
+
+func cgoCallVoidPtr3Func(fnc unsafe.Pointer, a1, a2, a3 unsafe.Pointer) {
+	C.go_call_void_ptr3_func((*[0]byte)(fnc), a1, a2, a3)
 }
 
 func cgoCallVoidUintFunc(fnc unsafe.Pointer, a1 uint32) {
