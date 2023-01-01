071be715cb0d795f6aa7a02e989a394d8c96e449
Denys Smirnov <dennwc@pm.me>
2023-01-01 17:02:46 +0200

    Port client handler for MSG_PLAYER_QUIT.

--- src/MixPatch.h
+++ src/MixPatch.h
@@ -3,7 +3,6 @@
 
 #include "defs.h"
 
-void OnLibraryNotice_264(uint32_t arg1);
 void OnLibraryNotice_420(uint32_t arg1, uint32_t arg2, uint32_t arg3, uint32_t arg4);
 
 void sub_980523(nox_object_t* a1);
