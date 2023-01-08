d041cc68a0d067a75f9078279ddf49c94e7b8587
Denys Smirnov <dennwc@pm.me>
2023-01-08 18:47:47 +0200

    Move Player structure to the server package.

--- src/server/player.go
+++ src/server/player.go
@@ -8,8 +8,153 @@ import (
 	"github.com/noxworld-dev/opennox/v1/common/alloc"
 )
 
+var _ = [1]struct{}{}[8-unsafe.Sizeof(PlayerNetData{})]
+
+type PlayerNetData struct {
+	Field0 uint16
+	Field2 uint16
+	Frame4 uint32
+}
+
+var _ = [1]struct{}{}[76-unsafe.Sizeof(PlayerJournal{})]
+
+type PlayerJournal struct {
+	EntryBuf [64]byte       // 0, 0
+	Next     *PlayerJournal // 1, 64
+	Prev     *PlayerJournal // 2, 68
+	Field3   uint16         // 3, 72
+	Field4   uint16         // 4, 74, likely just padding
+}
+
+var (
+	_ = [1]struct{}{}[4828-unsafe.Sizeof(Player{})]
+	_ = [1]struct{}{}[2185-unsafe.Offsetof(Player{}.info)]
+	_ = [1]struct{}{}[2282-unsafe.Offsetof(Player{}.Field2282)]
+	_ = [1]struct{}{}[3596-unsafe.Offsetof(Player{}.Frame3596)]
+	_ = [1]struct{}{}[4580-unsafe.Offsetof(Player{}.Field4580)]
+	_ = [1]struct{}{}[4800-unsafe.Offsetof(Player{}.Data4800)]
+)
+
 type Player struct {
-	_ [4828]byte
+	Field0              uint32             // 0, 0
+	Field4              uint32             // 1, 4
+	Field8              uint16             // 2, 8
+	Field10             uint16             // 2, 10
+	Field12             uint16             // 3, 12
+	Field14             uint16             // 3, 14
+	NetData16           [255]PlayerNetData // 4, 16
+	PlayerUnit          *Object            // 514, 2056
+	NetCodeVal          uint32             // 515, 2060
+	PlayerInd           byte               // 516, 2064
+	Field2064_1         byte               // 516, 2065
+	Field2064_2         uint16             // 516, 2066
+	Field2068           uint32             // 517, 2068
+	Field2072           [10]uint16         // 518, 2072
+	Active              byte               // 523, 2092
+	Field2096Buf        [12]byte           // 524, 2096
+	Field2108           uint32             // 527, 2108
+	SerialBuf           [22]byte           // 528, 2112
+	Field2134           byte
+	Field2135           byte
+	Lessons             int32  // 534, 2136
+	Field2140           uint32 // 535, 2140
+	Field2144           uint32 // 536, 2144
+	Field2148           uint32 // 537, 2148
+	Field2152           uint32 // 538, 2152
+	Field2156           uint32 // 539, 2156
+	Field2160           uint32 // 540, 2160
+	GoldVal             uint32 // 541, 2164
+	Data2168            [17]byte
+	info                [97]byte // 2185
+	Field2282           uint16   // 2282
+	CursorX             int32    // 2284
+	CursorY             int32    // 2288
+	Data2292            [30]uint32
+	Data2412            [97]uint32
+	Data2800            [100]uint32
+	Data3200            [50]uint32
+	Data3400            [25]uint32
+	Data3500            [20]uint32
+	Field3580           uint32
+	Field3584           uint32
+	Field3588           uint32
+	Field3592           uint32
+	Frame3596           uint32
+	Field3600           uint32         // 900, 3600
+	Field3604           uint32         // 901, 3604
+	Field3608           uint32         // 902, 3608
+	Field3612           uint32         // 903, 3612
+	Field3616           uint32         // 904, 3616
+	Field3620           uint32         // 905, 3620
+	Field3624           uint32         // 906, 3624
+	CameraFollowObj     *Object        // 907, 3628
+	PosX3632            float32        // 908, 3632
+	PosY3636            float32        // 909, 3636
+	Obj3640             *Object        // 910, 3640
+	Field3644           *PlayerJournal // 911, 3644, pointer to journal
+	Field3648           uint32
+	Field3652           uint32
+	Field3656           uint32
+	Field3660           uint32
+	Field3664           uint32
+	Field3668           uint32
+	Field3672           uint32 // 3672
+	Field3676           byte   // 3676, TODO: status?
+	Field3676_1         byte
+	Field3676_2         uint16
+	Field3680           uint32 // 920, 3680, TODO: some flags?
+	Field3684           uint8  // 921, 3684 // TODO: level?
+	Field3685           uint8
+	Field3686           uint16
+	Field3688           uint32
+	Field3692           uint32
+	SpellLvl            [137]uint32 // 3696
+	BeastScrollLvl      [41]uint32  // 4244
+	Data4408            [23]uint32
+	Data4500            [20]uint32
+	Field4580           unsafe.Pointer // 1145, 4580
+	ProtUnitHPCur       uint32         // 1146, 4584
+	ProtPlayerGold      uint32         // 1147, 4588
+	ProtUnitHPMax       uint32         // 1148, 4592
+	ProtUnitManaCur     uint32         // 1149, 4596
+	ProtUnitManaMax     uint32         // 1150, 4600
+	ProtUnitExperience  uint32         // 1151, 4604
+	ProtUnitMass        uint32         // 1152, 4608
+	ProtUnitBuffs       uint32         // 1153, 4612
+	ProtPlayerClass     uint32         // 1154, 4616
+	ProtPlayerField2235 uint32         // 1155, 4620
+	ProtPlayerField2239 uint32         // 1156, 4624
+	ProtPlayerOrigName  uint32         // 1157, 4628
+	Prot4632            uint32         // 1158, 4632
+	Prot4636            uint32         // 1159, 4636
+	Prot4640            uint32         // 1160, 4640
+	ProtPlayerLevel     uint32         // 1161, 4644
+	Field4648           int32          // 1162, 4648
+	Field4652           uint32         // 1163, 4652
+	Field4656           uint32         // 1164, 4656
+	Field4660           uint32         // 1165, 4660
+	Field4664           uint32         // 1166, 4664
+	Field4668           uint32         // 1167, 4668
+	Field4672           uint32         // 1168, 4672
+	Field4676           uint32         // 1169, 4676
+	Field4680           uint32         // 1170, 4680
+	Field4684           uint32         // 1171, 4684
+	Field4688           uint32         // 1172, 4688
+	Field4692           uint32         // 1173, 4692
+	Field4696           uint32         // 1174, 4696
+	Field4700           uint32         // 1175, 4700
+	NameFinal           [28]uint16     // 4704, server-approved player name // TODO: size is a wild guess
+	Field4760           [4]byte        // 1190, 4760
+	Field4764           uint32         // 1191, 4764
+	Field4768           uint32         // 1192, 4768
+	Field4772           uint32         // 1193, 4772
+	Field4776           uint32         // 1194, 4776
+	Field4780           uint32         // 1195, 4780
+	Field4784           uint32         // 1196, 4784
+	Field4788           uint32         // 1197, 4788
+	Field4792           uint32         // 1198, 4792
+	Field4796           uint32         // 1199, 4796
+	Data4800            [7]uint32
 }
 
 var (
