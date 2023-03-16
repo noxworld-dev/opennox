cd496d9d6c5e2105f95e9e2bb842ca2fd14cdc56
Denys Smirnov <dennwc@pm.me>
2023-03-16 20:12:52 +0200

    Port Guard/GroupGuard.

--- src/legacy/script_noxscript_builtins.go
+++ src/legacy/script_noxscript_builtins.go
@@ -9,8 +9,6 @@ int nox_script_SetQuestFloat_514C10();
 int nox_script_GetQuestInt_514C40();
 int nox_script_GetQuestFloat_514C60();
 int nox_script_ResetQuestStatus_514C90();
-int nox_script_Guard_515600();
-int nox_script_GuardGroup_515700();
 int nox_script_HitLocation_5159E0();
 int nox_script_HitLocationGroup_515AE0();
 int nox_script_HitFarLocation_515B30();
@@ -96,8 +94,6 @@ var noxScriptBuiltins = [asm.BuiltinGetScore + 1]noxscript.Builtin{
 	asm.BuiltinGetQuestStatus:      wrapScriptC(C.nox_script_GetQuestInt_514C40),
 	asm.BuiltinGetQuestStatusFloat: wrapScriptC(C.nox_script_GetQuestFloat_514C60),
 	asm.BuiltinResetQuestStatus:    wrapScriptC(C.nox_script_ResetQuestStatus_514C90),
-	asm.BuiltinCreatureGuard:       wrapScriptC(C.nox_script_Guard_515600),
-	asm.BuiltinCreatureGroupGuard:  wrapScriptC(C.nox_script_GuardGroup_515700),
 	asm.BuiltinHitLocation:         wrapScriptC(C.nox_script_HitLocation_5159E0),
 	asm.BuiltinGroupHitLocation:    wrapScriptC(C.nox_script_HitLocationGroup_515AE0),
 	asm.BuiltinHitFarLocation:      wrapScriptC(C.nox_script_HitFarLocation_515B30),
