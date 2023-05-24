97d676b02b2bff132c169ec8b5fe0750427cf10d
Denys Smirnov <dennwc@pm.me>
2023-05-24 20:41:03 +0300

    Cleanup unused.

--- src/legacy/client__network__cdecode.c
+++ src/legacy/client__network__cdecode.c
@@ -66,7 +66,6 @@ void sub_456140(unsigned char a1);
 uint32_t nox_client_fadeObjects_80836 = 1;
 
 //----- (0048EA70) --------------------------------------------------------
-void sub_470A80();
 void sub_41CC00(char* s);
 int nox_xxx_netOnPacketRecvCli_48EA70_switch(int a1, int op, unsigned char* data, int sz) {
 	unsigned char* old = data;
