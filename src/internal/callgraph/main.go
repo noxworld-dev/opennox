4200388d839eccc3797bdcf821aafff19859977c
Denys Smirnov <dennwc@pm.me>
2023-05-24 17:56:45 +0300

    Generate node table in callgraph.

--- src/internal/callgraph/main.go
+++ src/internal/callgraph/main.go
@@ -93,13 +93,13 @@ func run(root string) error {
 		_ = execIn(root, "cxgo", "-c", "./cxgo_h.yml")
 	}
 
-	cdir := filepath.Join(root, "gonox")   // requires cxgo run!
-	chdir := filepath.Join(root, "gonoxh") // requires cxgo run!
+	cdir := filepath.Join(root, "gonox")  // requires cxgo run!
+	hdir := filepath.Join(root, "gonoxh") // requires cxgo run!
 
 	godir := filepath.Join(root, "src")
 	legacydir := filepath.Join(godir, "legacy")
 
-	scanDir := func(dir string, fnc func(file *ast.File)) error {
+	scanDir := func(dir string, fnc func(fname string, file *ast.File)) error {
 		return filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
 			if err != nil {
 				return err
@@ -123,7 +123,7 @@ func run(root string) error {
 			if err != nil {
 				return err
 			}
-			fnc(file)
+			fnc(filepath.Base(path), file)
 			return nil
 		})
 	}
@@ -131,35 +131,68 @@ func run(root string) error {
 	var (
 		curNode string
 	)
-	cf, err := os.Create(filepath.Join(root, "callgraph.csv"))
+	cnodes, err := os.Create(filepath.Join(root, "callgraph_nodes.csv"))
 	if err != nil {
 		return err
 	}
-	defer cf.Close()
-	cw := csv.NewWriter(cf)
-	defer cw.Flush()
-	cw.Write([]string{"Source", "Target"})
-	addEdge := func(n1, n2 string) {
-		cw.Write([]string{n1, n2})
+	defer cnodes.Close()
+	cwnodes := csv.NewWriter(cnodes)
+	defer cwnodes.Flush()
+	cwnodes.Write([]string{"ID", "Label", "File", "Type"})
+
+	cedges, err := os.Create(filepath.Join(root, "callgraph_edges.csv"))
+	if err != nil {
+		return err
 	}
-	addNode := func(name string) {
+	defer cedges.Close()
+	cwedges := csv.NewWriter(cedges)
+	defer cwedges.Flush()
+	cwedges.Write([]string{"Source", "Target"})
+	addEdge := func(n1, n2 string) {
+		cwedges.Write([]string{n1, n2})
+	}
+	addNodeEdge := func(name string) {
 		if curNode == "" {
 			return
 		}
 		addEdge(curNode, name)
+	}
+	seenNode := make(map[string]struct{})
+	addNode := func(name, file string) {
+		if _, ok := seenNode[name]; ok {
+			return
+		}
+		seenNode[name] = struct{}{}
+		typ := ""
+		switch {
+		case strings.HasPrefix(name, "C."):
+			typ = "cgo"
+		case strings.HasPrefix(name, "legacy."):
+			typ = "legacy"
+		case strings.HasPrefix(name, "main.Client."):
+			typ = "client"
+		case strings.HasPrefix(name, "main.Server."):
+			typ = "server"
+		case strings.HasPrefix(name, "main."):
+			typ = "main"
+		default:
+			typ = "other"
+		}
+		cwnodes.Write([]string{name, name, file, typ})
 	}
 
 	// =======================================
 	// Search for declarations
 
 	// Scan converted C files for declarations
-	err = scanDir(cdir, func(file *ast.File) {
+	err = scanDir(cdir, func(fname string, file *ast.File) {
 		walkInDecl(file, "C.", nil, func(d ast.Decl) {
 			switch d := d.(type) {
 			case *ast.FuncDecl:
 
 				declsC[d.Name.Name] = struct{}{}
 				isFunc[d.Name.Name] = struct{}{}
+				addNode("C."+d.Name.Name, fname)
 			case *ast.GenDecl:
 				for _, s := range d.Specs {
 					switch s := s.(type) {
@@ -174,7 +207,7 @@ func run(root string) error {
 			switch n := n.(type) {
 			case *ast.Ident:
 				usedC[n.Name]++
-				addNode("C." + n.Name)
+				addNodeEdge("C." + n.Name)
 			}
 		})
 	})
@@ -183,12 +216,13 @@ func run(root string) error {
 	}
 
 	// Scan converted C headers for declarations
-	err = scanDir(chdir, func(file *ast.File) {
+	err = scanDir(hdir, func(fname string, file *ast.File) {
 		walkInDecl(file, "C.", nil, func(d ast.Decl) {
 			switch d := d.(type) {
 			case *ast.FuncDecl:
 				declsC[d.Name.Name] = struct{}{}
 				isFunc[d.Name.Name] = struct{}{}
+				addNode("C."+d.Name.Name, fname)
 			}
 		}, func(n ast.Node) {})
 	})
@@ -197,11 +231,14 @@ func run(root string) error {
 	}
 
 	// Scan Go source in the legacy package
-	err = scanDir(legacydir, func(file *ast.File) {
+	err = scanDir(legacydir, func(fname string, file *ast.File) {
 		walkInDecl(file, "legacy.", nil, func(d ast.Decl) {
 			switch d := d.(type) {
 			case *ast.FuncDecl:
-				if strings.Contains(d.Doc.Text(), "export "+d.Name.Name) {
+				// Add all declarations. Non-exported ones are for cgo, exported are for main (maybe unused).
+				addNode("legacy."+d.Name.Name, fname)
+				if isCgoExport(d) {
+					// For cgo exports we note the type of this identifier and link cgo->legacy.
 					declsLegacy[d.Name.Name] = struct{}{}
 					isFunc[d.Name.Name] = struct{}{}
 					addEdge("C."+d.Name.Name, "legacy."+d.Name.Name)
@@ -214,13 +251,14 @@ func run(root string) error {
 				if !ok {
 					return
 				}
+				// Collect all cgo and other legacy usages.
 				switch cname.Name {
 				case "C":
 					usedGo[n.Sel.Name]++
-					addNode("C." + n.Sel.Name)
+					addNodeEdge("C." + n.Sel.Name)
 				case "legacy":
 					usedGo["legacy."+n.Sel.Name]++
-					addNode("legacy." + n.Sel.Name)
+					addNodeEdge("legacy." + n.Sel.Name)
 				}
 			}
 		})
@@ -230,11 +268,12 @@ func run(root string) error {
 	}
 
 	// Scan Go source in the main package
-	err = scanDir(godir, func(file *ast.File) {
-		walkInDecl(file, "", nil, func(d ast.Decl) {
+	err = scanDir(godir, func(fname string, file *ast.File) {
+		walkInDecl(file, "main.", nil, func(d ast.Decl) {
 			switch d := d.(type) {
 			case *ast.FuncDecl:
 				isFunc[d.Name.Name] = struct{}{}
+				addNode("main."+declName(d), fname)
 			}
 		}, func(n ast.Node) {
 			switch n := n.(type) {
@@ -243,7 +282,7 @@ func run(root string) error {
 					return
 				}
 				usedGo["legacy."+n.Sel.Name]++
-				addNode("legacy." + n.Sel.Name)
+				addNodeEdge("legacy." + n.Sel.Name)
 			}
 		})
 	})
@@ -258,7 +297,7 @@ func run(root string) error {
 	// Search for usages, build call graph
 
 	// Scan converted C files for declarations
-	err = scanDir(cdir, func(file *ast.File) {
+	err = scanDir(cdir, func(fname string, file *ast.File) {
 		walkInDecl(file, "C.", &curNode, nil, func(n ast.Node) {
 			switch n := n.(type) {
 			case *ast.Ident:
@@ -269,7 +308,7 @@ func run(root string) error {
 				_, ignore := ignoreC[n.Name]
 				_, isfnc := isFunc[n.Name]
 				if !ignore && isfnc && (okC || okGo) {
-					addNode("C." + n.Name)
+					addNodeEdge("C." + n.Name)
 				}
 			}
 		})
@@ -279,7 +318,7 @@ func run(root string) error {
 	}
 
 	// Scan Go source in the legacy package
-	err = scanDir(legacydir, func(file *ast.File) {
+	err = scanDir(legacydir, func(fname string, file *ast.File) {
 		walkInDecl(file, "legacy.", &curNode, nil, func(n ast.Node) {
 			switch n := n.(type) {
 			case *ast.SelectorExpr:
@@ -296,7 +335,7 @@ func run(root string) error {
 					_, ignore := ignoreC[n.Sel.Name]
 					_, isfnc := isFunc[n.Sel.Name]
 					if !ignore && isfnc && (okC || okGo) {
-						addNode("C." + n.Sel.Name)
+						addNodeEdge("C." + n.Sel.Name)
 					}
 				case "legacy":
 					usedGo["legacy."+n.Sel.Name]++
@@ -304,7 +343,7 @@ func run(root string) error {
 					_, okGo := declsLegacy[n.Sel.Name]
 					_, isfnc := isFunc[n.Sel.Name]
 					if isfnc && okGo {
-						addNode("legacy." + n.Sel.Name)
+						addNodeEdge("legacy." + n.Sel.Name)
 					}
 				}
 			}
@@ -312,7 +351,7 @@ func run(root string) error {
 	})
 
 	// Scan Go source in the main package
-	err = scanDir(godir, func(file *ast.File) {
+	err = scanDir(godir, func(fname string, file *ast.File) {
 		walkInDecl(file, "main.", &curNode, nil, func(n ast.Node) {
 			switch n := n.(type) {
 			case *ast.SelectorExpr:
@@ -329,11 +368,11 @@ func run(root string) error {
 					_, ignore := ignoreC[n.Sel.Name]
 					_, isfnc := isFunc[n.Sel.Name]
 					if !ignore && isfnc && (okC || okGo) {
-						addNode("C." + n.Sel.Name)
+						addNodeEdge("C." + n.Sel.Name)
 					}
 				case "legacy":
 					usedGo["legacy."+n.Sel.Name]++
-					addNode("legacy." + n.Sel.Name)
+					addNodeEdge("legacy." + n.Sel.Name)
 				}
 			}
 		})
@@ -367,6 +406,33 @@ func run(root string) error {
 	return nil
 }
 
+func isCgoExport(d *ast.FuncDecl) bool {
+	if d == nil || d.Doc == nil {
+		return false
+	}
+	for _, c := range d.Doc.List {
+		if strings.Contains(c.Text, "export "+d.Name.Name) {
+			return true
+		}
+	}
+	return false
+}
+
+func declName(d *ast.FuncDecl) string {
+	if d.Recv == nil {
+		return d.Name.Name
+	}
+	typ := d.Recv.List[0].Type
+	if x, ok := typ.(*ast.StarExpr); ok {
+		typ = x.X
+	}
+	if x, ok := typ.(*ast.IndexListExpr); ok {
+		typ = x.X
+	}
+	tname := typ.(*ast.Ident)
+	return tname.Name + "." + d.Name.Name
+}
+
 func walkInDecl(file *ast.File, cpref string, cur *string, dfnc func(d ast.Decl), fnc func(n ast.Node)) {
 	for _, d := range file.Decls {
 		if dfnc != nil {
@@ -376,19 +442,7 @@ func walkInDecl(file *ast.File, cpref string, cur *string, dfnc func(d ast.Decl)
 		case *ast.FuncDecl:
 			if d.Body != nil {
 				if cur != nil {
-					if d.Recv != nil {
-						typ := d.Recv.List[0].Type
-						if x, ok := typ.(*ast.StarExpr); ok {
-							typ = x.X
-						}
-						if x, ok := typ.(*ast.IndexListExpr); ok {
-							typ = x.X
-						}
-						tname := typ.(*ast.Ident)
-						*cur = cpref + tname.Name + "." + d.Name.Name
-					} else {
-						*cur = cpref + d.Name.Name
-					}
+					*cur = cpref + declName(d)
 				}
 				ast.Inspect(d.Body, func(n ast.Node) bool {
 					fnc(n)
