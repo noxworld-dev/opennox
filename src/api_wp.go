25c1e7880c808d72cb2a796fd54621e6e30268b6
Denys Smirnov <dennwc@pm.me>
2023-09-02 15:17:17 +0300

    Better output for object debug.

--- src/api_wp.go
+++ src/api_wp.go
@@ -8,9 +8,11 @@ import (
 )
 
 type debugWaypoint struct {
-	Ind int          `json:"ind"`
-	ID  string       `json:"id,omitempty"`
-	Pos types.Pointf `json:"pos"`
+	Ind      int          `json:"ind"`
+	ID       string       `json:"id,omitempty"`
+	ScriptID int          `json:"script_id,omitempty"`
+	Pos      types.Pointf `json:"pos"`
+	Flags    uint32       `json:"flags,omitempty"`
 }
 
 func (w *Waypoint) dump() *debugWaypoint {
@@ -18,9 +20,11 @@ func (w *Waypoint) dump() *debugWaypoint {
 		return nil
 	}
 	return &debugWaypoint{
-		Ind: w.Ind(),
-		ID:  w.ID(),
-		Pos: w.Pos(),
+		Ind:      w.Ind(),
+		ID:       w.ID(),
+		ScriptID: w.ScriptID(),
+		Pos:      w.Pos(),
+		Flags:    w.Flags,
 	}
 }
 
