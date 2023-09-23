c10c7054f82c4dafb14a1c46af5505770b2c06cd
Denys Smirnov <dennwc@pm.me>
2023-09-23 18:30:28 +0300

    Proper types for known use data.

--- src/legacy/object_use.go
+++ src/legacy/object_use.go
@@ -24,12 +24,14 @@ var (
 )
 
 func init() {
-	server.RegisterObjectUse("ConsumeUse", C.nox_xxx_useConsume_53EE10, 4)
-	server.RegisterObjectUse("ConsumeConfuseUse", C.nox_xxx_useCiderConfuse_53EF00, 4)
-	server.RegisterObjectUse("FireWandUse", C.nox_xxx_useFireWand_53F670, 0)
-	server.RegisterObjectUse("CastUse", C.nox_xxx_useCast_53ED90, 4)
-	server.RegisterObjectUse("EnchantUse", C.nox_xxx_useEnchant_53ED60, 8)
+	server.RegisterObjectUse("ConsumeUse", C.nox_xxx_useConsume_53EE10, unsafe.Sizeof(server.ConsumeUseData{}))
+	server.RegisterObjectUse("ConsumeConfuseUse", C.nox_xxx_useCiderConfuse_53EF00, unsafe.Sizeof(server.ConsumeUseData{}))
+	server.RegisterObjectUse("CastUse", C.nox_xxx_useCast_53ED90, unsafe.Sizeof(server.CastUseData{}))
+	server.RegisterObjectUse("EnchantUse", C.nox_xxx_useEnchant_53ED60, unsafe.Sizeof(server.EnchantUseData{}))
 	server.RegisterObjectUse("MushroomUse", C.nox_xxx_useMushroom_53ECE0, 0)
+	server.RegisterObjectUse("PotionUse", C.nox_xxx_usePotion_53EF70, unsafe.Sizeof(server.PotionUseData{}))
+
+	server.RegisterObjectUse("FireWandUse", C.nox_xxx_useFireWand_53F670, 0)
 	server.RegisterObjectUse("ReadUse", C.nox_xxx_useRead_53F7C0, 260)
 	server.RegisterObjectUse("WarpReadUse", C.sub_53F830, 260)
 	server.RegisterObjectUse("WandUse", C.nox_xxx_useLesserFireballStaff_53F290, 116)
@@ -37,15 +39,9 @@ func init() {
 	server.RegisterObjectUse("SpellRewardUse", C.nox_xxx_useSpellReward_53F9E0, 1)
 	server.RegisterObjectUse("AbilityRewardUse", C.nox_xxx_useAbilityReward_53FAE0, 1)
 	server.RegisterObjectUse("FieldGuideUse", C.sub_53F930, 64)
-	server.RegisterObjectUse("PotionUse", C.nox_xxx_usePotion_53EF70, 4)
 
-	server.RegisterObjectUseParse("ConsumeUse", wrapObjectUseParseC(C.sub_536390))
-	server.RegisterObjectUseParse("ConsumeConfuseUse", wrapObjectUseParseC(C.sub_536390))
-	server.RegisterObjectUseParse("CastUse", wrapObjectUseParseC(C.sub_536180))
-	server.RegisterObjectUseParse("EnchantUse", wrapObjectUseParseC(C.sub_536130))
 	server.RegisterObjectUseParse("WandUse", wrapObjectUseParseC(C.sub_536260))
 	server.RegisterObjectUseParse("WandCastUse", wrapObjectUseParseC(C.sub_5361B0))
-	server.RegisterObjectUseParse("PotionUse", wrapObjectUseParseC(C.sub_5363C0))
 }
 
 func wrapObjectUseParseC(ptr unsafe.Pointer) server.ObjectParseFunc {
