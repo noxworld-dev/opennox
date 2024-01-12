60b12a65ed07f80b8873b67bee6bc7de41fefb45
Denys Smirnov <dennwc@pm.me>
2024-01-12 19:33:29 +0200

    WIP: nxz: refactoring decoder

--- src/legacy/cnxz/nxz/decoder.go
+++ src/legacy/cnxz/nxz/decoder.go
@@ -144,71 +144,44 @@ func (dec *Decoder) readBits4() int {
 	return int(dec.readBits(4))
 }
 func (dec *Decoder) decode(dst []byte) (int, error) {
-	var (
-		v22  int32
-		v29  int32
-		v31  int32
-		v37  int32
-		v39  int32
-		v43  int32
-		v45i int
-		v46  int32
-		v47  int32
-		v49  int32
-		v50  int32
-		v51  int32
-		v52  []byte
-		v54  int32
-		v55  int32
-		v59  []byte
-		v60  uint32
-		v61i int
-		v65  int32
-		v66  int32
-		v67  int32
-		v68i int
-		v70  int32
-		v71  int32
-	)
 	var recs [tableSize]decoderRec
-	dstInd := 0
+	di := 0
 	dec.bitsCnt = 0
 	for {
 		if !dec.hasMore() {
 			return 0, io.ErrUnexpectedEOF
 		}
 		v9 := dec.readBits4()
-		var v13 int
+		var op int
 		if n := int(dec.data8.field4[2*v9]); n == 0 {
-			v13 = int(dec.data8.field132[dec.data8.field4[v9*2+1]])
+			op = int(dec.data8.field132[dec.data8.field4[v9*2+1]])
 		} else {
 			v15 := dec.readBits(n)
 			ind := int(uint32(v15) + dec.data8.field4[v9*2+1])
 			if ind >= tableSize {
 				return 0, errors.New("wrong table index")
 			}
-			v13 = int(dec.data8.field132[ind])
+			op = int(dec.data8.field132[ind])
 		}
-		dec.data8.field0.field0[v13]++
-		if v13 < 256 {
-			if dstInd >= len(dst) {
+		dec.data8.field0.field0[op]++
+		if op < 256 {
+			if di >= len(dst) {
 				return 0, io.ErrShortBuffer
 			}
-			dst[dstInd] = uint8(int8(v13))
-			dstInd++
-			dec.buf0[dec.field4%bufferSize] = uint8(int8(v13))
+			dst[di] = uint8(int8(op))
+			di++
+			dec.buf0[dec.field4%bufferSize] = uint8(int8(op))
 			dec.field4++
 			continue
-		} else if v13 == 272 {
+		} else if op == 272 {
 			sub57DEA0(&dec.data8, recs[:])
 			for i := range dec.data8.field132 {
 				dec.data8.field132[i] = recs[i].ind0
 			}
-			v22 = 0
-			v23a := dec.data8.field4[:]
-			v70 = 0
-			v66 = 16
-			for {
+			v22 := 0
+			v70 := 0
+			it := dec.data8.field4[:]
+			for i := 0; i < 16; i++ {
 				var ind int
 				for {
 					if dec.readBits(1) != 0 {
@@ -216,98 +189,75 @@ func (dec *Decoder) decode(dst []byte) (int, error) {
 					}
 					ind++
 				}
-				v22 += int32(ind)
-				v23a[0] = uint32(v22)
-				v23a[1] = uint32(v70)
-				v23a = v23a[2:]
+				v22 += ind
+				it[0] = uint32(v22)
+				it[1] = uint32(v70)
+				it = it[2:]
 				v70 += 1 << v22
-				if func() int32 {
-					p := &v66
-					*p--
-					return *p
-				}() == 0 {
-					break
-				}
 			}
 			continue
 		}
-		if v13 == 273 {
-			return dstInd, nil
+		if op == 273 {
+			return di, nil
 		}
 		var v28 int
-		if v13 < 264 {
-			v28 = v13 - 256
+		if op < 264 {
+			v28 = op - 256
 		} else {
-			v29 = int32(nxz_table_1[v13].v1)
-			v31 = int32(dec.readBits(int(v29)))
-			v28 = int(uint32(v31) + nxz_table_1[v13].v2)
+			v29 := int32(nxz_table_1[op].v1)
+			v31 := int32(dec.readBits(int(v29)))
+			v28 = int(uint32(v31) + nxz_table_1[op].v2)
 		}
-		v67 = int32(v28)
-		v71 = int32(dec.readBits(3))
-		v65 = 0
-		v37 = int32(nxz_table_2[v71+1].v1 + 9)
+		v67 := v28
+		v71 := int32(dec.readBits(3))
+		v65 := 0
+		v37 := int32(nxz_table_2[v71+1].v1 + 9)
 		if v37 > 8 {
 			v37 = int32(nxz_table_2[v71+1].v1 + 1)
-			v39 = int32(dec.readBits(8))
-			v65 = v39 << v37
+			v65 = int(dec.readBits(8)) << v37
 		}
-		v43 = int32(dec.readBits(int(v37)))
-		v45i = dstInd
-		v46 = int32((nxz_table_2[v71+1].v2 << 9) + uint32(v65|v43))
-		v47 = v67 + 4
-		v68i = dstInd + int(v67) + 4
+		v43 := int(dec.readBits(int(v37)))
+		v45i := di
+		v46 := int((nxz_table_2[v71+1].v2 << 9) + uint32(v65|v43))
+		v47 := v67 + 4
+		v68i := di + v67 + 4
 		if v68i > len(dst) {
 			return 0, io.ErrShortBuffer
 		}
-		if v48 := int(dec.field4 - uint32(v46)); v47 >= v46 {
-			v50 = int32(uint16(int16(v48)))
-			var v53 int
-			if uint32(int32(uint16(int16(v48)))+v46) <= bufferSize {
-				v53 = int(v46)
-				v52 = dec.buf0[v50:]
+		if v48 := int(dec.field4-uint32(v46)) % bufferSize; v47 >= v46 {
+			if v48+v46 <= bufferSize {
+				copy(dst[v45i:v45i+v46], dec.buf0[v48:v48+v46])
 			} else {
-				v51 = int32(bufferSize - uint32(uint16(int16(v48))))
 				n := bufferSize - int(v48)
-				copy(dst[dstInd:dstInd+n], dec.buf0[v48:v48+n])
-				v52 = dec.buf0[:]
-				v53 = int(v46 - v51)
-				v45i = dstInd + int(v51)
+				copy(dst[di:di+n], dec.buf0[v48:v48+n])
+				v53 := v46 - n
+				v45i = di + n
+				copy(dst[v45i:v45i+v53], dec.buf0[:v53])
 			}
-			copy(dst[v45i:v45i+v53], v52[:v53])
-			v54 = 0
-			v55 = v47 - v46
-			if v47-v46 > 0 {
-				for {
-					dst[int32(dstInd)+v46+v54] = dst[dstInd+int(v54)]
-					v54++
-					if v54 >= v55 {
-						break
-					}
+			if n := v47 - v46; n > 0 {
+				for i := 0; i < n; i++ {
+					dst[di+v46+i] = dst[di+i]
 				}
 			}
 		} else {
-			v49 = int32(uint16(int16(v48)))
-			if uint32(v49+v47) <= bufferSize {
-				copy(dst[dstInd:dstInd+int(v47)], dec.buf0[v49:v49+v47])
+			if uint32(v48+v47) <= bufferSize {
+				copy(dst[di:di+int(v47)], dec.buf0[v48:v48+v47])
 			} else {
-				n := bufferSize - int(v49)
-				copy(dst[dstInd:dstInd+n], dec.buf0[v49:int(v49)+n])
-				copy(dst[dstInd+n:dstInd+int(v47)], dec.buf0[:int(v47)-n])
+				n := bufferSize - int(v48)
+				copy(dst[di:di+n], dec.buf0[v48:int(v48)+n])
+				copy(dst[di+n:di+int(v47)], dec.buf0[:int(v47)-n])
 			}
 		}
-		if v57 := int(dec.field4 & math.MaxUint16); v57+int(v47) <= bufferSize {
-			v61i = dstInd
-			v60 = uint32(v47)
-			v59 = dec.buf0[v57:]
+		if v57 := int(dec.field4 & math.MaxUint16); v57+v47 <= bufferSize {
+			copy(dec.buf0[v57:v57+v47], dst[di:di+v47])
 		} else {
 			n := bufferSize - v57
-			copy(dec.buf0[v57:v57+n], dst[dstInd:dstInd+n])
-			v59 = dec.buf0[:]
-			v60 = uint32(int(v47) - n)
-			v61i = dstInd + n
+			copy(dec.buf0[v57:v57+n], dst[di:di+n])
+			v60 := v47 - n
+			v61i := di + n
+			copy(dec.buf0[:v60], dst[v61i:v61i+int(v60)])
 		}
-		copy(v59[:v60], dst[v61i:v61i+int(v60)])
 		dec.field4 += uint32(v47)
-		dstInd = v68i
+		di = v68i
 	}
 }
