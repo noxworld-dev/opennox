6dcb24c6b6bdd5a422ee6e3ca5f1cca4b5addd71
Denys Smirnov <dennwc@pm.me>
2024-01-28 01:38:08 +0200

    Split network.go

---
+++ src/network_client.go
@@ -0,0 +1,556 @@
+package opennox
+
+import (
+	"bytes"
+	"encoding/binary"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"image"
+	"time"
+	"unsafe"
+
+	"github.com/noxworld-dev/opennox-lib/console"
+	"github.com/noxworld-dev/opennox-lib/noxnet"
+	"github.com/noxworld-dev/opennox-lib/strman"
+
+	"github.com/noxworld-dev/opennox/v1/client"
+	"github.com/noxworld-dev/opennox/v1/client/gui"
+	noxflags "github.com/noxworld-dev/opennox/v1/common/flags"
+	"github.com/noxworld-dev/opennox/v1/common/memmap"
+	"github.com/noxworld-dev/opennox/v1/common/ntype"
+	"github.com/noxworld-dev/opennox/v1/common/sound"
+	"github.com/noxworld-dev/opennox/v1/internal/netstr"
+	"github.com/noxworld-dev/opennox/v1/legacy"
+	"github.com/noxworld-dev/opennox/v1/legacy/common/alloc"
+	"github.com/noxworld-dev/opennox/v1/server"
+)
+
+func nox_xxx_netOnPacketRecvCli_48EA70(ind ntype.PlayerInd, buf *byte, sz int) int {
+	return noxClient.nox_xxx_netOnPacketRecvCli48EA70(ind, unsafe.Slice(buf, sz))
+}
+
+func nox_xxx_netHandleCliPacket_43C860(_ netstr.Handle, data []byte) int {
+	op := noxnet.Op(data[0])
+	noxPerfmon.packetSizeCli = len(data)
+	if op == noxnet.MSG_XXX_STOP {
+		sub_446380()
+	} else if op == noxnet.MSG_PING {
+		noxPerfmon.ping = time.Duration(binary.LittleEndian.Uint32(data[1:])) * time.Millisecond
+	} else if op >= noxnet.MSG_TIMESTAMP {
+		noxClient.nox_xxx_netOnPacketRecvCli48EA70(server.HostPlayerIndex, data)
+		if nox_client_isConnected() {
+			legacy.Sub_48D660()
+		}
+	}
+	lastCliHandlePackets = platformTicks()
+	if dword_5d4594_815704 {
+		legacy.Sub_4AB4A0(0)
+		dword_5d4594_815704 = false
+	}
+	if dword_5d4594_815708 {
+		sub_43CF40()
+	}
+	return 1
+}
+
+func (c *Client) nox_xxx_netOnPacketRecvCli48EA70_switch(ind ntype.PlayerInd, op noxnet.Op, data []byte, localFrame *uint32, localFrame16 *uint16) int {
+	if len(data) == 0 {
+		return 0
+	}
+	if n, ok, err := c.Client.OnClientPacketOpSub(ind, op, data, localFrame, localFrame16, client.CurPlayerInfo{
+		Connected: nox_client_isConnected(),
+		Class:     getPlayerClass(),
+	}); err != nil {
+		return -1
+	} else if ok {
+		return n
+	}
+	switch op {
+	case noxnet.MSG_FULL_TIMESTAMP:
+		var p noxnet.MsgFullTimestamp
+		n, err := p.Decode(data[1:])
+		if err != nil {
+			return -1
+		}
+		frame := p.T
+		*localFrame = frame
+		c.tsFullPRev = frame
+		c.tsUpperPrev = (frame & 0xffff) >> 14
+		c.srv.SetFrame(frame)
+		if p := getCurPlayer(); p != nil {
+			legacy.Nox_xxx_playerUnsetStatus_417530(p, 64)
+		}
+		legacy.Sub_43C650()
+		return 1 + n
+	case noxnet.MSG_SIMULATED_TIMESTAMP:
+		frame := uint32(*localFrame16)
+		if frame < (c.tsFullPRev + sub_43C790()) {
+			c.srv.SetFrame(frame)
+		}
+		return 5
+	case noxnet.MSG_TIMESTAMP:
+		var p noxnet.MsgTimestamp
+		n, err := p.Decode(data[1:])
+		if err != nil {
+			return -1
+		}
+		fr16 := p.T
+		*localFrame16 = fr16
+		if p := getCurPlayer(); p != nil && p.Field3680&0x40 != 0 {
+			return 1 + n
+		}
+		v9 := true
+		prevFrame := c.srv.Frame()
+		fullFrame := uint32(fr16) + (c.srv.Frame() & 0xFFFF0000)
+		if upper := uint32(fr16) >> 14; upper != c.tsUpperPrev {
+			if upper == ((uint32(c.tsUpperPrev&0xff) + 1) & 3) {
+				c.tsUpperPrev = upper
+				if upper == 0 {
+					fullFrame += 0x10000
+				}
+			} else {
+				v9 = false
+			}
+		}
+		if fullFrame < c.srv.Frame() {
+			v9 = false
+		}
+		if !noxflags.HasGame(noxflags.GameHost) && v9 {
+			c.srv.SetFrame(fullFrame)
+			c.tsFullPRev = fullFrame
+		}
+		*localFrame = fullFrame
+		if !noxflags.HasGame(noxflags.GameHost) && !v9 {
+			noxPerfmon.latePackets--
+			*memmap.PtrUint32(0x85B3FC, 120)++
+		} else {
+			if c.srv.Frame() > prevFrame+1 {
+				noxPerfmon.latePackets += int(c.srv.Frame() - prevFrame)
+			}
+			legacy.Sub_43C650()
+		}
+		return 1 + n
+	case noxnet.MSG_RATE_CHANGE:
+		if nox_client_isConnected() {
+			sub_43C7A0(uint32(data[1]))
+		}
+		return 1 + 1
+	case noxnet.MSG_SERVER_QUIT:
+		var buf [1]byte
+		buf[0] = byte(noxnet.MSG_SERVER_QUIT_ACK)
+		netstrGetClientIndex().Send(buf[:1], netstr.SendFlush)
+		sub_446380()
+		return 1
+	case noxnet.MSG_USE_MAP:
+		var p noxnet.MsgUseMap
+		n, err := p.Decode(data[1:])
+		if err != nil {
+			return -1
+		}
+		if mframe := p.T; mframe > uint32(legacy.Get_dword_5d4594_1200804()) {
+			noxSetUseMapFrame(int(mframe))
+			c.nox_xxx_gameClearAll_467DF0(true)
+			c.srv.nox_xxx_gameSetMapPath_409D70(p.MapName.Value)
+			nox_xxx_mapSetCrcMB_409B10(p.CRC)
+			if !noxflags.HasGame(noxflags.GameHost) {
+				noxflags.UnsetGame(noxflags.GameFlag4)
+				if legacy.Get_dword_5d4594_2650652() != 0 {
+					legacy.Sub_41D6C0()
+				}
+			}
+			noxflags.SetGame(noxflags.GameFlag24)
+			legacy.Set_dword_5d4594_1200804(c.srv.Frame())
+			nox_xxx_gameSetCliConnected(false)
+			legacy.Sub_49C7A0()
+			legacy.Nox_xxx_guiServerOptionsHide_4597E0(0)
+			sub_44A400()
+		}
+		return 1 + n
+	case noxnet.MSG_JOIN_DATA:
+		var p noxnet.MsgJoinData
+		n, err := p.Decode(data[1:])
+		if err != nil {
+			return -1
+		}
+		playerID := nox_xxx_netClearHighBit_578B30(p.NetCode)
+		legacy.ClientSetPlayerNetCode(int(playerID))
+		pl := c.srv.Players.NewRaw(int(playerID))
+		if pl != nil {
+			pl.Field2068 = p.Unk2
+			setCurPlayer(pl)
+		}
+		if !noxflags.HasGame(noxflags.GameHost) {
+			legacy.Sub_57B920(memmap.PtrOff(0x5D4594, 1198020))
+		}
+		legacy.Set_dword_5d4594_1200804(0)
+		noxPerfmon.latePackets = 0
+		*memmap.PtrUint32(0x85B3FC, 120) = 0
+		noxPerfmon.ping = 0
+		legacy.Set_dword_5d4594_1200832(0)
+		legacy.Nox_xxx_cliSetSettingsAcquired_4169D0(0)
+		return 1 + n
+	case noxnet.MSG_NEW_PLAYER:
+		if len(data) < 129 {
+			return -1
+		}
+		playerID := nox_xxx_netClearHighBit_578B30(binary.LittleEndian.Uint16(data[1:]))
+		pl := c.srv.Players.NewRaw(int(playerID))
+		if pl == nil {
+			return 129
+		}
+		if !noxflags.HasGame(noxflags.GameHost) {
+			pl.NetCodeVal = uint32(playerID)
+			pl.Lessons = int32(int16(binary.LittleEndian.Uint16(data[100:])))
+			pl.Field2140 = uint32(int32(int16(binary.LittleEndian.Uint16(data[102:]))))
+			pl.ArmorEquip = binary.LittleEndian.Uint32(data[104:])
+			pl.WeaponEquip = binary.LittleEndian.Uint32(data[108:])
+			pl.Field2152 = uint32(data[116])
+			pl.Field2156 = uint32(data[117])
+			pl.SetField2096(alloc.GoStringS(data[119:]))
+			pl.Field3680 |= binary.LittleEndian.Uint32(data[112:])
+			pl.Info()
+			*pl.Info() = *(*server.PlayerInfo)(unsafe.Pointer(&data[3 : 3+97][0])) // TODO: safe copy
+			pl.SetName(pl.Info().Name() + pl.Info().NameSuff())
+			if legacy.Get_dword_5d4594_2650652() != 0 {
+				pl.Field2108 = 0
+				legacy.Sub_41D670(pl.Field2096())
+			}
+			legacy.Nox_xxx_playerInitColors_461460(pl)
+		}
+		legacy.Sub_457140(int(playerID), &pl.NameFinal[0])
+		legacy.Sub_455920(&pl.NameFinal[0])
+		if gameGetPlayState() == 3 {
+			format := c.Strings().GetStringInFile("PlayerJoined", "cdecode.c")
+			nox_xxx_printCentered_445490(fmt.Sprintf(format, pl.Name()))
+		}
+		if uint32(playerID) == uint32(legacy.ClientPlayerNetCode()) && alloc.GoString16((*uint16)(memmap.PtrOff(0x85B3FC, 12204))) != pl.Name() {
+			legacy.Set_dword_5d4594_1200832(1)
+		}
+		return 129
+	case noxnet.MSG_PLAYER_QUIT:
+		if len(data) < 3 {
+			return -1
+		}
+		if !nox_client_isConnected() {
+			return 3
+		}
+
+		playerID := nox_xxx_netClearHighBit_578B30(binary.LittleEndian.Uint16(data[1:]))
+		pl := c.srv.Players.ByID(int(playerID))
+		var msg string
+		if pl != nil {
+			legacy.Sub_456DF0(int(playerID))
+			legacy.Sub_455950(&pl.NameFinal[0])
+			format := c.Strings().GetStringInFile("PlayerLeft", "cdecode.c")
+			msg = fmt.Sprintf(format, pl.Name())
+
+			pl.Active = 0
+			tobj := nox_xxx_objGetTeamByNetCode_418C80(int(playerID))
+			if tobj != nil && tobj.Has() {
+				legacy.Nox_xxx_netChangeTeamMb_419570(tobj, uint32(playerID))
+			}
+		} else {
+			msg = c.Strings().GetStringInFile("UnknownLeft", "cdecode.c")
+		}
+		if gameGetPlayState() == 3 {
+			nox_xxx_printCentered_445490(msg)
+		}
+		return 3
+	case noxnet.MSG_REPORT_SPELL_START:
+		if len(data) < 2 {
+			return -1
+		}
+		legacy.Sub_49BB80(data[1])
+		return 2
+	case noxnet.MSG_REPORT_INVENTORY_LOADED:
+		legacy.Sub_467CA0()
+		return 1
+	case noxnet.MSG_FX_DEATH_RAY:
+		if len(data) < 9 {
+			return -1
+		}
+		p1x := binary.LittleEndian.Uint16(data[1:])
+		p1y := binary.LittleEndian.Uint16(data[3:])
+		p2x := binary.LittleEndian.Uint16(data[5:])
+		p2y := binary.LittleEndian.Uint16(data[7:])
+		c.clientFXDeathRay(image.Pt(int(p1x), int(p1y)), image.Pt(int(p2x), int(p2y)))
+		return 9
+	case noxnet.MSG_FX_PARTICLEFX:
+		if len(data) < 14 {
+			return -1
+		}
+		if nox_client_isConnected() {
+			dr := c.Objs.ByNetCode(binary.LittleEndian.Uint16(data[8:]))
+			c.r.partfx.onParticleFx(data[1], dr, int(binary.LittleEndian.Uint16(data[2:])), binary.LittleEndian.Uint16(data[4:]) != 0, int(binary.LittleEndian.Uint16(data[6:])))
+		}
+		return 14
+	case noxnet.MSG_IMPORTANT:
+		n := 1
+		if noxflags.HasGame(noxflags.GameHost) {
+			n = 5
+		}
+		if len(data) < n {
+			return -1
+		}
+		if nox_client_isConnected() {
+			var buf [5]byte
+			buf[0] = byte(noxnet.MSG_IMPORTANT_ACK)
+			if noxflags.HasGame(noxflags.GameHost) {
+				v := binary.LittleEndian.Uint32(data[1:])
+				binary.LittleEndian.PutUint32(buf[1:], v)
+			} else {
+				binary.LittleEndian.PutUint32(buf[1:], *localFrame)
+			}
+			c.srv.NetList.AddToMsgListCli(ind, 0, buf[:5])
+		}
+		return n
+	case noxnet.MSG_MAP_SEND_START:
+		if len(data) < 88 {
+			return -1
+		}
+		sz := binary.LittleEndian.Uint32(data[4:])
+		name := alloc.GoStringS(data[8:88])
+		c.mapsend.onMapDownloadStart(name, uint(sz))
+		return 88
+	case noxnet.MSG_MAP_SEND_PACKET:
+		if len(data) < 6 {
+			return -1
+		}
+		bind := binary.LittleEndian.Uint16(data[2:])
+		sz := binary.LittleEndian.Uint16(data[4:])
+		c.mapsend.onMapDownloadPart(uint(bind), data[6:6+sz])
+		return 6 + int(sz)
+	case noxnet.MSG_MAP_SEND_ABORT:
+		if len(data) < 2 {
+			return -1
+		}
+		c.mapsend.onMapDownloadAbort()
+		return 2
+	case noxnet.MSG_FADE_BEGIN:
+		var p noxnet.MsgFadeBegin
+		n, err := p.Decode(data[1:])
+		if err != nil {
+			return -1
+		}
+		if nox_client_isConnected() && !noxflags.HasEngine(noxflags.EngineNoRendering) {
+			if p.Out == 1 {
+				legacy.Set_nox_gameDisableMapDraw_5d4594_2650672(0)
+				sub_413A00(1)
+			}
+			c.clientPacketFade(p.Menu == 1, p.Out == 1)
+		}
+		return 1 + n
+	case noxnet.MSG_DIALOG:
+		if len(data) < 2 {
+			return -1
+		}
+		typ := data[1]
+		switch typ {
+		case 3:
+			if len(data) < 135 {
+				return -1
+			}
+			if nox_client_isConnected() {
+				str := strman.ID(alloc.GoStringS(data[2:34]))
+				title := alloc.GoString16B(data[34:98])
+				snd := sound.ID(binary.LittleEndian.Uint32(data[98:]))
+				pic := alloc.GoStringS(data[102:134])
+				flags := server.DialogFlags(data[134])
+				guiOpenNPCDialogID(title, snd, str, pic, flags)
+				if pl := getCurPlayer(); pl != nil {
+					legacy.Nox_xxx_netNeedTimestampStatus_4174F0(pl, 512)
+				}
+			}
+			return 135
+		case 4:
+			if nox_client_isConnected() {
+				guiCloseNPCDialog()
+				if pl := getCurPlayer(); pl != nil {
+					legacy.Nox_xxx_playerUnsetStatus_417530(pl, 512)
+				}
+			}
+			return 2
+		}
+		return -1
+	case noxnet.MSG_XFER_MSG:
+		if len(data) < 2 {
+			return -1
+		}
+		typ := data[1]
+		switch typ {
+		case 0: // XFER_START
+			if len(data) < 140 {
+				return -1
+			}
+			act := data[2]
+			sz := binary.LittleEndian.Uint32(data[4:])
+			styp := alloc.GoStringS(data[8:136])
+			tok := data[136]
+			xferStart40B5D0(netstrGetClientIndex(), act, styp, sz, tok)
+			return 140
+		case 1: // XFER_ACCEPT
+			if len(data) < 4 {
+				return -1
+			}
+			a2 := data[2]
+			a3 := data[3]
+			xferAccept40BFF0(netstrGetClientIndex(), a2, a3)
+			return 4
+		case 2: // XFER_DATA
+			if len(data) < 8 {
+				return -1
+			}
+			a2 := data[2]
+			a3 := binary.LittleEndian.Uint16(data[4:])
+			sz := int(binary.LittleEndian.Uint16(data[6:]))
+			if len(data) < 8+sz {
+				return -1
+			}
+			var buf [6]byte
+			buf[0] = byte(noxnet.MSG_XFER_MSG)
+			buf[1] = 3 // XFER_ACK
+			buf[2] = a2
+			binary.LittleEndian.PutUint16(buf[4:], a3)
+			netstrGetClientIndex().Send(buf[:6], netstr.SendQueue|netstr.SendFlush)
+			xferDataChunk40B250(netstrGetClientIndex(), a2, a3, data[8:8+sz])
+			return 8 + sz
+		case 3: // XFER_ACK
+			if len(data) < 6 {
+				return -1
+			}
+			a2 := data[2]
+			a3 := binary.LittleEndian.Uint16(data[4:])
+			xferAck40BF60(netstrGetClientIndex(), a2, a3)
+			return 6
+		case 4: // XFER_CLOSE
+			if len(data) < 3 {
+				return -1
+			}
+			a2 := data[2]
+			xferClose40C030(netstrGetClientIndex(), a2)
+			return 3
+		case 5: // XFER_CODE5
+			if len(data) < 4 {
+				return -1
+			}
+			a2 := data[2]
+			a3 := data[3]
+			xferCodeFive40B720(a3, a2)
+			return 4
+		case 6: // XFER_CODE6
+			if len(data) < 4 {
+				return -1
+			}
+			a2 := data[2]
+			a3 := data[3]
+			xferCodeSix40C070(netstrGetClientIndex(), a3, a2)
+			return 4
+		}
+		return -1
+	case noxnet.MSG_TEXT_MESSAGE:
+		var p noxnet.MsgText
+		n, err := p.Decode(data[1:])
+		if err != nil {
+			return -1
+		}
+		netcode := p.NetCode
+
+		// Buffer size in vanilla: 636
+		text := p.Text()
+		if p.Flags.Has(noxnet.TextLocalized) { // Localized
+			name := strman.ID(text)
+			text = c.Strings().GetStringInFile(name, "cdecode.c")
+		} else {
+			if p.Flags.Has(noxnet.TextTeam) { // Team chat
+				tstr := c.Strings().GetStringInFile("Guirank.c:team", "cdecode.c")
+				text = fmt.Sprintf("%s: %s", tstr, text)
+			}
+		}
+		msz := 1 + n
+		if !nox_client_isConnected() {
+			return msz
+		}
+		if gameGetPlayState() != 3 {
+			return msz
+		}
+		if p.Flags.Has(noxnet.TextNotice) { // Notice
+			str := c.Strings().GetStringInFile("guiserv.c:Notice", "cdecode.c")
+			NewDialogWindow(nil, str, text, gui.DialogFlag6|gui.DialogOKButton, nil, nil)
+			return msz
+		}
+		if netcode == 0 { // From server
+			nox_xxx_printCentered_445490(text)
+			return msz
+		}
+		pl := c.Server.Players.ByID(int(netcode))
+		if pl != nil { // From player
+			if !nox_xxx_playerCantTalkMB_57A160(pl) {
+				c.Printf(console.ColorYellow, "%s> %s", pl.Name(), text)
+				legacy.Nox_xxx_createTextBubble_48D880(data, text)
+			}
+			return msz
+		}
+		// From object
+		legacy.Nox_xxx_createTextBubble_48D880(data, text)
+		if dr := c.Objs.ByNetCode(netcode); dr != nil {
+			var pname string
+			if t := c.Things.TypeByInd(int(dr.TypeIDVal)); t != nil {
+				pname = alloc.GoString16(t.PrettyName)
+			}
+			c.Printf(console.ColorRed, "%s(%d)> %s", pname, netcode, text)
+		}
+		return msz
+	}
+	return legacy.Nox_xxx_netOnPacketRecvCli_48EA70_switch(ind, op, data)
+}
+
+func (c *Client) HandleMessage(ind ntype.PlayerInd, msg noxnet.Message) int {
+	// TODO: avoid re-encoding
+	buf, err := noxnet.AppendPacket(nil, msg)
+	if err != nil {
+		panic(err)
+	}
+	return c.nox_xxx_netOnPacketRecvCli48EA70(ind, buf)
+}
+
+func (c *Client) nox_xxx_netOnPacketRecvCli48EA70(ind ntype.PlayerInd, data []byte) int {
+	orig := data
+	var iters [][]byte
+	c.guiFPS.sub_470A80()
+	var (
+		localFrame   uint32
+		localFrame16 uint16
+	)
+	for len(data) > 0 {
+		op := noxnet.Op(data[0])
+		n := c.nox_xxx_netOnPacketRecvCli48EA70_switch(ind, op, data, &localFrame, &localFrame16)
+		if n == 0 {
+			break // stop earlier
+		} else if n < 0 {
+			return 0 // error
+		}
+		if n == 0 { // safeguard
+			n = 1
+		}
+		if n > len(data) {
+			var buf bytes.Buffer
+			fmt.Fprintf(&buf, "overflow when handling net packet: [%d:%d]\n", n, len(data))
+			fmt.Fprintf(&buf, "PACKET: (%d bytes)\n", len(orig))
+			buf.WriteString(hex.EncodeToString(orig))
+			buf.WriteString("\n")
+			buf.WriteString("SPLIT:\n")
+			for _, b := range iters {
+				buf.WriteString(hex.EncodeToString(b))
+				buf.WriteString("\n")
+			}
+			buf.WriteString(hex.EncodeToString(data))
+			buf.WriteString("\n")
+			panic(errors.New(buf.String()))
+		}
+		iters = append(iters, data[:n])
+		noxOnCliPacketDebug(op, data[:n])
+		data = data[n:]
+	}
+	return 1
+}
---
+++ src/network_server.go
@@ -0,0 +1,409 @@
+package opennox
+
+import (
+	"encoding/binary"
+	"fmt"
+	"unsafe"
+
+	"github.com/noxworld-dev/opennox-lib/common"
+	"github.com/noxworld-dev/opennox-lib/console"
+	"github.com/noxworld-dev/opennox-lib/datapath"
+	"github.com/noxworld-dev/opennox-lib/ifs"
+	"github.com/noxworld-dev/opennox-lib/noxnet"
+
+	noxflags "github.com/noxworld-dev/opennox/v1/common/flags"
+	"github.com/noxworld-dev/opennox/v1/common/ntype"
+	"github.com/noxworld-dev/opennox/v1/internal/netstr"
+	"github.com/noxworld-dev/opennox/v1/legacy"
+	"github.com/noxworld-dev/opennox/v1/legacy/common/alloc"
+	"github.com/noxworld-dev/opennox/v1/server"
+)
+
+func (s *Server) onPacket(ind ntype.PlayerInd, data []byte) bool {
+	cdata, cfree := alloc.Make([]byte{}, len(data))
+	defer cfree()
+	return s.onPacketRaw(ind, cdata)
+}
+
+func (s *Server) onPacketRaw(pli ntype.PlayerInd, data []byte) bool {
+	pl := s.Players.ByInd(pli)
+	if len(data) == 0 {
+		if pl != nil {
+			pl.Frame3596 = s.Frame()
+		}
+		return true
+	}
+	if noxflags.HasEngine(noxflags.EngineReplayWrite) {
+		s.nox_xxx_replayWriteMSgMB(pl, data)
+	}
+	op := noxnet.Op(data[0])
+	switch op {
+	case 0x20:
+		if s.newPlayerFromPacket(pli, data[1:]) == 0 {
+			netstr.Global.ReadPackets(netstr.Global.ByPlayerInd(pli))
+		}
+		return true
+	case 0x22:
+		legacy.Nox_xxx_playerForceDisconnect_4DE7C0(pli)
+		return true
+	case 0x25:
+		if pl != nil {
+			pl.Frame3596 = s.Frame()
+		}
+		return true
+	}
+	if pl == nil {
+		return true
+	}
+	u := pl.PlayerUnit
+	if u == nil {
+		return true
+	}
+	for len(data) > 0 {
+		op = noxnet.Op(data[0])
+		n, ok := s.onPacketOp(pli, op, data, pl, u)
+		if !ok {
+			netstr.Log.Printf("SERVER: ERR: op=%d (%s) [%d:???]\n%02x %x", int(op), op.String(), op.Len(), data[0], data[1:])
+			return false
+		}
+		if netstr.Global.Debug && n != 0 {
+			netstr.Log.Printf("SERVER: op=%d (%s) [%d:%d]\n%02x %x", int(op), op.String(), int(n)-1, op.Len(), data[0], data[1:])
+		}
+		if n > len(data) {
+			panic(fmt.Errorf("incorrect length returned for op: %v", op))
+		}
+		data = data[n:]
+	}
+	pl.Frame3596 = s.Frame()
+	return true
+}
+
+func (s *Server) onPacketOp(pli ntype.PlayerInd, op noxnet.Op, data []byte, pl *server.Player, u *server.Object) (int, bool) {
+	if n, ok, err := s.Server.OnPacketOpSub(pli, op, data, pl, u); err != nil {
+		return n, false
+	} else if ok {
+		return n, true
+	}
+	switch op {
+	case noxnet.MSG_NEED_TIMESTAMP:
+		legacy.Nox_xxx_netNeedTimestampStatus_4174F0(pl, 64)
+		return 1, true
+	case noxnet.MSG_TRY_ABILITY:
+		if len(data) < 2 {
+			return 0, false
+		}
+		if !noxflags.HasGame(noxflags.GameModeChat) && pl.Field3680&0x3 == 0 {
+			s.abilities.Do(u, server.Ability(data[1]))
+		}
+		return 2, true
+	case noxnet.MSG_CLIENT_READY:
+		legacy.Nox_xxx_gameServerReadyMB_4DD180(pl.Index())
+		return 1, true
+	case noxnet.MSG_SERVER_QUIT_ACK:
+		serverQuitAck()
+		return 1, true
+	case noxnet.MSG_INCOMING_CLIENT:
+		legacy.Nox_xxx_netPlayerIncomingServ_4DDF60(pl.Index())
+		return 1, true
+	case noxnet.MSG_OUTGOING_CLIENT:
+		nox_xxx_playerDisconnFinish_4DE530(pl.PlayerIndex(), 2)
+		return 1, true
+	case noxnet.MSG_REQUEST_TIMER_STATUS:
+		v41 := legacy.Sub_40A220()
+		Nox_xxx_netTimerStatus_4D8F50(pli, v41)
+		return 1, true
+	case noxnet.MSG_CANCEL_MAP:
+		s.mapSend.mapSendCancel(pl.PlayerIndex())
+		return 1, true
+	case noxnet.MSG_RECEIVED_MAP:
+		pl.Field3676 = 3
+		s.mapSend.EndReceive(pl.PlayerIndex())
+		return 1, true
+	case noxnet.MSG_TEXT_MESSAGE:
+		var msg noxnet.MsgText
+		n, err := msg.Decode(data[1:])
+		if err != nil {
+			return 0, false
+		}
+		text := msg.Text()
+		msz := 1 + n
+		if pl != nil && (pl.Field3680>>2)&0x1 != 0 {
+			return msz, true
+		}
+		orig := text
+		if !msg.Flags.Has(noxnet.TextTeam) { // global chat
+			text = s.CallOnChat(nil, pl, pl.PlayerUnit, text)
+			if text == "" {
+				return msz, true // mute
+			}
+			if text != orig {
+				msg.Flags = noxnet.TextUTF8
+				msg.Data = []byte(text + "\x00")
+				msg.Size = byte(len(text))
+			}
+			for it := s.Players.First(); it != nil; it = s.Players.Next(it) {
+				if noxflags.HasGame(noxflags.GameClient) && it.Index() == server.HostPlayerIndex {
+					noxClient.HandleMessage(server.HostPlayerIndex, &msg)
+				} else {
+					conn := netstr.Global.ByPlayer(it)
+					conn.SendMsg(&msg, 0)
+					conn.SendReadPacket(true)
+				}
+			}
+			return msz, true
+		}
+		// team message
+		netcode := int(msg.NetCode)
+		tm := nox_xxx_objGetTeamByNetCode_418C80(netcode)
+		if tm == nil || !tm.Has() {
+			return msz, true
+		}
+		tcl := s.Teams.ByID(tm.ID)
+		if tcl == nil {
+			return msz, true
+		}
+		text = s.CallOnChat(tcl, pl, pl.PlayerUnit, text)
+		if text == "" {
+			return msz, true // mute
+		}
+		if text != orig {
+			msg.Flags = noxnet.TextUTF8 | noxnet.TextTeam
+			msg.Data = []byte(text + "\x00")
+			msg.Size = byte(len(text))
+		}
+		for it := s.Players.First(); it != nil; it = s.Players.Next(it) {
+			uit := it.PlayerUnit
+			if uit == nil {
+				continue
+			}
+			if legacy.Nox_xxx_teamCompare2_419180(uit.TeamPtr(), tcl.ID()) != 0 {
+				if noxflags.HasGame(noxflags.GameClient) && int(uit.NetCode) == legacy.ClientPlayerNetCode() {
+					noxClient.HandleMessage(it.PlayerIndex(), &msg)
+				} else {
+					conn := netstr.Global.ByPlayer(it)
+					conn.SendMsg(&msg, 0)
+					conn.SendReadPacket(true)
+				}
+			}
+		}
+		return msz, true
+	case noxnet.MSG_SYSOP_PW:
+		if len(data) < 21 {
+			return 0, false
+		}
+		var buf [2]byte
+		buf[0] = byte(noxnet.MSG_SYSOP_RESULT)
+		pass := legacy.Nox_xxx_sysopGetPass_40A630()
+		got := alloc.GoString16B(data[1:])
+		if pass == "" || pass != got {
+			buf[1] = 0
+		} else {
+			buf[1] = 1
+			if legacy.Sub_4D12A0(pl.Index()) == 0 {
+				legacy.Sub_4D1210(pl.Index())
+				str := s.Strings().GetStringInFile("sysopAccessGranted", "sdecode.c")
+				s.Printf(console.ColorRed, str, pl.Name())
+			}
+		}
+		s.NetSendPacketXxx0(pl.Index(), buf[:2], 0, 1)
+		return 1 + 20, true
+	case noxnet.MSG_SERVER_CMD:
+		if len(data) < 7 {
+			return 0, false
+		}
+		sz := int(data[4])
+		wtext := data[5:]
+		if len(wtext) < 2*sz {
+			return 0, false
+		}
+		wtext = wtext[:2*sz]
+		nox_xxx_serverHandleClientConsole_443E90(pl, data[1], alloc.GoString16B(wtext))
+		return 5 + 2*sz, true
+	case noxnet.MSG_IMPORTANT_ACK:
+		if len(data) < 5 {
+			return 0, false
+		}
+		id := binary.LittleEndian.Uint32(data[1:])
+		legacy.Nox_net_importantACK_4E55A0(pl.Index(), int(id))
+		return 5, true
+	case noxnet.MSG_REQUEST_MAP:
+		s.PlayerGoObserver(pl, true, true)
+		if u != nil {
+			legacy.Nox_xxx_netChangeTeamMb_419570(u.TeamPtr(), uint32(pl.NetCode()))
+		}
+		s.mapSend.StartSendShared(pl.PlayerIndex())
+		return 1, true
+	case noxnet.MSG_REQUEST_SAVE_PLAYER:
+		if len(data) < 3 {
+			return 0, false
+		}
+		if noxflags.HasGame(noxflags.GameModeQuest) && pl.Index() != server.HostPlayerIndex && pl.IsActive() && u != nil && u.UpdateDataPlayer().Field138 == 1 {
+			s.PlayerDisconnect(pl, 2)
+		} else {
+			fname := datapath.Save(common.SaveDir, "_temp_.dat")
+			defer ifs.Remove(fname)
+			if savePlayerData(fname, pl.PlayerIndex()) {
+				sub41CFA0(fname, pl.PlayerIndex())
+			}
+		}
+		return 3, true
+	case noxnet.MSG_TEAM_MSG:
+		if len(data) < 10 {
+			return 0, false
+		}
+		switch data[1] {
+		case 10:
+			ti := server.TeamID(binary.LittleEndian.Uint32(data[2:]))
+			tm := s.Teams.ByID(ti)
+			if tm == nil {
+				return 10, true
+			}
+			netcode := int(binary.LittleEndian.Uint16(data[6:]))
+			obj := s.getObjectFromNetCode(netcode)
+			legacy.Nox_xxx_createAtImpl_4191D0(tm.ID(), obj.TeamPtr(), 1, netcode, 1)
+			return 10, true
+		case 11:
+			netcode := int(binary.LittleEndian.Uint16(data[6:]))
+			u2 := s.getObjectFromNetCode(netcode)
+			if u2 == nil {
+				return 10, true
+			}
+			if !noxflags.HasGame(noxflags.GameModeChat) {
+				pos := s.nox_xxx_mapFindPlayerStart_4F7AB0(u2)
+				asObjectS(u2).SetPos(pos)
+			}
+			ti := server.TeamID(binary.LittleEndian.Uint32(data[2:]))
+			tm := s.Teams.ByID(ti)
+			if tm == nil {
+				return 10, true
+			}
+			legacy.Sub_4196D0(unsafe.Pointer(u2.TeamPtr()), tm.C(), netcode, 1)
+			return 10, true
+		default:
+			return 0, false
+		}
+	case noxnet.MSG_SOCIAL:
+		if len(data) < 2 {
+			return 0, false
+		}
+		switch data[1] {
+		case 1:
+			nox_xxx_playerSetState_4FA020(u, server.PlayerStateLaugh)
+		case 2:
+			nox_xxx_playerSetState_4FA020(u, server.PlayerStateShakeFist)
+		case 4:
+			nox_xxx_playerSetState_4FA020(u, server.PlayerState27) // TODO: should it be state 28 (point)?
+		}
+		return 2, true
+	case noxnet.MSG_DIALOG:
+		if len(data) < 2 {
+			return 0, false
+		}
+		typ := data[1]
+		switch typ {
+		case 1:
+			if len(data) < 4 {
+				return 0, false
+			}
+			if nox_xxx_gameGet_4DB1B0() {
+				return 4, true
+			}
+			if pl.Field3680&0x3 != 0 {
+				return 4, true
+			}
+			id := binary.LittleEndian.Uint16(data[2:])
+			if obj := legacy.Nox_server_getObjectFromNetCode_4ECCB0(int(id)); obj != nil {
+				nox_xxx_script_forcedialog_548CD0(u, obj)
+			}
+			return 4, true
+		case 2:
+			if len(data) < 3 {
+				return 0, false
+			}
+			legacy.Nox_xxx_scriptDialog_548D30(u, data[2])
+			return 3, true
+		}
+		return 0, false
+	case noxnet.MSG_XFER_MSG:
+		if len(data) < 2 {
+			return 0, false
+		}
+		typ := data[1]
+		switch typ {
+		case 0: // XFER_START
+			if len(data) < 140 {
+				return 0, false
+			}
+			a2 := data[2]
+			a3 := binary.LittleEndian.Uint32(data[4:])
+			styp := alloc.GoStringS(data[8:136])
+			a4 := data[136]
+			xferStart40B5D0(netstr.Global.ByPlayer(pl), a2, styp, a3, a4)
+			return 140, true
+		case 1: // XFER_ACCEPT
+			if len(data) < 4 {
+				return 0, false
+			}
+			a2 := data[2]
+			a3 := data[3]
+			xferAccept40BFF0(netstr.Global.ByPlayer(pl), a2, a3)
+			return 4, true
+		case 2: // XFER_DATA
+			if len(data) < 8 {
+				return 0, false
+			}
+			a2 := data[2]
+			a3 := binary.LittleEndian.Uint16(data[4:])
+			sz := int(binary.LittleEndian.Uint16(data[6:]))
+			if len(data) < 8+sz {
+				return 0, false
+			}
+			var buf [6]byte
+			buf[0] = byte(noxnet.MSG_XFER_MSG)
+			buf[1] = 3 // XFER_ACK
+			buf[2] = a2
+			binary.LittleEndian.PutUint16(buf[4:], a3)
+			netstr.Global.ByPlayer(pl).Send(buf[:6], netstr.SendQueue|netstr.SendFlush)
+			xferDataChunk40B250(netstr.Global.ByPlayer(pl), a2, a3, data[8:8+sz])
+			return 8 + sz, true
+		case 3: // XFER_ACK
+			if len(data) < 6 {
+				return 0, false
+			}
+			a2 := data[2]
+			a3 := binary.LittleEndian.Uint16(data[4:])
+			xferAck40BF60(netstr.Global.ByPlayer(pl), a2, a3)
+			return 6, true
+		case 4: // XFER_CLOSE
+			if len(data) < 3 {
+				return 0, false
+			}
+			a2 := data[2]
+			xferClose40C030(netstr.Global.ByPlayer(pl), a2)
+			return 3, true
+		case 5: // XFER_CODE5
+			if len(data) < 4 {
+				return 0, false
+			}
+			a2 := data[2]
+			a3 := data[3]
+			xferCodeFive40B720(a3, a2)
+			return 4, true
+		case 6: // XFER_CODE6
+			if len(data) < 4 {
+				return 0, false
+			}
+			a2 := data[2]
+			a3 := data[3]
+			xferCodeSix40C070(netstr.Global.ByPlayer(pl), a3, a2)
+			return 4, true
+		}
+		return 0, false
+	default:
+		res := legacy.Nox_xxx_netOnPacketRecvServ_51BAD0_net_sdecode_switch(pli, data, pl, u, u.UpdateData)
+		if res <= 0 || res > len(data) {
+			return 0, false
+		}
+		return res, true
+	}
+}
